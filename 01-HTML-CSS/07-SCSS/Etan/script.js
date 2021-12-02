ccfc7f7a1802d0295939bb269b70547a328c5339
EtanHey <etan@heyman.net>
2021-12-01 18:27:21 -0600

    What I finally got

--- 01-HTML-CSS/07-SCSS/Etan/script.js
+++ 01-HTML-CSS/07-SCSS/Etan/script.js
@@ -1,11 +1,1 @@
-const video = document.querySelectorAll("video")
 
-videos.forEach(video => {
-    video.addEvenetListener("mouseover", function (){
-         this.play()
-    })
-
-video.addEvenetListener("mouseout", function() {
-    this.pause()
-})
-})
