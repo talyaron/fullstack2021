4510cddcc54e7bca5fff230d4d4dd5d0047054a1
ShaniRom <93940556+ShaniRom@users.noreply.github.com>
2021-12-09 23:03:56 +0200

    shani js

--- 02-JavaScript/01-variables/shani/index.js
+++ 02-JavaScript/01-variables/shani/index.js
@@ -10,3 +10,16 @@ console.log(z+4);
 for(let w = 0 ; w<20 ; w ++) {
     console.log(w)
 }
+
+//exercise 2:
+let num1, num2 ,num3;
+num1=window.prompt("Put the first number", "0");
+num2=window.prompt("Put the first number", "0");
+num3=window.prompt("Put the first number", "0");
+
+console.log("the sign is"+(Math.sign(num1)));
+console.log("the sign is"+(Math.sign(num2)));
+console.log("the sign is"+(Math.sign(num3)));
+
+//exercise 3:
+
