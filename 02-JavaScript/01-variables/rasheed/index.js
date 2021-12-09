5c350a9a1c87990fdcd2360c3b2182cc3f008b99
Rasheed <rasheedj966@gmail.com>
2021-12-09 18:19:11 +0200

    update

--- 02-JavaScript/01-variables/rasheed/index.js
+++ 02-JavaScript/01-variables/rasheed/index.js
@@ -6,9 +6,10 @@ x= y+z;
 y= x-z;
 z= x-y;
 
-console.log(x);
-console.log(y);
-console.log(z);
+console.log(x); console.log(y+z);
+console.log(y); console.log(x-z);
+console.log(z); console.log(x-y);
+
 
 function changeColor(ev){
     document.body.style.background = ev.target.value;
