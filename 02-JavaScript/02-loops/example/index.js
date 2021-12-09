b0e16741c24c4893ca45cd553f04462db8549037
Tal <tal.yaron@gmail.com>
2021-12-09 19:54:51 +0200

    cont

--- 02-JavaScript/02-loops/example/index.js
+++ 02-JavaScript/02-loops/example/index.js
@@ -2,6 +2,20 @@
 
 //for loop
 //for(initial value; condition to be true; increment){
-    //code block
+//code block
 //}
 
+for (let i = 0; i < 10; i++) {
+    console.log(i);
+    // console.log(i * 5);
+    // console.log('I is ' + i)
+
+    let a = 12;
+    var b = 23;
+
+    console.log(a)
+
+};
+
+console.log(b); //no error
+// console.log(a); //error
