f4a6efb787583c0c91a1ea14efdea7331d66d9a8
Chaim <chaimytcohen@gmail.com>
2021-12-13 13:46:40 +0200

    chaim

--- 02-JavaScript/01-variables/chaim/index.js
+++ 02-JavaScript/01-variables/chaim/index.js
@@ -5,10 +5,123 @@
     //code block
 //}
 
-for(let i = 0; i<9; i += 4){
+for(let i = 0; i<10; i += 3){
     console.log(i*5);
-    console.log(i*5);
-    console.log('I is '+ i)
+    console.log(i*4);
+    console.log(i*8);
+}
+
+
+
+//let x,y;{
+//x = window.prompt("input numer");
+//y = window.prompt("input another number");
+//if (y < x) 
+// console.log(  "this is a big number:" +x )
+
+
+//if (x <y) 
+//
+
+
+
+
+//console.log( "this is a big number:" +y )
+
+//}
+
+
+// let a, b, z;//
+ {
+ //a= window. prompt ("numaer one");
+ //b=window.prompt("number two");
+ //z=window.prompt("number three");
+
+ //if (a*b*z<0) 
+ //console.log("this is -")
+ //else
+ //console.log("this is : +")
+ }
+
+
+
+ //let a, b, z;
+ //{
+ //a= window. prompt ("numaer one");
+ //b=window.prompt("number two");
+// z=window.prompt("number three");
+
+ //if (a > b,z)+(b>z) 
+// console.log(a,b,z)
+ //if (a > b,z)+(z>b) 
+ //co/nsole.log(a,z,b)
+//if (b>a,z)+(a>z)
+ //console.log(b,a,z)
+ //if(b>a,z)+(z>a)
+ //console.log(b,z,a)
+ //}
+
+ 
+{
+let h= 8;
+let i= 1;
+let f= 5;
+let j= 3;
+let k= 2;
+
+{
+if (i > h &&i>f  &&i> j &&i>k) 
+   console.log(i)
+   else 
+  console.log ("nothing")
+}
+
+if (h > i &&h>f  &&h> j &&h>k) 
+   console.log(h)
+   else
+  console.log ("nothing")
+
+
+  if (f > h &&f>i  &&f> j &&f>k) 
+   console.log(f)
+   else (i>h,f,k,j) 
+  console.log ("nothing")
+
+
+  if (j > h &&j>f  &&j> i &&j>k) 
+   console.log(k)
+   else (i>h,f,k,j) 
+  console.log ("nothing")
+
+
+  if (k > h &&k>f  &&k> j &&k>i) 
+   console.log(j)
+   else (i>h,f,k,j) 
+  console.log ("nothing")
+}
+
+for (var x=0; x<=15; x++) {
+
+    if (x == 0) {
+        console.log(x +  " is even");
+}
+else if (x % 2 == 0) {
+        console.log(x + " is even");   
+}
+else {
+        console.log(x + " is odd");
+}
+}
+
+
+{
+let student=  ('moshe 80,   yakov 90,   moty 100,    yosi 60,   yoni 85');
+   console.log (student*5)
+
+  
+
+
+ 
 }
 
 
