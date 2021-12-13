fa46a55754131262f186a1130926127e2e38018e
Lkrasnop <93338033+Lkrasnop@users.noreply.github.com>
2021-12-13 18:55:57 +0200

    test

---
+++ 02-JavaScript/01-variables/example/Lior/dist/index2.dev.js
@@ -0,0 +1,15 @@
+"use strict";
+
+function BMI(height, weight) {
+  var bmi = weight / math.pow(height, 2);
+
+  if (bmi > 25) {
+    console.log('You are overweitht');
+  }
+
+  elsif(bmi > 18.5 && bmi < 25);
+  {
+    console.log('You are Normal');
+  }
+  els;
+}
