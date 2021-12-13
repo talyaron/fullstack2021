c703f5d3f57e9da47e47fb627e74862fc709913a
artur121255 <arturyaski96@gmail.com>
2021-12-13 19:28:50 +0200

    js,task

--- 02-JavaScript/00-introduction/Example/index.js
+++ 02-JavaScript/00-introduction/Example/index.js
@@ -1,15 +1,11 @@
-let counter = 0;
-
-const resultHTML = document.getElementById('theResult');
-
-
-function addOne() {
-    counter++;
-    resultHTML.innerText = counter
-
-
+function bmi (a,b,){
+    return a/(b*b);
 }
-
-function changeColor(ev){
-    document.body.style.background = ev.target.value;
-}
+let x = bmi(100,1.6)
+console.log (x)
+if (x<18.5)console.log('under-weight')
+else if (x<25)console.log('correct')
+else if (x<29.9)console.log('over-weight')
+else if (x<34.9)console.log('obese-lvl-1')
+else if (x<39.9)console.log('obese-lvl-2')
+else console.log('obese-lvl-3')
