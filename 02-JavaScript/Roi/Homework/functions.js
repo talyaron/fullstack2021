e2c4051c114cdd00be8f31667bf5802f63e7a4c7
RoiMirmelshtein <93940584+RoiMirmelshtein@users.noreply.github.com>
2021-12-14 16:43:34 +0200

    Roi

---
+++ 02-JavaScript/Roi/Homework/functions.js
@@ -0,0 +1,23 @@
+// exercise 1
+
+let num1 = window.prompt("please enter first Num:");
+let num2 = window.prompt("please enter second Num:");
+
+function BiggerNum (num1,num2) {
+
+if (num1 == num2) {
+		return result
+} 
+else if (num1 > num2) {
+		return result
+} 
+else {
+		return result
+}
+
+}
+
+alert(num1 + " is larger than " + num2);
+alert(num1 + " is larger than " + num2);
+alert(num1 + " is equal to " + num2);
+
