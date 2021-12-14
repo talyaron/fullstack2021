7b00d29f7341d7adcc62f78c09374c01ec78065e
OrA99 <oravramashvili@gmail.com>
2021-12-14 23:05:19 +0200

    or HW

---
+++ 02-JavaScript/04-functions/Or/HomeWork/1/dist/script.dev.js
@@ -0,0 +1,9 @@
+"use strict";
+
+function numbers(numberA, numberB) {
+  if (numberA > numberB) return numberA;else return numberB;
+} //choose 2 numbers
+
+
+var x = numbers(5, 8);
+console.log(x);
