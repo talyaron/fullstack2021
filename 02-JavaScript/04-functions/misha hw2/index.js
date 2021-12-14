dc5015a3eae33861e81b043aa8f254226a8878a0
Michael Dubovyk <93338459+mishalon@users.noreply.github.com>
2021-12-14 23:38:13 +0200

    1

---
+++ 02-JavaScript/04-functions/misha hw2/index.js
@@ -0,0 +1,58 @@
+// --- 3 --- //
+
+// let yearborn = window.prompt("Enter the year you were born");
+// let yearnow = 2021;
+
+// function age(yearnow, yearborn){
+
+//     return yearnow - yearborn;
+
+// }
+
+// let yourage = age(yearnow, yearborn);
+
+// alert("you are "+ yourage + " years old");
+
+
+// --- 4 --- //
+
+let num = window.prompt("enter a number to check if its a prime number");
+
+primecheck();
+
+function primecheck(num){
+
+    if(num<=1){
+       return false;
+    }
+
+     else if (num === 2){
+         return true;
+    }
+    
+    else{
+        
+        for (i=3; i<num; i++){
+            if(num % i == 0){
+                return false;
+            }
+            else{
+                return true;
+            }
+            
+        }
+    }
+}
+
+if(primecheck(num) == false){
+alert(num + " is not prime");
+}
+
+else if(primecheck(num) == true){
+alert(num + " is prime");
+}
+
+
+
+
+
