7daa2402f66de3f2003acada02a803275eb7df10
noachasha <noachasha@gmail.com>
2021-12-15 21:31:01 +0200

    Update index.js

--- 02-JavaScript/04-functions/noa/index.js
+++ 02-JavaScript/04-functions/noa/index.js
@@ -1,20 +1,34 @@
 
-function bmi(height, weight){
-    return weight/(height*height)
+function bmi(height, weight) {
+    return weight / (height * height)
 }
-q=bmi(1.59,52)
+q = bmi(1.59, 52)
 
-q=bmi(1.8,90)
+q = bmi(1.8, 90)
 
 
-if(q>18 && q<=25){
-console.log(q,+' תקין')
-}
-
-else if(q<=18){
-    console.log(q+'תת משקל')
+if (q > 18 && q <= 25) {
+    console.log(q, +' תקין')
 }
 
-if(q>25){
-console.log(`${q} עודף משקל`)
+else if (q <= 18) {
+    console.log(q + 'תת משקל')
 }
+
+if (q > 25) {
+    console.log(`${q} עודף משקל`)
+}
+
+
+
+function calcIntrest(loan, precentage, years) {
+    let sum = loan;
+    for (i = o; i <= year; i++) {
+        sum = loan + ((precentage / 100) * loan)
+    }
+    return sum
+}
+
+let x = calcIntrest(100, 5, 10);
+console.log(x);
+
