f490565bef5d7f270f6c96cd36a044b1efdf5f4a
Wiltemes <wiltemes1@gmail.com>
2021-12-15 10:49:23 +0200

    HwWIL

--- 02-JavaScript/01-variables/example/wil/index.js
+++ 02-JavaScript/01-variables/example/wil/index.js
@@ -1,15 +1,2 @@
 
-var x=6;
-var y=20;
 
-x=y+10;
-
-console.log(x)
-console.log(y)
-console.log(x+y)
-
-for(let i=5; i<25; i+=5){
-
-console.log (i)
-
-}
---
+++ 02-JavaScript/03-conditions/wil/index.js
@@ -0,0 +1,1 @@
+function calcIntrest(loan,percentage,years)
