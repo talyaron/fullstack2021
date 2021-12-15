0d20661b74c85d946d8e07ef4eb134e55bfeb6cc
Michael Dubovyk <93338459+mishalon@users.noreply.github.com>
2021-12-15 11:00:12 +0200

    1

---
+++ 02-JavaScript/04-functions/misha homework/index.js
@@ -0,0 +1,71 @@
+// PLEASE UNCOMMENT THE RIGHT SECTION TO ACTIVATE CODE //
+
+// --- 1 --- //
+
+// let a = window.prompt("what is the first number?")
+// let b = window.prompt("what is the second number?")
+
+// function largest(a,b){
+
+// if(a>b){
+//     return a;
+// }
+// else{
+//     return b;
+// }
+// }
+
+// alert("largest number is "+ largest(a,b));
+
+
+// ------------------------------------------------- //
+
+
+/// --- 2 --- ///
+
+// let a = window.prompt("enter a number between 1-12 to get the name of the month");
+
+// let months = [".", "Januray", "Fabruary", "March", "April",
+//  "May", "June", "July", "August", 
+//  "September", "October", "November", "December"]
+
+// function monthName(a){
+
+//     let b = months[a];
+//     return b;
+
+//  }
+
+// if(a<1 || a>12){
+//     alert("No such month number, please try again!")
+// }
+
+// else{
+
+// alert("month name: "+monthName(a));
+
+// }
+
+// ------------------------------------------------- //
+
+/// --- 3 --- ///
+
+// let a = window.prompt("Please, enter your age");
+
+let year = date().getFullYear();
+
+let date =  new Date().getFullYear();
+console.log(date);
+
+function yourage(a){
+
+
+}
+
+
+
+
+
+
+
+
