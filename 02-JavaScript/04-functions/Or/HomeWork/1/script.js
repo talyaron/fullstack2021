62d6b1d86dd4094c5392ea10aa5557327539c35e
Katya Rukosuev <72752681+RuKatya@users.noreply.github.com>
2021-12-15 12:14:44 +0200

    return HW

--- 02-JavaScript/04-functions/Or/HomeWork/1/script.js
+++ 02-JavaScript/04-functions/Or/HomeWork/1/script.js
@@ -4,5 +4,8 @@ function numbers(numberA, numberB) {
     else return numberB
 }
 //choose 2 numbers
-let x = numbers (9,15);
+let x = numbers(20, 15);
 console.log(x);
+
+// What about if you get numberB bigger than numberA.
+// Also import not forget about equal numbers, (if (numberA==numberB))
