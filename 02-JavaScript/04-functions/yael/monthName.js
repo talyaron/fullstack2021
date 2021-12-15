867f01513734e381db5141045429f2aca8aeb146
yael <yaeledr@gmail.com>
2021-12-15 09:35:18 +0200

    Js_junctions

---
+++ 02-JavaScript/04-functions/yael/monthName.js
@@ -0,0 +1,48 @@
+function monthName(){
+    let month;
+    let month_num=prompt("enter a number between 1-12");
+    
+    switch (month_num) {
+        case '1':
+          month = "January";
+          break;
+        case '2':
+           month = "February";
+          break;
+        case '3':
+            month = "March";
+          break;
+        case '4':
+            month = "April";
+          break;
+        case '5':
+            month = "May";
+          break;
+        case '6':
+            month = "June";
+          break;
+        case '7':
+            month = "July";    
+          break;
+        case '8':
+            month = "August";
+          break;
+        case '9':
+            month = "September";
+            break;
+        case '10':
+            month = "October";
+            break;
+        case '11':
+            month = "November";
+            break;
+        case '12':
+            month = "December";
+            break;
+        default:
+            console.log("non valid number");
+    }
+    console.log(month);
+    }
+    
+    monthName();
