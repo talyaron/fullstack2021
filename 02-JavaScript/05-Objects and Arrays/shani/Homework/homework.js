24a0ecdd09fdcdf02ca98931eae736e36e6b32fc
ShaniRom <93940556+ShaniRom@users.noreply.github.com>
2021-12-18 18:52:24 +0200

    shani js homework

---
+++ 02-JavaScript/05-Objects and Arrays/shani/Homework/homework.js
@@ -0,0 +1,52 @@
+const tamar = {
+    name: 'Tamar',
+    lastName: 'Rom',
+    my:'Mother',
+    age: 50,
+    hobbies: 'Sports and Art ' ,
+    friends: ['Betty','Orit' ,'and Smadar']
+
+}
+
+const rotem = {
+    name: 'Rotem',
+    lastName:'Rom',
+    my:'Sister',
+    age: 19,
+    hobbies: 'Drawing, Jewelry making and Music',
+    friends: ['Rosa',' and Rachel']
+}
+
+const keren = {
+    name: 'Keren',
+    lastName:'Rom',
+    my:'Sister',
+    age: 19,
+    hobbies: 'Drawing, Jewelry making and Sewing',
+    friends: ['Alice', 'and Ofir']
+
+}
+
+const family = [tamar ,rotem ,keren]; 
+
+for(let i= 0; i < family.length; i++ ) {
+
+console.log(family[i].name)
+ 
+for(let q=0; q<family[i].friends.length; q++)
+
+console.log(`${family[i].name}'s friends are ${family[i].friends}.`)
+}
+ 
+
+ 
+function myFamily(member){
+    return (`${member.name} ${member.lastName} is my ${member.my},
+        is ${member.age} and hobbies are ${member.hobbies}.
+        ${member.name}'s friends are ${member.friends}. `)
+
+}
+
+console.log(myFamily(tamar));
+console.log(myFamily(rotem));
+console.log(myFamily(keren));
