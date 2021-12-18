66b3674ba4d2623bb2fa1f6a8ec56504f13ecaf4
Michael Dubovyk <93338459+mishalon@users.noreply.github.com>
2021-12-18 18:04:09 +0200

    1

--- 02-JavaScript/05-Objects and Arrays/mishadu/dist/index.dev.js
+++ 02-JavaScript/05-Objects and Arrays/mishadu/dist/index.dev.js
@@ -1,1 +1,17 @@
 "use strict";
+
+var ohad = {
+  name: 'Ohad',
+  lastName: 'Gur',
+  math: 20,
+  history: 30,
+  physics: 48,
+  english: 80,
+  sports: 100
+};
+
+function studentmath(student) {
+  console.log("".concat(student.name, " ").concat(student.lastName, " got ").concat(student.math, " at math"));
+}
+
+studentmath(ohad);
