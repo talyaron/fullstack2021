20232b8f0de35370987851e3609771426cd1e300
Rasheed <rasheedj966@gmail.com>
2021-12-20 18:44:52 +0200

    Update index.js

--- 02-JavaScript/04-functions/functions-js-rasheed/3-kindsoffunctions/index.js
+++ 02-JavaScript/04-functions/functions-js-rasheed/3-kindsoffunctions/index.js
@@ -6,5 +6,18 @@ return (a*b);
 }
 console.log(sum(a,b))
 
+console.log("-------------------")
+
+const sum2 = function(sum2){
+    return(a*b);
+}
+console.log(sum2(a,b))
+
+console.log("----------------")
 
 
+const sum3 = (a,b)=>{
+    return(a*b);
+}
+
+console.log(sum3(a,b))
