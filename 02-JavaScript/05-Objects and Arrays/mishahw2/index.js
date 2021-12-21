07518a5eaf637e89f2fa26a9aa90be20b95f6908
Michael Dubovyk <93338459+mishalon@users.noreply.github.com>
2021-12-21 22:17:03 +0200

    1

--- 02-JavaScript/05-Objects and Arrays/mishahw2/index.js
+++ 02-JavaScript/05-Objects and Arrays/mishahw2/index.js
@@ -1,10 +1,17 @@
 const movies = [
-    {name: 'spiderman',director: 'Sam Raimi', rank: 4, year: 2012},
-    {name: 'matrix',director: 'Wachowskis', rank: 5, year: 1999},
-    {name: 'ankanto',director: 'Paul Anka', rank: 1, year: 2021},
-    {name: 'perfect strangers',director: 'Paolo Genovese', rank: 2, year: 2018},
-    {name: 'adams family',director: 'Barry Sonnenfeld', rank: 3, year: 2010}
+    {name: 'spiderman', director: 'Sam Raimi', rank: 9, year: 2012},
+    {name: 'matrix', director: 'Wachowskis', rank: 8.3, year: 1999},
+    {name: 'ankanto', director: 'Paul Anka', rank: 9.2, year: 2021},
+    {name: 'perfect strangers', director: 'Paolo Genovese', rank: 7.5, year: 2018},
+    {name: 'adams family', director: 'Barry Sonnenfeld', rank: 9.4, year: 2010},
+    {name: 'the dark knight', director: 'Christopher Nolan', rank: 8.7, year: 2010},
 ]
 
-const byrank = movies.sort(movies.rank);
-console.log(byrank);
+movies.sort((a,b) => {
+    return b.rank - a.rank;
+});
+
+for(let i in movies){
+    console.log(`${movies[i].name} - ${movies[i].rank}`);
+};
+
