ab20fffbd54836be18dcedf31841eb85034ca13a
shayfoyer <93338189+shayfoyer@users.noreply.github.com>
2021-12-21 21:40:22 +0200

    shay

---
+++ 02-JavaScript/05-Objects and Arrays/shay_movies_hw/index.js
@@ -0,0 +1,37 @@
+const movies=[
+
+{movieName:'The intouchables', movieDirector:'Olivier Nakach and eÉric Toledano', movieRating: 8.5, movieYearReleas: 2011
+},
+{movieName:'Spider-Man: No Way Home', movieDirector:'Jon Watts', movieRating: 9, movieYearReleas: 2021
+},
+{movieName:'Avengers infinity war', movieDirector:'Anthony and Joe Russo', movieRating: 8.4, movieYearReleas: 2018
+},
+{movieName:'The dark knight rises', movieDirector:'Christopher Nolan', movieRating: 8.4, movieYearReleas: 2012
+},
+{movieName:'Avenger endgame', movieDirector:'Anthony and Joe Russo', movieRating: 8.4, movieYearReleas: 2019
+},
+{movieName:'Monster, inc.', movieDirector:'Pete Docter and David Silverman andLee Unkrich', movieRating: 8.1, movieYearReleas: 2001
+}
+]
+ 
+//sort movies by rate// 
+const movieRate = movies.sort((a,b) => (a.movieRating-b.movieRating));
+console.log(movieRate);
+ 
+//sort movies by year//
+//  const movieYear = movies.sort((i, j)=>(i.movieYearReleas-j.movieYearReleas));
+// console.log(movieYear);
+
+//find the name of the movie and add a number to the rating
+
+
+const changeRate =movies.filter(name =>name.movieName=='Spider-Man: No Way Home');
+
+console.log(changeRate);
+
+function newMovieRate(title,rank){
+    title = changeRate.movieName[1];
+    rank= 2+ changeRate.movieRating[1];
+    return (movies[1], movies[1].movieName);
+}
+console.log(newMovieRate(changeRate));
