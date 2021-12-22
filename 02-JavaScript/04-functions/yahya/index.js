79afd5bddf41d9b8f6e0e69cd9c0402c5991e854
Yahya123127 <93523876+Yahya123127@users.noreply.github.com>
2021-12-22 15:05:18 +0200

    yahia-home work

---
+++ 02-JavaScript/04-functions/yahya/index.js
@@ -0,0 +1,69 @@
+//----------1------------//
+/*
+function bignum(x,y){
+    if (x>y) 
+        return x;
+    else    
+        return y;
+}
+
+
+
+//------------2---------//
+console.log(month(Number));
+
+function month(m){
+    if (m==1)
+        return "January";
+    else if (m==2)
+        return "February";
+    else if (m==3)
+        return "March";
+    else if (m==4)
+        return "April";
+    else if (m==5)
+        return "May";
+    else if (m==6)
+        return "June";
+    else if (m==7)
+        return "July";
+    else if (m==8)
+        return "August";
+    else if (m==9)
+        return "September";
+    else if (m==10)
+        return "October";
+    else if (m==11)
+        return "November";
+    else if (m==12)
+        return "December";
+}
+
+//------------3----------------// 
+
+function age(thisYear, bornYear){
+    return thisYear-bornYear;
+}
+
+let q = age(2000,2002);
+
+console.log(`Age is ${q}`);
+
+*/
+//------------4----------------// 
+
+//???????????????// 
+//////////////////////////////////////////////////////////////////////////
+const footballTeam = [
+   {teamName:'machbi beer sheva ', place:16},
+   {teamName:'machbi beer sheva ',place:7},
+   {teamName:'machbi beer sheva ',place:5},
+   {teamName:'machbi beer sheva ',place:6}
+
+];
+
+footballTeam.forEach(team=> {
+    if(team.place==7){
+    console.log(footballTeam.indexOf(team));
+}
+})
