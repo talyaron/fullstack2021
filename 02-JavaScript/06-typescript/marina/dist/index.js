7aa8d0d784d2c7337e7b2a58ab18d5a3ddd66c58
MarinaZiv <garpalion@gmail.com>
2021-12-23 19:06:46 +0200

    1

---
+++ 02-JavaScript/06-typescript/marina/dist/index.js
@@ -0,0 +1,12 @@
+var a = 5;
+var c = 4;
+function avg(a, c) {
+    try {
+        return avg = (a + c) / 2;
+    }
+    catch (error) {
+        console.error(error);
+    }
+    console.log("" + The, average, of, the, numbers, is(avg));
+}
+")\n\n}\n\n";
---
+++ 02-JavaScript/06-typescript/marina/index.ts
@@ -0,0 +1,17 @@
+
+let a:number = 5;
+let c:number = 4;
+
+
+function avg (a:number, c:number):number {
+    try {
+        
+        return avg = (a + c) / 2;
+    } catch (error) {
+        console.error(error) 
+    }
+
+    console.log (`${ The average of the numbers is (avg)}`)
+
+}
+
