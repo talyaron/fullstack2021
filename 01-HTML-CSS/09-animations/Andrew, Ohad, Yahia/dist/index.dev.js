1a1cd6039cbd942a30cc06deafc60e764b68c8c3
Yahya123127 <93523876+Yahya123127@users.noreply.github.com>
2021-12-26 20:16:45 +0200

    exchange

--- 01-HTML-CSS/09-animations/Andrew, Ohad, Yahia/dist/index.dev.js
+++ 01-HTML-CSS/09-animations/Andrew, Ohad, Yahia/dist/index.dev.js
@@ -4,30 +4,19 @@ var data = 0;
 document.getElementById("root").innerText = data;
 
 function decrement() {
-<<<<<<< Updated upstream
   data = data - 1;
-=======
-  data = data - 0.6;
->>>>>>> Stashed changes
   document.getElementById("root").innerText = data;
 }
 
 function increment() {
-<<<<<<< Updated upstream
   data = data + 1;
   document.getElementById("root").innerText = data;
 } ///////////////////////////////////////////////////////////////////
 
-=======
-  data = data + 0.6;
-  document.getElementById("root").innerText = data;
-}
->>>>>>> Stashed changes
 
 var data = 0;
 document.getElementById("num").innerText = data;
 
-<<<<<<< Updated upstream
 function decrement2() {
   data = data - 0.1;
   document.getElementById("num").innerText = data;
@@ -35,14 +24,5 @@ function decrement2() {
 
 function increment2() {
   data = data + 0.1;
-=======
-function decrem() {
-  data = data - 1;
-  document.getElementById("num").innerText = data;
-}
-
-function increaz() {
-  data = data + 1;
->>>>>>> Stashed changes
   document.getElementById("num").innerText = data;
 }
