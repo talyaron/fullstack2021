7d2e793f8b60f1dbd990f948977926ffbdd43ba0
ShaniRom <93940556+ShaniRom@users.noreply.github.com>
2021-12-27 20:28:40 +0200

    js type script dom

--- 02-JavaScript/06-typescript/Example/shani/dist/index.js
+++ 02-JavaScript/06-typescript/Example/shani/dist/index.js
@@ -1,13 +1,19 @@
-var m = 24;
-var w = 22;
-function average(a, b) {
-    try {
-        var avg = (a + b) / 2;
-        console.log("The average of m & w is " + avg);
-    }
-    catch (error) {
-        console.error(error);
-        return 0;
-    }
+// let m:number=24
+// let w:number=22
+// function average( a:number ,b:number ):number {
+//     try{
+//         const avg = (a+b)/2;
+//         console.log(`The average of m & w is ${avg}`);
+//     }
+//     catch(error){
+//        console.error(error);
+//         return 0;
+//     }
+// }
+// console.log(average(m,w))
+//console.dir(navigator);
+var btn = document.querySelector('#btn');
+function display() {
+    alert('It was clicked!');
 }
-console.log(average(m, w));
+btn.addEventListener('click', display);
