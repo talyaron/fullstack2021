2de17b5c3440ec042db2d553381790efa79329c1
noachasha <noachasha@gmail.com>
2021-12-28 21:40:39 +0200

    zx

--- 02-JavaScript/07-DOM manipulations/noa/index.ts
+++ 02-JavaScript/07-DOM manipulations/noa/index.ts
@@ -6,6 +6,9 @@ circle1.style.backgroundColor='red';
 circle1.style.width='50px';
 circle1.style.height='50px';
 
+setTimeout(()=>{circle1.style.transform='rotate(200deg)', circle1.style.fontSize='30px'},3000);
+
+
 const plus= document.getElementById('text');
 plus.style.padding='37%';
 
@@ -14,6 +17,5 @@ box.forEach((elm:any)=>{
 elm.style.width='50px';
 elm.style.height='30px';
 elm.style.border='thick solid green';
-
-
 })
+
