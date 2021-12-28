073d1ced5ffb87de67b4a98ac58206f861800a1b
OmriAharonov <omri832@gmail.com>
2021-12-28 23:17:43 +0200

    DOM Omri

--- 02-JavaScript/07-DOM manipulations/OmriAharonov/homeWork.ts
+++ 02-JavaScript/07-DOM manipulations/OmriAharonov/homeWork.ts
@@ -1,2 +1,15 @@
+const box:any = document.querySelector('.box')
+box.style.width = '400px';
+box.style.height = '600px';
+box.style.background = 'yellow';
+box.style.margin = 'auto';
+
 const header:any = document.querySelector('#header');
-header.style.color = 'blue';
+header.style.color = 'orange';
+header.style.margin = '10px 150px'
+
+const img:any = document.querySelector('.img')
+img.style.margin = '0px 100px'
+
+const text:any = document.querySelector('.text')
+text.style.margin = '5px 100px'
