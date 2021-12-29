8352b8927f0bc1cf3bcfd1669fdd8b9405bbdc10
Katya Rukosuev <72752681+RuKatya@users.noreply.github.com>
2021-12-29 15:52:51 +0200

    running text

--- 02-JavaScript/07-DOM manipulations/mishaHw/running text/index.js
+++ 02-JavaScript/07-DOM manipulations/mishaHw/running text/index.js
@@ -1,11 +1,27 @@
-let text = `Michael Dubovik.....`
+let el = document.getElementById('text').innerHTML
+console.log(el)
 
 i = 0;
 function type() {
     i++;
-    if (i <= text.length)
-        document.getElementById("text").innerHTML = text.substr(0, i);
+    if (i <= el.length)
+        document.getElementById("text").innerHTML = el.substr(0, i);
 
-    setTimeout(type, 50);
+    setTimeout(type, 100);
 }
 type();
+
+let el2 = document.getElementById('lorem-text').innerHTML
+console.log(el2)
+
+y = 0;
+function type2() {
+    y++;
+    if (y <= el2.length)
+        document.getElementById("lorem-text").innerHTML = el2.substr(0, y);
+
+    setTimeout(type2, 100)
+}
+type2();
+
+
