61ad09ac4bde02dc52fbf3e5e9d4e612a55acccb
EtanHey <etan@heyman.net>
2021-12-28 18:23:08 -0600

    .

--- 02-JavaScript/07-DOM manipulations/Etan HW/dist/index.js
+++ 02-JavaScript/07-DOM manipulations/Etan HW/dist/index.js
@@ -1,9 +1,61 @@
 var welcome = document.querySelector(".hello");
+var world = document.querySelector('#world');
+//const time was taken down along with the stylings because of loading order
 var here = document.querySelector("#man");
 var goodBye = document.querySelector(".bye");
 var unwelcome = document.getElementById("everyone");
+console.log(unwelcome);
+// time projected message and background:
+var getBody = document.body;
+var newElement = document.createElement('h1');
+var date = new Date();
+var currentHour = date.getHours();
+var changeBack;
+var createTxtMsg;
+if (currentHour >= 4 && currentHour < 10) {
+    createTxtMsg = 'Good Morning!';
+}
+else if (currentHour >= 10 && currentHour < 12) {
+    createTxtMsg = 'Good Day!';
+}
+else if (currentHour >= 12 && currentHour < 18) {
+    createTxtMsg = 'Good Afternoon!';
+}
+else if (currentHour >= 18 && currentHour < 22) {
+    createTxtMsg = 'Good Evening!';
+}
+else if (currentHour >= 22 && currentHour < 4) {
+    createTxtMsg = 'Good Night!';
+}
+else {
+    createTxtMsg = 'Are you from mars? ;)';
+}
+var createEleTxt = document.createTextNode(createTxtMsg);
+newElement.appendChild(createEleTxt);
+welcome.appendChild(newElement);
+newElement.setAttribute('id', 'time');
+function changeColor() {
+    //rgb
+    console.log('event fired');
+    var colors = [0, 0, 0];
+    console.log(currentHour);
+    //Will get a valid rgb color
+    var color = (255 / 24 * currentHour);
+    console.log(color);
+    for (var i = 0; i < colors.length; i++) {
+        colors[i] = color;
+    }
+    getBody.style.background = "rgb(" + colors[0] + "," + colors[1] + "," + colors[2] + ")";
+}
+changeColor();
+var time = document.querySelector('#time');
 var changes = function () {
-    welcome.style.fontSize = welcome.style.fontSize + 4 + "rem";
+    welcome.style.fontSize = welcome.style.fontSize + 2 + "rem";
+    welcome.style.display = 'flex';
+    welcome.style.justifyContent = 'space-around';
+    welcome.style.alignItems = 'baseline';
+    welcome.style.color = '#FAEBD7';
+    time.style.cssText = 'text-align: center; font-size: 60px; font-family: arial';
     here.style.color = "red";
     goodBye.style.padding = "20vh";
     goodBye.style.fontSize = "4rem";
@@ -12,9 +64,11 @@ var changes = function () {
 };
 changes();
 function fadeIn() {
-    unwelcome.style.position = 'relative';
-    unwelcome.style.width = '100vw';
+    unwelcome.style.position = 'absolute';
+    unwelcome.style.width = 'fit-content';
     unwelcome.style.display = 'block';
+    unwelcome.style.right = 0;
+    unwelcome.style.bottom = 0;
     unwelcome.style.fontSize = '5rem';
 }
 setTimeout(fadeIn, 1000);
@@ -26,13 +80,3 @@ function fadeOut() {
     unwelcome.style.display = 'none';
 }
 setTimeout(fadeOut, 2000);
-console.log(unwelcome);
-// function fadeOut() {
-//     opacity--;
-//     unwelcome.style.opacity = opacity/100;
-//     unwelcome.style.display = display/100;
-//     if (opacity > 0){
-//         setTimeout(fadeOut,speed);
-//     }
-// }
-// fadeOut();
--- 02-JavaScript/07-DOM manipulations/Etan HW/index.ts
+++ 02-JavaScript/07-DOM manipulations/Etan HW/index.ts
@@ -1,10 +1,77 @@
 const welcome:any = document.querySelector(".hello");
+const world:any = document.querySelector('#world')
+//const time was taken down along with the stylings because of loading order
 const here:any = document.querySelector("#man");
 const goodBye:any = document.querySelector(".bye");
 const unwelcome:any = document.getElementById("everyone");
 
+
+
+
+
+console.log(unwelcome)
+
+// time projected message and background:
+let getBody:any= document.body;
+
+let newElement = document.createElement('h1');
+
+let date = new Date();
+let currentHour = date.getHours();
+
+
+let changeBack;
+let createTxtMsg;
+if (currentHour >= 4 && currentHour < 10){
+createTxtMsg = 'Good Morning!';
+
+} else if (currentHour >= 10 && currentHour < 12){
+    createTxtMsg = 'Good Day!';
+} else if (currentHour >= 12 && currentHour < 18){
+    createTxtMsg = 'Good Afternoon!';
+}else if (currentHour >= 18 && currentHour < 22){
+    createTxtMsg = 'Good Evening!';
+}else if (currentHour >= 22 && currentHour < 4){
+    createTxtMsg = 'Good Night!';
+} else {
+    createTxtMsg ='Are you from mars? ;)'
+}
+let createEleTxt = document.createTextNode(createTxtMsg)
+
+newElement.appendChild(createEleTxt);
+
+welcome.appendChild(newElement);
+
+newElement.setAttribute('id', 'time')
+
+
+function changeColor() {
+//rgb
+console.log('event fired');
+
+let colors:any = [0, 0, 0];
+
+
+
+console.log(currentHour)
+
+//Will get a valid rgb color
+let color = (255/24*currentHour);
+console.log(color);
+for(let i = 0; i < colors.length; i++) {
+colors[i] = color;
+}
+getBody.style.background = "rgb("+colors[0] + "," + colors[1] + "," + colors[2] +")";
+}
+changeColor()
+const time:any = document.querySelector('#time')
 let changes = function () {
-    welcome.style.fontSize = welcome.style.fontSize + 4 + "rem";
+    welcome.style.fontSize = welcome.style.fontSize + 2 + "rem";
+    welcome.style.display = 'flex';
+    welcome.style.justifyContent = 'space-around';
+    welcome.style.alignItems = 'baseline';
+    welcome.style.color = '#FAEBD7';
+    time.style.cssText = 'text-align: center; font-size: 60px; font-family: arial'
     here.style.color = "red";
     goodBye.style.padding = "20vh";
     goodBye.style.fontSize = "4rem";
@@ -17,10 +84,11 @@ changes();
 
 
 function fadeIn(){
-    unwelcome.style.position = 'relative';
-    unwelcome.style.width = '100vw'
+    unwelcome.style.position = 'absolute';
+    unwelcome.style.width = 'fit-content'
     unwelcome.style.display = 'block';
-    
+    unwelcome.style.right = 0
+    unwelcome.style.bottom = 0
     unwelcome.style.fontSize ='5rem'
 }
 setTimeout(fadeIn,1000);
@@ -41,18 +109,3 @@ function fadeOut(){
     
 }
 setTimeout(fadeOut, 2000);
-
-
-
-console.log(unwelcome)
-
-
-// function fadeOut() {
-//     opacity--;
-//     unwelcome.style.opacity = opacity/100;
-//     unwelcome.style.display = display/100;
-//     if (opacity > 0){
-//         setTimeout(fadeOut,speed);
-//     }
-// }
-// fadeOut();
