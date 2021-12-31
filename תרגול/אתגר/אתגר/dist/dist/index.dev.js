bf62f822b7a7ed9fe708711193451041eaa8e482
Michael-Frankel <76725406+Michael-Frankel@users.noreply.github.com>
2021-12-31 02:20:12 +0200

    Michael F

---
+++ תרגול/אתגר/אתגר/dist/dist/index.dev.js
@@ -0,0 +1,20 @@
+"use strict";
+
+//variables for input & storage
+var textStorage = [];
+var theInput = document.querySelector('.form');
+var textDemo = document.querySelector('.demoText');
+var textEvent = document.querySelectorAll('.input');
+textEvent.forEach(function (elem) {
+  elem.addEventListener('keyup', function (event) {
+    textStorage = event.target.value;
+    console.dir(event);
+  });
+}); // textEvent.addEventListener('keyup', handleText);
+
+theInput.addEventListener('submit', handleInput);
+
+function handleInput(ev) {
+  console.dir(ev);
+  textDemo.innerHTML = textStorage;
+}
