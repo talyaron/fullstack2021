9ae099235786229768066583d1844ae1c49173a8
AndrewLishafay <andrew567234@gmail.com>
2022-01-03 17:54:33 +0200

    123

--- 02-JavaScript/07-DOM manipulations/Andrew/dist/index.js
+++ 02-JavaScript/07-DOM manipulations/Andrew/dist/index.js
@@ -1,10 +1,12 @@
 var board = document.querySelector('#board');
-board.style.width = '650px';
-board.style.height = '650px';
-board.style.background = 'brown';
-board.style.display = 'flex';
-board.style.justifyContent = 'center';
-board.style.alignItems = 'center';
+// board.style.width = '650px';
+// board.style.height = '650px';
+// board.style.background = 'brown';
+// board.style.display = 'flex';
+// board.style.justifyContent = 'center';
+// board.style.alignItems = 'center';
+board.style.cssText =
+    "\nwidth: 650px;\nheight: 650px;\nbackground: brown;\ndisplay: flex;\njustify-content: center;\nalign-items: center;\n";
 var grid = document.querySelector('#grid');
 grid.style.width = '600px';
 grid.style.height = '600px';
@@ -12,6 +14,15 @@ grid.style.display = 'grid';
 grid.style.gridTemplateRows = 'repeat(8, 1fr)';
 grid.style.gridTemplateColumns = 'repeat(8, 1fr)';
 grid.style.position = 'relative';
+// grid.style.cssText =
+// `
+// width: 600px;
+// height: 600px;
+// display: grid;
+// grid-template-rows: repeat(8, 1fr);
+// grid-template-columns: repeat(8, 1fr);
+// grid.style.position: relative;
+// `
 var cells = document.querySelectorAll('.cell');
 cells.forEach(function (element) {
     element.style.width = '70px';
@@ -27,11 +38,13 @@ blacks.forEach(function (element) {
 });
 var pieces = document.querySelectorAll('img');
 pieces.forEach(function (element) {
-    element.style.width = '65px';
-    element.style.height = '65px';
-    element.style.position = 'absolute';
-    element.style.filter = 'drop-shadow(0px 0px 8px white)';
-    element.style.transition = 'ease 0.4s';
+    // element.style.width = '65px';
+    // element.style.height = '65px';
+    // element.style.position = 'absolute';
+    // element.style.filter = 'drop-shadow(0px 0px 8px white)';
+    // element.style.transition = 'ease 0.4s';
+    element.style.cssText =
+        "\n    width: 65px;\n    height: 65px;\n    position: absolute;\n    filter: drop-shadow(0px 0px 8px white);\n    transition: ease 0.4s;\n    ";
 });
 var kingBlack = document.querySelector('#kingB');
 var kingWhite = document.querySelector('#kingW');
@@ -55,11 +68,11 @@ pawnWhite.style.top = '380px';
 pawnWhite.style.left = '75px';
 var animationTime = 700;
 var animationDelay = 3000;
-var timeout1st = setTimeout(firstMove, animationDelay + animationTime);
-var timeout2nd = setTimeout(secondMove, animationDelay + animationTime * 2);
-var timeout3rd = setTimeout(thirdMove, animationDelay + animationTime * 3);
-var timeout4th = setTimeout(fourthMove, animationDelay + animationTime * 4);
-var timeout5th = setTimeout(fifthMove, animationDelay + animationTime * 5);
+var timeout1st = setTimeout(firstMove, animationDelay + animationTime); // 3700ms
+var timeout2nd = setTimeout(secondMove, animationDelay + animationTime * 2); // 4400ms
+var timeout3rd = setTimeout(thirdMove, animationDelay + animationTime * 3); //  5100ms 
+var timeout4th = setTimeout(fourthMove, animationDelay + animationTime * 4); // 5800ms
+var timeout5th = setTimeout(fifthMove, animationDelay + animationTime * 5); //  6500ms
 function firstMove() {
     pawnWhite.style.top = '300px';
 }
