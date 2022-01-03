b1f849244da779d527c65761ed9965df05cc549d
EtanHey <etan@heyman.net>
2022-01-03 10:30:29 -0600

    .

---
+++ 02-JavaScript/07-DOM manipulations/Example/02-events/Etan/index.ts
@@ -0,0 +1,8 @@
+const btn1 = document.querySelector('#first');
+const btn2 = document.querySelector('#second');
+const btn3 = document.querySelector('#third');
+const field = document.querySelector('#field')
+
+function goodBye(event){
+    btn3.textContent = 'Good Bye!'
+}
