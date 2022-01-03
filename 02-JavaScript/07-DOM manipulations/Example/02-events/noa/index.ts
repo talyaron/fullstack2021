f9fc4d54f9efe6500c884f782daf2bd5e51ae5dc
noachasha <noachasha@gmail.com>
2022-01-03 18:37:28 +0200

    up

---
+++ 02-JavaScript/07-DOM manipulations/Example/02-events/noa/index.ts
@@ -0,0 +1,14 @@
+const button1= document.getElementById('button1');
+
+button1.addEventListener('dblclick', ()=>{
+    console.log('double click')
+} )
+button1.addEventListener('wheel', RunButton1);
+
+function RunButton1(){
+    console.log("it's on wheel")
+}
+
+function RunMouseleave(){
+    console.log('mouse leaved')
+}
