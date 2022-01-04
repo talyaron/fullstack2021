204eb330c437b03221332b50ce6970a6108719e9
OmriAharonov <omri832@gmail.com>
2022-01-05 00:02:58 +0200

    Omri event

---
+++ 02-JavaScript/07-DOM manipulations/OmriAharonov/events/homeWork/gallery.ts
@@ -0,0 +1,18 @@
+const box:any = document.querySelector('.box');
+box.style.cssText = 
+`
+width: 90vw;
+height: 500px;
+margin: 50px auto ;
+background: yellow;
+
+`
+
+const gallery:any = document.querySelector('.gallery');
+gallery.style.cssText = 
+`
+display: flex;
+
+
+`
+
