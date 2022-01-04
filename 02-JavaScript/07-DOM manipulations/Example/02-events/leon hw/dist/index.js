90c426ccc2ddb3cb299af0b9b49324e8d81ca0b0
leon <lmalul13@gmail.com>
2022-01-04 10:04:03 +0200

    6

---
+++ 02-JavaScript/07-DOM manipulations/Example/02-events/leon hw/dist/index.js
@@ -0,0 +1,2 @@
+var divs = document.querySelector(".gridimg_box");
+var last = document.querySelector(".gridimg_description");
---
+++ 02-JavaScript/07-DOM manipulations/Example/02-events/leon hw/index.ts
@@ -0,0 +1,5 @@
+const divs=document.querySelector(`.gridimg_box`);
+const last=document.querySelector(`.gridimg_description`)
+
+
+
