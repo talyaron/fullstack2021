cc7ea8ecbad4887d5d1e8c7952349313d4cf043e
leon <lmalul13@gmail.com>
2022-01-04 11:40:01 +0200

    1

--- 02-JavaScript/07-DOM manipulations/Example/02-events/leon hw/dist/index.js
+++
@@ -1,2 +0,0 @@
-var divs = document.querySelector(".gridimg_box");
-var last = document.querySelector(".gridimg_description");
--- 02-JavaScript/07-DOM manipulations/Example/02-events/leon hw/index.ts
+++ 02-JavaScript/07-DOM manipulations/Example/02-events/leon hw/index.ts
@@ -1,5 +1,2 @@
-const divs=document.querySelector(`.gridimg_box`);
-const last=document.querySelector(`.gridimg_description`)
-
 
 
