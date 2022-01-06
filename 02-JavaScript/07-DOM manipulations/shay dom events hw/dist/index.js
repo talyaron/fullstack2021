579b7021353b11fd6a6a1366255d4f693aff6180
shayfoyer <93338189+shayfoyer@users.noreply.github.com>
2022-01-06 17:15:33 +0200

    shay

--- 02-JavaScript/07-DOM manipulations/shay dom events hw/dist/index.js
+++ 02-JavaScript/07-DOM manipulations/shay dom events hw/dist/index.js
@@ -14,37 +14,3 @@ for (var _i = 0, img_1 = img; _i < img_1.length; _i++) {
     var item = img_1[_i];
     _loop_1(item);
 }
-//   switch(text){
-// case betty:
-//   text.style.top = '400px'
-//   text.style.left = '50px'
-//   break;
-//  case selena:
-//   text.style.top = '400px'
-//   text.style.left = '100px'
-//   break;
-//   case tom:
-//     text.style.top = '400px'
-//     text.style.left = '150px'
-//     break;
-//     case zendaya:
-//       text.style.top = '400px'
-//       text.style.left = '200px'
-//       break;
-//   }
-//     if(img == betty){
-//   text.style.top = '300px'
-//   text.style.left = '20px'
-// }
-// if(item.alt == "selena"){
-//   text.style.top = '300px'
-//   text.style.left = '150px'
-// }
-// if(item.alt == "tom"){
-//   text.style.top = '300px'
-//   text.style.left = '200px'
-// }
-// if(item.alt == "zendaya"){
-//   text.style.top = '300px'
-//   text.style.left = '250px'
-// }
