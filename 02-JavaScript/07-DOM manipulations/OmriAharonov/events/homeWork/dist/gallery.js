d55b7c80306152a684738571013f2f298a6a64b1
OmriAharonov <omri832@gmail.com>
2022-01-06 13:43:14 +0200

    event Omri

--- 02-JavaScript/07-DOM manipulations/OmriAharonov/events/homeWork/dist/gallery.js
+++ 02-JavaScript/07-DOM manipulations/OmriAharonov/events/homeWork/dist/gallery.js
@@ -1,6 +1,17 @@
 var box = document.querySelector('.box');
-box.style.cssText =
-    "\nwidth: 90vw;\nheight: 500px;\nmargin: 50px auto ;\nbackground: yellow;\n\n";
+// box.style.cssText = 
+// `
+// width: 90vw;
+// height: 500px;
+// margin: 50px auto ;
+// background: yellow;
+// dispaly: grid;
+// grid-tamplate-row: 80% 20%;
+// grid-tamplate-column: 1fr;
+// `
 var gallery = document.querySelector('.gallery');
-gallery.style.cssText =
-    "\ndisplay: flex;\n\n\n";
+// gallery.style.cssText = 
+// `
+// display: flex;
+// display:block;
+// `
--- 02-JavaScript/07-DOM manipulations/OmriAharonov/events/homeWork/gallery.ts
+++ 02-JavaScript/07-DOM manipulations/OmriAharonov/events/homeWork/gallery.ts
@@ -1,18 +1,23 @@
 const box:any = document.querySelector('.box');
-box.style.cssText = 
-`
-width: 90vw;
-height: 500px;
-margin: 50px auto ;
-background: yellow;
 
-`
+// box.style.cssText = 
+// `
+// width: 90vw;
+// height: 500px;
+// margin: 50px auto ;
+// background: yellow;
+// dispaly: grid;
+// grid-tamplate-row: 80% 20%;
+// grid-tamplate-column: 1fr;
+
+// `
 
 const gallery:any = document.querySelector('.gallery');
-gallery.style.cssText = 
-`
-display: flex;
 
+// gallery.style.cssText = 
+// `
+// display: flex;
+// display:block;
 
-`
+// `
 
