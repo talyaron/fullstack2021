9e72ae1225782fd62162e94b332ea24364e116ba
AndrewLishafay <andrew567234@gmail.com>
2022-01-08 15:19:55 +0200

    Js homework

--- 02-JavaScript/07-DOM manipulations/Andrew/event/dist/index.js
+++ 02-JavaScript/07-DOM manipulations/Andrew/event/dist/index.js
@@ -1,12 +1,20 @@
 var photos = document.querySelectorAll('.content__galley__photo');
-var bigPhoto1 = document.querySelector('.bigPic1');
+var bigPhotos = document.querySelectorAll('.bigPic');
 var items = document.querySelectorAll('.obs');
+var discription = document.querySelector('#galleryDisc');
 photos.forEach(function (photo) {
     photo.addEventListener('mouseenter', handleHoverGallery);
 });
-bigPhoto1.addEventListener('mousemove', handleHoverBig);
+photos.forEach(function (photo) {
+    photo.addEventListener('mouseout', handleOutGallery);
+});
+bigPhotos.forEach(function (bigPhoto) {
+    bigPhoto.addEventListener('mousemove', handleHoverBig);
+});
+function handleOutGallery() {
+    discription.innerHTML = "";
+}
 function handleHoverGallery(ev) {
-    var discription = document.querySelector('#galleryDisc');
     var id = ev.target.id;
     switch (id) {
         case 'pic2':
@@ -20,50 +28,99 @@ function handleHoverGallery(ev) {
             break;
     }
     console.log("id is " + id);
-    var removeText = setTimeout(textEraser, 2000);
-    function textEraser() {
-        discription.innerHTML = "";
-    }
 }
 function handleHoverBig(ev) {
-    var discription = document.querySelector('#bigPic1Disc');
+    var discription1 = document.querySelector('#bigPic1Disc');
+    var discription2 = document.querySelector('#bigPic2Disc');
     var boundaries = ev.target.getBoundingClientRect();
+    var id = ev.target.id;
     var curserLocation = {
         x: boundaries.width / (ev.clientX - boundaries.left),
         y: boundaries.height / (ev.clientY - boundaries.top)
     };
     console.log("left: " + curserLocation.x + " top " + curserLocation.y);
-    if (curserLocation.x > 4.15 &&
-        curserLocation.x < 38 &&
-        curserLocation.y > 1.58 &&
-        curserLocation.y < 4.52) {
-        discription.innerHTML = "This is me and my awsome mustache";
-    }
-    else if (curserLocation.x > 2.27 &&
-        curserLocation.x < 3.27 &&
-        curserLocation.y > 1.8 &&
-        curserLocation.y < 3.8) {
-        discription.innerHTML = "This is my friend Moran";
-    }
-    else if (curserLocation.x > 1.46 &&
-        curserLocation.x < 1.73 &&
-        curserLocation.y > 2.08 &&
-        curserLocation.y < 3.4) {
-        discription.innerHTML = "This is my friend Ben";
-    }
-    else if (curserLocation.x > 1.03 &&
-        curserLocation.x < 1.15 &&
-        curserLocation.y > 1.98 &&
-        curserLocation.y < 3.45) {
-        discription.innerHTML = "This is my friend Yuri";
-    }
-    else {
-        discription.innerHTML = "";
+    switch (id) {
+        case 'big1':
+            if (curserLocation.x > 4.15 &&
+                curserLocation.x < 38 &&
+                curserLocation.y > 1.58 &&
+                curserLocation.y < 4.52) {
+                discription1.innerHTML = "This is me and my awsome mustache";
+            }
+            else if (curserLocation.x > 2.27 &&
+                curserLocation.x < 3.27 &&
+                curserLocation.y > 1.8 &&
+                curserLocation.y < 3.8) {
+                discription1.innerHTML = "This is my friend Ross";
+            }
+            else if (curserLocation.x > 1.46 &&
+                curserLocation.x < 1.73 &&
+                curserLocation.y > 2.08 &&
+                curserLocation.y < 3.4) {
+                discription1.innerHTML = "This is my friend Ben";
+            }
+            else if (curserLocation.x > 1.03 &&
+                curserLocation.x < 1.15 &&
+                curserLocation.y > 1.98 &&
+                curserLocation.y < 3.45) {
+                discription1.innerHTML = "This is my friend Yuri";
+            }
+            else {
+                discription1.innerHTML = "";
+            }
+            break;
+        case 'big2':
+            if (curserLocation.x > 6.6 &&
+                curserLocation.x < 10.2 &&
+                curserLocation.y > 2.65 &&
+                curserLocation.y < 3.6) {
+                discription2.innerHTML = "This is my friend Ben";
+            }
+            else if (curserLocation.x > 4.1 &&
+                curserLocation.x < 4.96 &&
+                curserLocation.y > 2.57 &&
+                curserLocation.y < 3.19) {
+                discription2.innerHTML = "This is my Norwegian freind Jani";
+            }
+            else if (curserLocation.x > 2.9 &&
+                curserLocation.x < 3.41 &&
+                curserLocation.y > 2.75 &&
+                curserLocation.y < 3.6) {
+                discription2.innerHTML = "That is me!";
+            }
+            else if (curserLocation.x > 2.08 &&
+                curserLocation.x < 2.26 &&
+                curserLocation.y > 2.55 &&
+                curserLocation.y < 3.14) {
+                discription2.innerHTML = "This is my friend Sapir";
+            }
+            else if (curserLocation.x > 1.58 &&
+                curserLocation.x < 1.72 &&
+                curserLocation.y > 2.57 &&
+                curserLocation.y < 3.35) {
+                discription2.innerHTML = "This is my friend Yuri";
+            }
+            else if (curserLocation.x > 1.28 &&
+                curserLocation.x < 1.37 &&
+                curserLocation.y > 2.4 &&
+                curserLocation.y < 3.3) {
+                discription2.innerHTML = "This is my friend Max";
+            }
+            else if (curserLocation.x > 1.1 &&
+                curserLocation.x < 1.17 &&
+                curserLocation.y > 2.5 &&
+                curserLocation.y < 3.36) {
+                discription2.innerHTML = "This is my friend Ross";
+            }
+            else {
+                discription2.innerHTML = "";
+            }
+            break;
     }
 }
 var obsOptions = {
-    threshold: 0,
-    rootMargin: "-200px"
+    threshold: 0.6,
+    rootMargin: "-50px"
 };
 var observer = new IntersectionObserver(function (items) {
     items.forEach(function (item) {
