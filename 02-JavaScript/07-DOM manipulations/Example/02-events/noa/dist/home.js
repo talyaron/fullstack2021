92fac91f6a798919efa90142d33899c1efb285fa
noachasha <noachasha@gmail.com>
2022-01-09 19:05:16 +0200

    up

--- 02-JavaScript/07-DOM manipulations/Example/02-events/noa/dist/home.js
+++ 02-JavaScript/07-DOM manipulations/Example/02-events/noa/dist/home.js
@@ -1,1 +1,8 @@
 var img = document.querySelectorAll('img');
+console.log('img');
+img.forEach(function (item) {
+    item.addEventListener('mouseenter', function (e) {
+        text.innerHTML = item.alt;
+        console.log('MouseEnter');
+    });
+});
