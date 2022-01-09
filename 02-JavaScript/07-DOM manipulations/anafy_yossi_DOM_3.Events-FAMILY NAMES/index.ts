1a17686d0607440bd2d8299a817f2aa70bd5cc4f
Yossi A <asnafy.junior@gmail.com>
2022-01-09 15:17:58 +0200

    cleaner code with only one picLeave function for all

--- 02-JavaScript/07-DOM manipulations/anafy_yossi_DOM_3.Events-FAMILY NAMES/index.ts
+++ 02-JavaScript/07-DOM manipulations/anafy_yossi_DOM_3.Events-FAMILY NAMES/index.ts
@@ -1,67 +1,58 @@
 
 const personName: any = document.querySelector('#name');
 
+function picLeave() {
+    personName.innerText = '( please hover image )'; personName.style.opacity = '0.5';
+}
 
 document.querySelector('.picOne').addEventListener('mouseenter', picOneEnter);
-document.querySelector('.picOne').addEventListener('mouseleave', picOneLeave);
-
+document.querySelector('.picOne').addEventListener('mouseleave', picLeave);
 function picOneEnter() {
     personName.innerText = 'Bob Asnafy'; personName.style.opacity = '1';
 }
-function picOneLeave() {
-    personName.innerText = '( please hover image )'; personName.style.opacity = '0.5';
-}
+(picLeave: void) => { }
+
 
 document.querySelector('.picTwo').addEventListener('mouseenter', picTwoEnter)
-document.querySelector('.picTwo').addEventListener('mouseleave', picTwoLeave)
+document.querySelector('.picTwo').addEventListener('mouseleave', picLeave)
 function picTwoEnter() {
     personName.innerText = 'Jasmine Asnafy'; personName.style.opacity = '1';
 }
-function picTwoLeave() {
-    personName.innerText = '( please hover image )'; personName.style.opacity = '0.5';
-}
+(picLeave: void) => { }
 
 document.querySelector('.picThree').addEventListener('mouseenter', picThreeEnter)
-document.querySelector('.picThree').addEventListener('mouseleave', picThreeLeave)
+document.querySelector('.picThree').addEventListener('mouseleave', picLeave)
 function picThreeEnter() {
     personName.innerText = 'Quasimodo Notre Dame De Paris Asnafy'; personName.style.opacity = '1';
 }
-function picThreeLeave() {
-    personName.innerText = '( please hover image )'; personName.style.opacity = '0.5';
-}
+(picLeave: void) => { }
 
 document.querySelector('.picFour').addEventListener('mouseenter', picFourEnter)
-document.querySelector('.picFour').addEventListener('mouseleave', picFourLeave)
+document.querySelector('.picFour').addEventListener('mouseleave', picLeave)
 function picFourEnter() {
     personName.innerText = 'Winnie Asnafy'; personName.style.opacity = '1';
 }
-function picFourLeave() {
-    personName.innerText = '( please hover image )'; personName.style.opacity = '0.5';
-}
+(picLeave: void) => { }
 
 document.querySelector('.picFive').addEventListener('mouseenter', picFiveEnter)
-document.querySelector('.picFive').addEventListener('mouseleave', picFiveLeave)
+document.querySelector('.picFive').addEventListener('mouseleave', picLeave)
 function picFiveEnter() {
     personName.innerText = 'Snoopy Asnafy'; personName.style.opacity = '1';
 }
-function picFiveLeave() {
-    personName.innerText = '( please hover image )'; personName.style.opacity = '0.5';
-}
+(picLeave: void) => { }
 
 document.querySelector('.picSix').addEventListener('mouseenter', picSixEnter)
-document.querySelector('.picSix').addEventListener('mouseleave', picSixeLeave)
+document.querySelector('.picSix').addEventListener('mouseleave', picLeave)
 function picSixEnter() {
     personName.innerText = 'Simba Asnafy'; personName.style.opacity = '1';
 }
-function picSixeLeave() {
-    personName.innerText = '( please hover image )'; personName.style.opacity = '0.5';
-}
+(picLeave: void) => { }
 
 document.querySelector('.picSeven').addEventListener('mouseenter', picSevenEnter)
-document.querySelector('.picSeven').addEventListener('mouseleave', picSevenLeave)
+document.querySelector('.picSeven').addEventListener('mouseleave', picLeave)
 function picSevenEnter() {
     personName.innerText = 'Dora Asnafy'; personName.style.opacity = '1';
 }
-function picSevenLeave() {
-    personName.innerText = '( please hover image )'; personName.style.opacity = '0.5';
-}
+(picLeave: void) => { }
+
+
