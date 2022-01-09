9a22db3fc42251252954203bb8e9da958a92f49c
Etanhey <etan@heyman.net>
2022-01-09 10:45:30 -0600

    dono

--- 02-JavaScript/07-DOM manipulations/Example/02-events/Etan Hw/dist/index.js
+++ 02-JavaScript/07-DOM manipulations/Example/02-events/Etan Hw/dist/index.js
@@ -5,7 +5,7 @@ var splitText = strText.split("");
 console.log(splitText);
 text.textContent = "";
 splitText.forEach(function (i) {
-    text.innerHTML += '<span>' + ("" + i) + '</span>';
+    text.innerHTML += "<span>" + ("" + i) + "</span>";
 });
 // for (i = 0; i < splitText.length; i++) {
 //   text.innerHTML += "<span>" + splitText[i] + "</span>";
@@ -26,66 +26,67 @@ function complete() {
     clearInterval(timer);
     timer = null;
 }
-var mom = document.getElementById('mom');
-var dad = document.getElementById('dad');
-var Benji = document.getElementById('Benji');
-var Ning = document.getElementById('Ning');
-var Tamir = document.getElementById('Tamir');
-var Romi = document.getElementById('Romi');
-var Mejohnindavi = document.getElementById('Mejohnindavi');
-var Citron = document.getElementById('Citron');
-var Chen = document.getElementById('Chen');
-mom.addEventListener('mouseenter', function () {
-    text.innerHTML = 'this is my mother.';
+var mom = document.getElementById("mom");
+var dad = document.getElementById("dad");
+var Benji = document.getElementById("Benji");
+var Ning = document.getElementById("Ning");
+var Tamir = document.getElementById("Tamir");
+var Romi = document.getElementById("Romi");
+var Mejohnindavi = document.getElementById("Mejohnindavi");
+var Citron = document.getElementById("Citron");
+var Chen = document.getElementById("Chen");
+mom.addEventListener("mouseenter", function () {
+    text.innerHTML = "this is my mother, Mozhgan.";
 });
-mom.addEventListener('mouseleave', function () {
+mom.addEventListener("mouseleave", function () {
     text.innerHTML = "Who's next?";
 });
-dad.addEventListener('mouseenter', function () {
-    text.innerHTML = 'this is my father.';
+dad.addEventListener("mouseenter", function () {
+    text.innerHTML = "this is my father, David.";
 });
-dad.addEventListener('mouseleave', function () {
-    text.innerHTML = "Who's-next?";
+dad.addEventListener("mouseleave", function () {
+    text.innerHTML = "Who's next?";
 });
-Benji.addEventListener('mouseenter', function () {
-    text.innerHTML = 'this is my half brother, Benjamin (Benji).';
+Benji.addEventListener("mouseenter", function () {
+    text.innerHTML = "this is my half brother, Benjamin (Benji).";
 });
-Benji.addEventListener('mouseleave', function () {
+Benji.addEventListener("mouseleave", function () {
     text.innerHTML = "his mother is to the right";
 });
-Ning.addEventListener('mouseenter', function () {
+Ning.addEventListener("mouseenter", function () {
     text.innerHTML = "this is Ning, she's my fathers wife.";
 });
-Ning.addEventListener('mouseleave', function () {
+Ning.addEventListener("mouseleave", function () {
     text.innerHTML = "Who's next?";
 });
-Tamir.addEventListener('mouseenter', function () {
-    text.innerHTML = 'this is my brother, Tamir.';
+Tamir.addEventListener("mouseenter", function () {
+    text.innerHTML = "this is my brother, Tamir.";
 });
-Tamir.addEventListener('mouseleave', function () {
+Tamir.addEventListener("mouseleave", function () {
     text.innerHTML = "Who's next?";
 });
-Romi.addEventListener('mouseenter', function () {
-    text.innerHTML = "this is my wife, Romina, she's from paraguay";
+Romi.addEventListener("mouseenter", function () {
+    text.innerHTML = "this is my wife, Romina, she's from Paraguay.";
 });
-Romi.addEventListener('mouseleave', function () {
+Romi.addEventListener("mouseleave", function () {
     text.innerHTML = "Who's next?";
 });
-Mejohnindavi.addEventListener('mouseenter', function () {
-    text.innerHTML = 'oh no that picture again? johni and avi are two of my best friends who dont have recent pics';
+Mejohnindavi.addEventListener("mouseenter", function () {
+    text.innerHTML =
+        "oh no that picture again? are two of my best friends who dont have recent pics";
 });
-Mejohnindavi.addEventListener('mouseleave', function () {
+Mejohnindavi.addEventListener("mouseleave", function () {
     text.innerHTML = "Who's next?";
 });
-Citron.addEventListener('mouseenter', function () {
-    text.innerHTML = 'this is my friend citron.';
+Citron.addEventListener("mouseenter", function () {
+    text.innerHTML = "this is my friend Citron.";
 });
-Citron.addEventListener('mouseleave', function () {
+Citron.addEventListener("mouseleave", function () {
     text.innerHTML = "Who's next?";
 });
-Chen.addEventListener('mouseenter', function () {
-    text.innerHTML = 'this is my friend chen';
+Chen.addEventListener("mouseenter", function () {
+    text.innerHTML = "this is my friend Chen";
 });
-Chen.addEventListener('mouseleave', function () {
+Chen.addEventListener("mouseleave", function () {
     text.innerHTML = "Who's next?";
 });
--- 02-JavaScript/07-DOM manipulations/Example/02-events/Etan Hw/index.ts
+++ 02-JavaScript/07-DOM manipulations/Example/02-events/Etan Hw/index.ts
@@ -3,10 +3,10 @@
 const text = document.querySelector("#description");
 const strText = text.textContent;
 const splitText = strText.split("");
-console.log(splitText)
+console.log(splitText);
 text.textContent = "";
-splitText.forEach(i => {
-  text.innerHTML +=  '<span>' + `${i}` + '</span>';
+splitText.forEach((i) => {
+  text.innerHTML += "<span>" + `${i}` + "</span>";
 });
 // for (i = 0; i < splitText.length; i++) {
 //   text.innerHTML += "<span>" + splitText[i] + "</span>";
@@ -30,68 +30,68 @@ function complete() {
   timer = null;
 }
 
+const mom = document.getElementById("mom");
+const dad = document.getElementById("dad");
+const Benji = document.getElementById("Benji");
+const Ning = document.getElementById("Ning");
+const Tamir = document.getElementById("Tamir");
+const Romi = document.getElementById("Romi");
+const Mejohnindavi = document.getElementById("Mejohnindavi");
+const Citron = document.getElementById("Citron");
+const Chen = document.getElementById("Chen");
 
-const mom = document.getElementById('mom')
-const dad = document.getElementById('dad')
-const Benji = document.getElementById('Benji')
-const Ning = document.getElementById('Ning')
-const Tamir = document.getElementById('Tamir')
-const Romi = document.getElementById('Romi')
-const Mejohnindavi = document.getElementById('Mejohnindavi')
-const Citron = document.getElementById('Citron')
-const Chen = document.getElementById('Chen')
-
-mom.addEventListener('mouseenter',() => {
-    text.innerHTML = 'this is my mother.'
-})
-mom.addEventListener('mouseleave',() => {
-    text.innerHTML = "Who's next?"
-})
-dad.addEventListener('mouseenter',() => {
-    text.innerHTML = 'this is my father.'
-})
-dad.addEventListener('mouseleave',() => {
-    text.innerHTML = "Who's-next?"
-})
-Benji.addEventListener('mouseenter',() => {
-    text.innerHTML = 'this is my half brother, Benjamin (Benji).'
-})
-Benji.addEventListener('mouseleave',() => {
-    text.innerHTML = "his mother is to the right"
-})
-Ning.addEventListener('mouseenter',() => {
-    text.innerHTML = "this is Ning, she's my fathers wife."
-})
-Ning.addEventListener('mouseleave',() => {
-    text.innerHTML = "Who's next?"
-})
-Tamir.addEventListener('mouseenter',() => {
-    text.innerHTML = 'this is my brother, Tamir.'
-})
-Tamir.addEventListener('mouseleave',() => {
-    text.innerHTML = "Who's next?"
-})
-Romi.addEventListener('mouseenter',() => {
-    text.innerHTML = "this is my wife, Romina, she's from paraguay"
-})
-Romi.addEventListener('mouseleave',() => {
-    text.innerHTML = "Who's next?"
-})
-Mejohnindavi.addEventListener('mouseenter',() => {
-    text.innerHTML = 'oh no that picture again? johni and avi are two of my best friends who dont have recent pics'
-})
-Mejohnindavi.addEventListener('mouseleave',() => {
-    text.innerHTML = "Who's next?"
-})
-Citron.addEventListener('mouseenter',() => {
-    text.innerHTML = 'this is my friend citron.'
-})
-Citron.addEventListener('mouseleave',() => {
-    text.innerHTML = "Who's next?"
-})
-Chen.addEventListener('mouseenter',() => {
-    text.innerHTML = 'this is my friend chen'
-})
-Chen.addEventListener('mouseleave',() => {
-    text.innerHTML = "Who's next?"
-})
+mom.addEventListener("mouseenter", () => {
+  text.innerHTML = "this is my mother, Mozhgan.";
+});
+mom.addEventListener("mouseleave", () => {
+  text.innerHTML = "Who's next?";
+});
+dad.addEventListener("mouseenter", () => {
+  text.innerHTML = "this is my father, David.";
+});
+dad.addEventListener("mouseleave", () => {
+  text.innerHTML = "Who's next?";
+});
+Benji.addEventListener("mouseenter", () => {
+  text.innerHTML = "this is my half brother, Benjamin (Benji).";
+});
+Benji.addEventListener("mouseleave", () => {
+  text.innerHTML = "his mother is to the right";
+});
+Ning.addEventListener("mouseenter", () => {
+  text.innerHTML = "this is Ning, she's my fathers wife.";
+});
+Ning.addEventListener("mouseleave", () => {
+  text.innerHTML = "Who's next?";
+});
+Tamir.addEventListener("mouseenter", () => {
+  text.innerHTML = "this is my brother, Tamir.";
+});
+Tamir.addEventListener("mouseleave", () => {
+  text.innerHTML = "Who's next?";
+});
+Romi.addEventListener("mouseenter", () => {
+  text.innerHTML = "this is my wife, Romina, she's from Paraguay.";
+});
+Romi.addEventListener("mouseleave", () => {
+  text.innerHTML = "Who's next?";
+});
+Mejohnindavi.addEventListener("mouseenter", () => {
+  text.innerHTML =
+    "oh no that picture again? are two of my best friends who dont have recent pics";
+});
+Mejohnindavi.addEventListener("mouseleave", () => {
+  text.innerHTML = "Who's next?";
+});
+Citron.addEventListener("mouseenter", () => {
+  text.innerHTML = "this is my friend Citron.";
+});
+Citron.addEventListener("mouseleave", () => {
+  text.innerHTML = "Who's next?";
+});
+Chen.addEventListener("mouseenter", () => {
+  text.innerHTML = "this is my friend Chen";
+});
+Chen.addEventListener("mouseleave", () => {
+  text.innerHTML = "Who's next?";
+});
