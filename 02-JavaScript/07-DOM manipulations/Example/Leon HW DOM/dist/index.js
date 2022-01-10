cf5ef730dc1075e5481678c2b99ebbc485ac3137
tamir-dadon <tamir.dadon63@gmail.com>
2022-01-10 18:44:52 +0200

    zz

---
+++ 02-JavaScript/07-DOM manipulations/Example/Leon HW DOM/dist/index.js
@@ -0,0 +1,32 @@
+//  id
+var header = document.querySelector("#header");
+header.style.borderBottom = "solid 2px #ccc";
+header.innerHTML = 'Hello world';
+//button click event
+var button = document.querySelector("#myButton");
+button.addEventListener("click", function (event) {
+    console.log("click!");
+});
+// class
+var container = document.querySelector(".container");
+container.style.color = "blue";
+container.style.fontSize = "40px";
+var box = document.querySelectorAll(".box");
+box.forEach(function (element) {
+    element.style.color = "black";
+    element.style.border = "2px solid black";
+});
+//setTimeout
+var myTimeout = setTimeout(myGreeting, 3000);
+function myGreeting() {
+    document.querySelector("#greet").innerHTML = "Happy Birthday!";
+}
+//Another settimeout
+var timey = document.querySelector('#timey');
+timey.style.fontWeight = "bold";
+timey.style.fontSize = "30px";
+setTimeout(function () {
+    timey.style.color = "red";
+    timey.style.transform = 'rotateX(180deg)';
+    timey.style.backgroundColor = "black";
+}, 3000);
--- 02-JavaScript/07-DOM manipulations/tamir2/dist/dist/index.dev.js
+++ 02-JavaScript/07-DOM manipulations/tamir2/dist/dist/index.dev.js
@@ -1,17 +1,16 @@
+"use strict";
+
 // const members = [
 //     { name: 'yosef', age: 65, city: 'netivot', year: 1950 },
 //     { name: 'aliza', age: 60, city: 'netivot', year: 1955 },
 //     { name: 'aviel', age: 26, city: 'netivot', year: 1995 },
 //     { name: 'tamir', age: 24, city: 'netivot', year: 1997 },
 // ];
-
-
 // function changecity(members, name:string, newcity:string) {
 //     try {
 //         if (!Array.isArray(members)) throw new Error("this is not your array");
 //         if (typeof name !== 'string') throw new Error("name is not a string");
 //         if (typeof newcity !== 'string') throw new Error("the new city is not a string");
-
 //         const index = members.findIndex(member => member.name === name)
 //         if (index === -1) {
 //             return members
@@ -24,11 +23,8 @@
 //         console.log(error)
 //     }
 // }
-
 // const newcity1 = changecity(members, 'aliza', 'ashdod')
-
 // console.log(newcity1)
-
 // events:
 // const thefirstbtn =document.getElementById('theone');
 // const thesecbtn =document.getElementById('thetwo');
@@ -37,17 +33,13 @@
 // const thefivebtn =document.getElementById('thefive');
 // for only 1 btn
 // thefirstbtn.addEventListener('mouseenter',runbtn1);
-
 // function runbtn1(){
 //     console.log('tamir')
 // }
-
 // thesecbtn.addEventListener('click',runbtn2,);
-
 // function runbtn2(){
 //     console.log('dont click on me')
 // }
-
 // setTimeout(()=>{thefirstbtn.style.borderRadius='40px';},3000);
 // setTimeout(()=>{thefirstbtn.style.color='red';},3000);
 // setTimeout(()=>{thesecbtn.style.borderRadius='40px';},4000);
@@ -58,43 +50,22 @@
 // setTimeout(()=>{theforbtn.style.color='yellow';},6000);
 // setTimeout(()=>{thefivebtn.style.borderRadius='40px';},7000);
 // setTimeout(()=>{thefivebtn.style.color='green';},7000);
-
 // for all-
 // const buttons = document.querySelectorAll('.btn')
-
 // buttons.forEach(button=>{
 //     button.addEventListener('click',runbtn1)
 // })
-
 // move item-
+function superman1() {
+  var supermen = document.getElementById('supermen');
+  var id = ev.target.id;
+}
 
-const buttons = document.querySelectorAll('.btn')
+console.log(supermen.getBondingClientRect());
+var position = supermen.getbonBingClientRect();
 
-buttons.forEach(button => {
-    button.addEventListener('click', runnow)
-})
-function runnow(ev): void {
-    const supermen = document.getElementById('supermen')
-    const id: string = ev.target.id;
-
-
-    const position = supermen.getBoundingClientRect();
-
-    switch (id) {
-        case 'up':
-            supermen.style.top = `${position.top - 30}px`;
-            break;
-        case 'down':
-            supermen.style.top = `${position.top + 30}px`;
-            break;
-        case 'right':
-            supermen.style.left = `${position.left + 30} px`;
-            break;
-        case 'left':
-            supermen.style.left = `${position.left - 30} px`;
-            break;
-
-
-    }
-
+switch (id) {
+  case 'up':
+    supermen.style.top = position.top - 20 + "px";
+    break;
 }
