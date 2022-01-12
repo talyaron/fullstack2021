2d6bc747608dc4c2160bd869dc21364973dff908
Michael Dubovyk <93338459+mishalon@users.noreply.github.com>
2022-01-12 18:15:35 +0200

    4

--- 02-JavaScript/08-forms/mishaHW/dist/index.js
+++ 02-JavaScript/08-forms/mishaHW/dist/index.js
@@ -1,26 +1,16 @@
-
-
 function createCard(event) {
-
     // event.preventDefault();
-    const formObj = {};
-    for (let field of event.target) {
-
+    var formObj = {};
+    for (var _i = 0, _a = event.target; _i < _a.length; _i++) {
+        var field = _a[_i];
         // console.log(field.name, field.value)
-        if (field.name !== 'Submit') formObj[field.name] = field.value;
-
+        if (field.name !== 'Submit')
+            formObj[field.name] = field.value;
     }
-
     console.log(formObj);
     // const fname = formObj.name;
     // console.log(fname);
     // console.log(formObj[1]);
-
 }
-
 console.log(formObj.name);
-
-
-
-
 // fname.innerHTML = formObj[0];
