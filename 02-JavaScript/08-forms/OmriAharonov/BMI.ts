ee4531fdc61683423b0049f6d359088af86235aa
OmriAharonov <omri832@gmail.com>
2022-01-12 11:35:03 +0200

    form omri

--- 02-JavaScript/08-forms/OmriAharonov/BMI.ts
+++ 02-JavaScript/08-forms/OmriAharonov/BMI.ts
@@ -1,11 +1,27 @@
-const bmiObj = {};
+const info = {};
 
 function handleSubmit(ev){
     ev.preventDefault();
-  
-    
-   bmiObj[ev.target.placeholder('weight')] /  (ev.target.placeholder('Height')) * (ev.target.placeholder('Height'))
+    for(let elem of ev.target){
 
-   
+        if(elem.name !== 'submit')info[elem.name] = elem.valueAsNumber;
+        //console.dir(elem)
+    }
+
+  
 }
+
+function calculateBmi(obj){
+    let calc = (obj.weight / ((obj.height * obj.height)/100))
+}
+
+const result = calculateBmi(info);
+document.querySelector('bmi').innerHTML = `${result}`
+
+// console.log(info);
+// console.dir(info);
+
+
+
+
 
--- 02-JavaScript/08-forms/OmriAharonov/dist/BMI.js
+++ 02-JavaScript/08-forms/OmriAharonov/dist/BMI.js
@@ -1,5 +1,17 @@
-var bmiObj = {};
+var info = {};
 function handleSubmit(ev) {
     ev.preventDefault();
-    bmiObj[ev.target.placeholder('weight')] / (ev.target.placeholder('Height')) * (ev.target.placeholder('Height'));
+    for (var _i = 0, _a = ev.target; _i < _a.length; _i++) {
+        var elem = _a[_i];
+        if (elem.name !== 'submit')
+            info[elem.name] = elem.valueAsNumber;
+        //console.dir(elem)
+    }
 }
+function calculateBmi(obj) {
+    var calc = (obj.weight / ((obj.height * obj.height) / 100));
+}
+var result = calculateBmi(info);
+document.querySelector('bmi').innerHTML = "" + result;
+// console.log(info);
+// console.dir(info);
