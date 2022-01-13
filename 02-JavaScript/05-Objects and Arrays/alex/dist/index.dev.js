039628ed9bdc5ac6fe61e71aa67cfde53b6425b9
usercodes89 <52649498+usercodes89@users.noreply.github.com>
2022-01-13 22:34:50 +0200

    x

--- 02-JavaScript/05-Objects and Arrays/alex/dist/index.dev.js
+++ 02-JavaScript/05-Objects and Arrays/alex/dist/index.dev.js
@@ -1,18 +1,15 @@
 "use strict";
 
-// const keys = ['author', 'yearOfPublication', 'pages'];
-// const values = ['T.J', 2010, 350]
-// let result = {};
-// keys.forEach(function (value, index) {
-//     result[value] = values[index]
-// });
-// console.log(result);
 var keys = ['author', 'yearOfPublication', 'pages'];
 var values = ['T.J', 2010, 350];
 var result = {};
-
-for (var i = 0; i < keys.length; i++) {
-  result[keys[i]] = values[i];
-}
-
-console.log(result);
+keys.forEach(function (value, index) {
+  result[value] = values[index];
+});
+console.log(result); // const keys = ['author', 'yearOfPublication', 'pages'];
+// const values = ['T.J', 2010, 350]
+// let result = {};
+// for(let i=0; i<keys.length; i++) {
+//    result[keys[i]]= values[i];
+// }
+// console.log(result);
