423e7a47e3cad6d9bc2e39308e8c9a4be3047029
OmriAharonov <omri832@gmail.com>
2022-01-13 20:19:59 +0200

    object - Omri

--- 02-JavaScript/05-Objects and Arrays/start/OmriAharonov/repeat/dist/index.js
+++ 02-JavaScript/05-Objects and Arrays/start/OmriAharonov/repeat/dist/index.js
@@ -18,3 +18,10 @@ keys.forEach(function (elem) {
 //     book.elem = 
 // })
 //console.log(book);
+// const keys = ['author', 'yearOfPublication', 'pages'];
+// const values = ['T.J', 2010, 350]
+// let result = {};
+// for(let i=0; i<keys.length; i++) {
+//    result[keys[i]]= values[i];
+// }
+// console.log(result);
--- 02-JavaScript/05-Objects and Arrays/start/OmriAharonov/repeat/index.ts
+++ 02-JavaScript/05-Objects and Arrays/start/OmriAharonov/repeat/index.ts
@@ -28,3 +28,16 @@ keys.forEach(elem =>{
 
 //console.log(book);
 
+
+
+// const keys = ['author', 'yearOfPublication', 'pages'];
+// const values = ['T.J', 2010, 350]
+
+// let result = {};
+
+
+// for(let i=0; i<keys.length; i++) {
+//    result[keys[i]]= values[i];
+// }
+
+// console.log(result);
