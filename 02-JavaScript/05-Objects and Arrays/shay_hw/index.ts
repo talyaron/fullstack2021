813a5f815756bdd206f1f0edc94fe3b8683bfe7b
shayfoyer <93338189+shayfoyer@users.noreply.github.com>
2022-01-15 20:23:53 +0200

    shay

---
+++ 02-JavaScript/05-Objects and Arrays/shay_hw/index.ts
@@ -0,0 +1,40 @@
+interface Class{
+lessons:Array<{
+    title: string,
+    grades:Array<any>
+}>
+    newLesson(string, Array):void
+}
+const student:Array<string> = ['shir', 'moshe', 'miri', 'david', 'lea'];
+
+let fullStackClass: Class = {
+    lessons:[
+        {
+            title: 'object - advanced',
+            grades:[]
+        }
+    ],
+    newLesson(newTitle, studentList) {
+        this.lessons.push({ title: newTitle, grades: [] });
+        studentList.forEach((student, i) => {
+            this.lessons.slice(-1)[0].grades[i] = { name: student, grade: getRandomgGrade() }
+        });
+    }
+}
+
+
+ 
+function getRandomgGrade(){
+
+return Math.floor(Math.random() * 61)+40;
+}
+ function generateTheGrade(theClass, studentList){
+     studentList.forEach((student, i) => {
+         theClass.lessons[0].grades[i] = { name: student, grade: getRandomgGrade()}
+     });
+
+ }
+ generateTheGrade(fullStackClass, student);
+ console.log(fullStackClass);
+ fullStackClass.newLesson("form",student);
+ 
