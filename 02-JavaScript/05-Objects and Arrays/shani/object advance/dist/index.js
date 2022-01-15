e7b162b36746cba11dadfec4ae1ed55184ad52bc
ShaniRom <93940556+ShaniRom@users.noreply.github.com>
2022-01-15 17:15:47 +0200

    hw

--- 02-JavaScript/05-Objects and Arrays/shani/object advance/dist/index.js
+++ 02-JavaScript/05-Objects and Arrays/shani/object advance/dist/index.js
@@ -1,3 +1,4 @@
+var students = ['moshe, ahron, miriam', 'dan', 'levi'];
 var fullStackClass = {
     lessons: [
         {
@@ -6,35 +7,38 @@ var fullStackClass = {
         }
     ]
 };
-var students = ['moshe, ahron, miriam', 'dan', 'levi'];
 //create a function which genrate random grades between 40 and 100 for all students (make the score an integer).
+function getRandomgGrade() {
+    return Math.floor(Math.random() * (100 - 40 + 1)) + 40;
+}
+console.log(getRandomgGrade());
 //at the end the object should look somthing like that:
-fullStackClass = {
-    lessons: [{
-            title: 'Objects - advance',
-            grades: [
-                {
-                    name: 'moshe',
-                    grade: 45
-                },
-                {
-                    name: 'ahron',
-                    grade: 67
-                },
-                {
-                    name: 'miriam',
-                    grade: 78
-                },
-                {
-                    name: 'dan',
-                    grade: 45
-                },
-                {
-                    name: 'levi',
-                    grade: 45
-                }
-            ]
-        }
-    ]
-};
+// fullStackClass = {
+//     lessons: [{
+//         title: 'Objects - advance',
+//         grades: [
+//             {
+//                 name: 'moshe',
+//                 grade: 45
+//             },
+//             {
+//                 name: 'ahron',
+//                 grade: 67
+//             },
+//             {
+//                 name: 'miriam',
+//                 grade: 78
+//             },
+//             {
+//                 name: 'dan',
+//                 grade: 45
+//             },
+//             {
+//                 name: 'levi',
+//                 grade: 45
+//             }
+//         ]
+//     }
+//     ]
+// }
 //create an internal function to the object
--- 02-JavaScript/05-Objects and Arrays/shani/object advance/index.ts
+++ 02-JavaScript/05-Objects and Arrays/shani/object advance/index.ts
@@ -5,6 +5,8 @@ interface Class {
     }>
 }
 
+const students: Array<string> = ['moshe, ahron, miriam', 'dan', 'levi'];
+
 let fullStackClass: Class = {
     lessons: [
         {
@@ -14,39 +16,48 @@ let fullStackClass: Class = {
     ]
 }
 
-const students = ['moshe, ahron, miriam', 'dan', 'levi'];
+
 
 //create a function which genrate random grades between 40 and 100 for all students (make the score an integer).
+
+
+function getRandomgGrade() {
+    return Math.floor(Math.random() * (100 - 40 + 1) ) + 40;
+  }
+console.log(getRandomgGrade())
+
+
+
 //at the end the object should look somthing like that:
 
-fullStackClass = {
-    lessons: [{
-        title: 'Objects - advance',
-        grades: [
-            {
-                name: 'moshe',
-                grade: 45
-            },
-            {
-                name: 'ahron',
-                grade: 67
-            },
-            {
-                name: 'miriam',
-                grade: 78
-            },
-            {
-                name: 'dan',
-                grade: 45
-            },
-            {
-                name: 'levi',
-                grade: 45
-            }
+// fullStackClass = {
+//     lessons: [{
+//         title: 'Objects - advance',
+//         grades: [
+//             {
+//                 name: 'moshe',
+//                 grade: 45
+//             },
+//             {
+//                 name: 'ahron',
+//                 grade: 67
+//             },
+//             {
+//                 name: 'miriam',
+//                 grade: 78
+//             },
+//             {
+//                 name: 'dan',
+//                 grade: 45
+//             },
+//             {
+//                 name: 'levi',
+//                 grade: 45
+//             }
 
-        ]
-    }
-    ]
-}
+//         ]
+//     }
+//     ]
+// }
 
 //create an internal function to the object
