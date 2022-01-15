3a5848a77c139da7e1a3236fc6259d4f6305299e
ShaniRom <93940556+ShaniRom@users.noreply.github.com>
2022-01-15 21:06:09 +0200

    shani hw

--- 02-JavaScript/05-Objects and Arrays/shani/object advance/dist/index.js
+++ 02-JavaScript/05-Objects and Arrays/shani/object advance/dist/index.js
@@ -1,44 +1,31 @@
-var students = ['moshe, ahron, miriam', 'dan', 'levi'];
+//create an internal function to the object
+var students = ['moshe', 'ahron', 'miriam', 'dan', 'levi'];
 var fullStackClass = {
     lessons: [
         {
             title: 'Objects - advance',
             grades: []
         }
-    ]
+    ],
+    newLesson: function (newTitle, student) {
+        this.lessons.push({ title: newTitle, grades: [] });
+        for (var i = 0; i < students.length; i++) {
+            this.lessons.slice(-1)[0].grades[i] = { name: student[i], grade: getRandomgGrade() };
+        }
+        return 'newLesson()';
+    }
 };
+fullStackClass.newLesson("DOM manipulation", students);
+console.log(fullStackClass);
 //create a function which genrate random grades between 40 and 100 for all students (make the score an integer).
 function getRandomgGrade() {
     return Math.floor(Math.random() * (100 - 40 + 1)) + 40;
 }
-console.log(getRandomgGrade());
-//at the end the object should look somthing like that:
-// fullStackClass = {
-//     lessons: [{
-//         title: 'Objects - advance',
-//         grades: [
-//             {
-//                 name: 'moshe',
-//                 grade: 45
-//             },
-//             {
-//                 name: 'ahron',
-//                 grade: 67
-//             },
-//             {
-//                 name: 'miriam',
-//                 grade: 78
-//             },
-//             {
-//                 name: 'dan',
-//                 grade: 45
-//             },
-//             {
-//                 name: 'levi',
-//                 grade: 45
-//             }
-//         ]
-//     }
-//     ]
-// }
-//create an internal function to the object
+//console.log(getRandomgGrade())
+function creatGrades(fullStack, student) {
+    for (var i = 0; i < students.length; i++) {
+        fullStack.lessons[0].grades[i] = { name: student[i], grade: getRandomgGrade() };
+    }
+    return 'fullStack.lessons[0].grades';
+}
+creatGrades(fullStackClass, students);
--- 02-JavaScript/05-Objects and Arrays/shani/object advance/index.ts
+++ 02-JavaScript/05-Objects and Arrays/shani/object advance/index.ts
@@ -3,9 +3,13 @@ interface Class {
         title: string,
         grades: Array<any>
     }>
+    newLesson(string, Array): void//no output
 }
 
-const students: Array<string> = ['moshe, ahron, miriam', 'dan', 'levi'];
+
+//create an internal function to the object
+
+const students: Array<string> = ['moshe', 'ahron', 'miriam', 'dan', 'levi'];
 
 let fullStackClass: Class = {
     lessons: [
@@ -13,51 +17,40 @@ let fullStackClass: Class = {
             title: 'Objects - advance',
             grades: []
         }
-    ]
-}
 
+    ],
+    newLesson(newTitle, student) {
+        this.lessons.push({ title: newTitle, grades: [] });
+        for (let i = 0; i < students.length; i++) {
+           this.lessons.slice(-1)[0].grades[i] = { name: student[i], grade: getRandomgGrade() };
+
+        }
+        return 'newLesson()'     
+    
+    }
+}
+fullStackClass.newLesson("DOM manipulation", students);
+
+console.log(fullStackClass)
 
 
 //create a function which genrate random grades between 40 and 100 for all students (make the score an integer).
 
-
 function getRandomgGrade() {
-    return Math.floor(Math.random() * (100 - 40 + 1) ) + 40;
-  }
-console.log(getRandomgGrade())
+    return Math.floor(Math.random() * (100 - 40 + 1)) + 40;
+}
+
+//console.log(getRandomgGrade())
+
+function creatGrades(fullStack, student) {
+    for (let i = 0; i < students.length; i++) {
+        fullStack.lessons[0].grades[i] = { name: student[i], grade: getRandomgGrade() };
+
+    }
+    return'fullStack.lessons[0].grades';
+}
+creatGrades(fullStackClass, students)
 
 
 
-//at the end the object should look somthing like that:
 
-// fullStackClass = {
-//     lessons: [{
-//         title: 'Objects - advance',
-//         grades: [
-//             {
-//                 name: 'moshe',
-//                 grade: 45
-//             },
-//             {
-//                 name: 'ahron',
-//                 grade: 67
-//             },
-//             {
-//                 name: 'miriam',
-//                 grade: 78
-//             },
-//             {
-//                 name: 'dan',
-//                 grade: 45
-//             },
-//             {
-//                 name: 'levi',
-//                 grade: 45
-//             }
-
-//         ]
-//     }
-//     ]
-// }
-
-//create an internal function to the object
