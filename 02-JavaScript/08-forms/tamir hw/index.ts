a278de116d0c8d1111fc979533387345487558c3
tamir-dadon <tamir.dadon63@gmail.com>
2022-01-16 16:31:19 +0200

    z

---
+++ 02-JavaScript/08-forms/tamir hw/index.ts
@@ -0,0 +1,38 @@
+interface Class {
+    lessons: Array<{
+        title: string,
+        grades: Array<any>
+    }>,
+    newLesson(string, Array): void,
+};
+const students = ['moshe', 'shron', 'miriam', 'dan', 'levi']
+let fulstackClass: Class = {
+    lessons: [
+        {
+            title: 'objects-advance',
+            grades: [],
+        }
+    ],
+
+
+
+    newLesson(titleNew, studentNames) {
+        this.lessons.push({ title: titleNew, grades: [] })
+        for (let i in studentNames) {
+            this.lessons[this.lessons.length - 1].grades[i] = { name: studentNames[i], grade: createRandomGradeFunc(40, 100) };
+        }
+    }
+};
+
+
+
+for (let i in students) {
+    fulstackClass.lessons[0].grades[i] = { name: students[i], grade: createRandomGradeFunc(40, 100) };
+}
+
+function createRandomGradeFunc(minGrade, maxGrade) {
+    return Math.floor(Math.random() * (maxGrade - minGrade + 1) + minGrade);
+}
+
+console.log(fulstackClass);
+fulstackClass.newLesson("sport", students);
