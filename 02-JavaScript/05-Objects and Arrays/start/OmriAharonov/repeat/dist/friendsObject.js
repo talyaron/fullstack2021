111ce7a765a41a8ed07194b0a481403ea1b795cd
OmriAharonov <omri832@gmail.com>
2022-01-16 20:17:53 +0200

    friendsObject challenge

--- 02-JavaScript/05-Objects and Arrays/start/OmriAharonov/repeat/dist/friendsObject.js
+++ 02-JavaScript/05-Objects and Arrays/start/OmriAharonov/repeat/dist/friendsObject.js
@@ -4,19 +4,39 @@ var myFreinds = {
             name: 'Yossi',
             age: 40,
             height: 190,
-            hobbies: ['Sky diving', 'skiing']
+            hobbies: ['Sky diving', 'Skiing'],
+            showInfo: function () {
+                console.log('My name is: ', myFreinds.frineds[0].name);
+                console.log('my height is: ', myFreinds.frineds[0].height);
+                console.log("my hobbies are:  " + myFreinds.frineds[0].hobbies);
+            }
         },
         {
             name: 'Alex',
             age: 38,
             height: 192,
-            hobbies: ['Welding', 'carpentry']
+            hobbies: ['Welding', 'carpentry'],
+            showInfo: function () {
+                console.log('My name is: ', myFreinds.frineds[1].name);
+                console.log('my height is: ', myFreinds.frineds[1].height);
+                console.log("my hobbies are:  " + myFreinds.frineds[1].hobbies);
+            }
         },
         {
             name: 'Pini',
             age: 30,
             height: 178,
-            hobbies: ['motorcycle', 'swiming']
+            hobbies: ['motorcycle', 'swiming'],
+            showInfo: function () {
+                console.log('My name is: ', myFreinds.frineds[2].name);
+                console.log('my height is: ', myFreinds.frineds[2].height);
+                console.log("my hobbies are:  " + myFreinds.frineds[2].hobbies);
+            }
         }
     ]
 };
+// myFreinds[0].showInfo
+myFreinds.frineds[0].showInfo();
+myFreinds.frineds[1].showInfo();
+myFreinds.frineds[2].showInfo();
+console.log(window.document.title);
