33be0680ff74759d36027325699a82ae676c955a
leon <lmalul13@gmail.com>
2022-01-17 12:06:43 +0200

    654

--- 02-JavaScript/05-Objects and Arrays/leon hw/dist/index.js
+++ 02-JavaScript/05-Objects and Arrays/leon hw/dist/index.js
@@ -1,19 +1,27 @@
-// interface Class {
-//     lessons: Array<{
-//         title: string,
-//         grades: Array<any>
-//     }>
-//     generateNewLesson(string, Array):void
-// }
-// const students: Array<string> = ['Moshe', 'Ahron', 'Miriam', 'Dan', 'Levi'];
-// let fullStackClass: Class = {
-//     lessons: [
-//         {
-//             title: 'Objects - advance',
-//             grades: []
-//         }
-//     ]
-function randomnumber() {
+var students = ['Moshe', 'Ahron', 'Miriam', 'Dan', 'Levi'];
+var fullStackClass = {
+    lessons: [
+        {
+            title: 'Objects - advance',
+            grades: []
+        }
+    ],
+    Lesson: function (newTitle, studentList) {
+        var _this = this;
+        this.lessons.push({ title: newTitle, grades: [] });
+        studentList.forEach(function (student, i) {
+            _this.lessons.slice(-1)[0].grades[i] = { name: student, grade: RandomGrade() };
+        });
+    }
+};
+function RandomGrade() {
     return Math.floor(Math.random() * 60) + 40;
 }
-console.log(randomnumber());
+function generateTheGrade(theClass, studentList) {
+    studentList.forEach(function (student, i) {
+        theClass.lessons[0].grades[i] = { name: student, grade: RandomGrade() };
+    });
+}
+generateTheGrade(fullStackClass, students);
+console.log(fullStackClass);
+fullStackClass.Lesson("form", students);
