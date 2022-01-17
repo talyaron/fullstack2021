7201f9440d56449e60209fb119a01a3e9abfcd82
Marina <garpalion@gmail.com>
2022-01-17 14:21:38 +0200

    ob

---
+++ 02-JavaScript/05-Objects and Arrays/marina/ObjAdv/dist/objArrHW.js
@@ -0,0 +1,58 @@
+var students = ['moshe', 'ahron', 'miriam', 'dan', 'levi'];
+// internal function to the object
+var fullStackClass = {
+    lessons: [
+        {
+            title: 'Objects - advance',
+            grades: []
+        }
+    ],
+    newLesson: function (newTitle, student) {
+        this.lessons.push({ title: newTitle, grades: [] });
+        for (var i = 0; i < students.length; i++) {
+            this.lessons.slice(-1)[0].grades[i] = { name: student[i], grade: randomGrade() };
+        }
+        return 'newLesson()';
+    }
+};
+fullStackClass.newLesson('Type script', students);
+console.log(fullStackClass);
+// a function which genrate random grades between 40 and 100 for all students(make the score an integer)
+function randomGrade() {
+    return Math.floor(Math.random() * 61 + 40);
+}
+function gradesGenerator(classObj, students) {
+    students.forEach(function (student, i) {
+        classObj.lessons[0].grades[i] = { name: student, grade: randomGrade() };
+    });
+}
+gradesGenerator(fullStackClass, students);
+// fullStackClass = {
+//     lessons: [{
+//         title: 'Objects - advance',
+//         grades: [
+//             {
+//                 name: 'moshe',
+//                 grade: 45
+//             },
+//             {
+//                 name: 'ahron',
+//                 grade: 67
+//             },
+//             {
+//                 name: 'miriam',
+//                 grade: 78
+//             },
+//             {
+//                 name: 'dan',
+//                 grade: 45
+//             },
+//             {
+//                 name: 'levi',
+//                 grade: 45
+//             }
+//         ]
+//     }
+//     ]
+// }
+//create an internal function to the object
