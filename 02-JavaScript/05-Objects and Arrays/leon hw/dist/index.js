be4682e1358b317533a6c32f4a39760ac6bc3923
leon <lmalul13@gmail.com>
2022-01-17 07:52:49 +0200

    3

--- 02-JavaScript/05-Objects and Arrays/leon hw/dist/index.js
+++ 02-JavaScript/05-Objects and Arrays/leon hw/dist/index.js
@@ -1,13 +1,19 @@
-var students = ['Moshe', 'Ahron', 'Miriam', 'Dan', 'Levi'];
-var fullStackClass = {
-    lessons: [
-        {
-            title: 'Objects - advance',
-            grades: []
-        }
-    ]
-    //create a function which genrate random grades between 40 and 100 for all students (make the score an integer).
-    ,
-    //create a function which genrate random grades between 40 and 100 for all students (make the score an integer).
-    "function": randomNumber()
-};
+// interface Class {
+//     lessons: Array<{
+//         title: string,
+//         grades: Array<any>
+//     }>
+//     generateNewLesson(string, Array):void
+// }
+// const students: Array<string> = ['Moshe', 'Ahron', 'Miriam', 'Dan', 'Levi'];
+// let fullStackClass: Class = {
+//     lessons: [
+//         {
+//             title: 'Objects - advance',
+//             grades: []
+//         }
+//     ]
+function randomnumber() {
+    return Math.floor(Math.random() * 60) + 40;
+}
+console.log(randomnumber());
--- 02-JavaScript/05-Objects and Arrays/leon hw/index.ts
+++ 02-JavaScript/05-Objects and Arrays/leon hw/index.ts
@@ -1,20 +1,30 @@
-interface Class {
-    lessons: Array<{
-        title: string,
-        grades: Array<any>
-    }>
-    generateNewLesson(string, Array):void
-}
+// interface Class {
+//     lessons: Array<{
+//         title: string,
+//         grades: Array<any>
+//     }>
+//     generateNewLesson(string, Array):void
+// }
 
-const students: Array<string> = ['Moshe', 'Ahron', 'Miriam', 'Dan', 'Levi'];
+// const students: Array<string> = ['Moshe', 'Ahron', 'Miriam', 'Dan', 'Levi'];
 
-let fullStackClass: Class = {
-    lessons: [
-        {
-            title: 'Objects - advance',
-            grades: []
-        }
-    ]
+// let fullStackClass: Class = {
+//     lessons: [
+//         {
+//             title: 'Objects - advance',
+//             grades: []
+//         }
+//     ]
 
-    //create a function which genrate random grades between 40 and 100 for all students (make the score an integer).
-   function randomNumber()
+   
+
+    function randomnumber(){
+        return Math.floor(Math.random() * 60) + 40;
+    }
+
+    console.log(randomnumber())
+
+
+    
+
+    
