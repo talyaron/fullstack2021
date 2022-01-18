b289359df90a72d88f0295142a8a0770a88044b5
Michael Dubovyk <93338459+mishalon@users.noreply.github.com>
2022-01-18 20:36:22 +0200

    1

---
+++ 02-JavaScript/05-Objects and Arrays/misha1801/index.ts
@@ -0,0 +1,70 @@
+interface Home {
+
+    books: Array<{
+        title: string
+        author: string
+    }>;
+
+    shoes: Array<{
+        brand: string
+        price: number
+    }>;
+
+    guitars: Array<{
+        brand: string
+        yearmade: number
+    }>;
+
+}
+
+
+let myHome: Home = {
+
+    books: [
+        {
+            title: 'The Silent Treatment',
+            author: 'Alex Michalids'
+        },
+        {
+            title: 'Last Mikve in Sibir',
+            author: 'Eshkol Nevo'
+        },
+        {
+            title: 'Bakery Names Passion',
+            author: 'Tomer Kaufman'
+        }
+    ],
+
+
+    shoes: [
+        {
+            brand: 'Adidas',
+            price: 350
+        },
+        {
+            brand: 'Reebok',
+            price: 250
+        },
+        {
+            brand: 'Timberland',
+            price: 700
+        },
+
+
+    ],
+
+    guitars: [
+
+        {
+            brand: 'Fender',
+            yearmade: 2008
+        },
+        {
+            brand: 'Yamaha',
+            yearmade: 2008
+        },
+    ],
+
+
+    
+}
