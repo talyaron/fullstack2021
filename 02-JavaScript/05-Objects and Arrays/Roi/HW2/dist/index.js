1d2a709b44791035409c7d6902cb2a8617b367de
RoiMirmelshtein <93940584+RoiMirmelshtein@users.noreply.github.com>
2022-01-20 09:06:25 +0200

    Roi

--- 02-JavaScript/05-Objects and Arrays/Roi/HW2/dist/index.js
+++ 02-JavaScript/05-Objects and Arrays/Roi/HW2/dist/index.js
@@ -1,8 +1,17 @@
 var myHome = {
-    books: [],
-    clothes: [],
-    movies: [],
-    addItems: function () {
-        this.books.push();
+    books: [{ name: 'Harry Potter' }, { name: 'Moby Dick' }, { name: 'The Great Gatsby' }, { name: 'Ulysses' },],
+    bands: [{ name: 'U2' }, { name: 'Iron Maiden' }, { name: 'The Smiths' }, { name: 'Blind Faith' },],
+    movies: [{ name: 'Jaws' }, { name: 'The Godfather' }, { name: 'The Dark Knight' }, { name: "Schindler's List" },],
+    addItems: function (obj, title) {
+        this.books.push({ name: title });
+        return this;
+    },
+    removeItem: function (obj, title) {
+        var index = obj.indexOf(title);
+        obj.splice(index);
+        return this;
     }
 };
+myHome.addItems(myHome.books, "xxx");
+myHome.removeItem(myHome.bands, "Blind Faith");
+console.dir(myHome);
