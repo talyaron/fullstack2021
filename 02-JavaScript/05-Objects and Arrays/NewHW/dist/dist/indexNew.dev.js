86cac9ed1203c5d900d332199872aaef4dd929e7
Michael-Frankel <76725406+Michael-Frankel@users.noreply.github.com>
2022-01-20 10:45:32 +0200

    MichaelF-new HW

---
+++ 02-JavaScript/05-Objects and Arrays/NewHW/dist/dist/indexNew.dev.js
@@ -0,0 +1,47 @@
+"use strict";
+
+var home = {
+  books: ['gmara', 'more-nevuchim', 'the death of the west'],
+  cloths: ['shirt', 'coat', 'pents', "shoos"],
+  movies: ['the usual suspect', 'shawshank', 'snatch', "lock stock"],
+  pushFunction: function pushFunction(value, name) {
+    console.log(value);
+    console.log(name);
+    this[name].push(value);
+    console.log(this);
+  }
+};
+
+function handleSubmit(event) {
+  event.preventDefault();
+  console.log(event);
+
+  for (var _i = 0, _a = event.target; _i < _a.length; _i++) {
+    var field = _a[_i];
+
+    if (field.name !== 'submit') {
+      switch (field.value) {
+        case '':
+          alert('Please fill all fields');
+          return;
+          break;
+
+        default:
+          home.pushFunction(field.value, field.name);
+          break;
+      }
+    }
+  }
+}
+
+function handleRemove(event) {
+  event.preventDefault();
+  console.log(event);
+  home.books.pop();
+  home.cloths.pop();
+  home.movies.pop();
+  console.log(home);
+}
+
+var c = document.querySelector(".render");
+c.innerHTML = "kddddddddddddddddddddddddddddddddd";
