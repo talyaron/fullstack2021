80afd8da21b89261c6bcc316177ee56dfb4dbb4e
ShaniRom <93940556+ShaniRom@users.noreply.github.com>
2022-01-20 19:07:44 +0200

    s

---
+++ 02-JavaScript/05-Objects and Arrays/shani/hw solution/dist/index.js
@@ -0,0 +1,38 @@
+var talHome = {
+    movies: [],
+    books: [],
+    clothes: [],
+    addMovie: function (movie) {
+        this.movies.push(movie);
+    },
+    removeMovie: function (movieTitle) {
+        var index = this.movies.findIndex(function (movie) { return movie.title === movieTitle; });
+        if (index >= 0) {
+            this.movies.splice(index, 1);
+        }
+    },
+    renderMovies: function (domElement) {
+        var html = '';
+        this.movies.forEach(function (movie) {
+            html += "<div class='card'>\n            <p>" + movie.title + ", " + movie.year + "</p></div>";
+        });
+        domElement.innerHTML = html;
+    }
+};
+function handleAddMovie(ev) {
+    ev.preventDefault();
+    console.dir(ev.target);
+    var title = ev.target.elements.title.value;
+    var year = ev.target.elements.year.valueAsNumber;
+    talHome.addMovie({ title: title, year: year });
+    var rootMovies = document.getElementById('rootMovies');
+    talHome.renderMovies(rootMovies);
+    ev.target.reset();
+}
+talHome.addMovie({ title: 'matrix', year: 1999 });
+talHome.addMovie({ title: 'matrix-2', year: 2000 });
+talHome.addMovie({ title: 'matrix-3', year: 2003 });
+talHome.removeMovie('matrix-2');
+var rootMovies = document.getElementById('rootMovies');
+talHome.renderMovies(rootMovies);
+console.log(talHome);
