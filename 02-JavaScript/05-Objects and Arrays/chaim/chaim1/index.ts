d1d5fbc4a4de4719b8ef5e68a7ef7a91513261af
Chaim <chaimytcohen@gmail.com>
2022-01-20 16:02:28 +0200

    chaim

--- 02-JavaScript/05-Objects and Arrays/chaim/chaim1/index.ts
+++ 02-JavaScript/05-Objects and Arrays/chaim/chaim1/index.ts
@@ -1,17 +1,29 @@
 
-interface Obj {
-    books: Array<string>
-    clothes: Array<string>
-    movies: Array<string>
+interface home {
+    books: Array<{ name: string }>,
+    clothes: Array<{ name: string }>,
+    movies: Array<{ name: string }>,
     addItems?: any
     removeitems?: any
+
 }
 
-let myHome: Obj = {
-    books: ["tora", "mishna", "gmara"],
-    clothes: ["tishert"],
-    movies: ["tom & jerry", 'world'],
+const myhome: home = {
+    books: [
+        { name: "tora" },
+        { name: "mishna" },
+        { name: "gmara" }
+    ],
 
+    clothes: [
+        { name: "tishert" },
+        { name: "hats" } 
+    ],
+
+    movies: [
+        { name: "national jeografick" },
+        { name: 'world' }
+    ],
 
     addItems: function () {
         this.books.push("tehilim")
@@ -21,24 +33,36 @@ let myHome: Obj = {
     },
 
     removeitems: function () {
+        this.books.splice([2])
+        this.clothes.splice([1])
         this.movies.splice([1])
         return this
     }
 }
-console.log(myHome)
-myHome.addItems()
-myHome.removeitems()
+console.log(myhome)
+myhome.addItems()
+myhome.removeitems()
 
+function renderhome(myhome, domElement: any) {
+    let html: string =
+        `<h1>myhome</h1>`
 
-function print (myHomeobj: domElement: any) {
-        let html: string = '';
-
-    myHome.books.forEach(books => {
-        let bookshtml = `<div class= 'home'><h2> ${myHome.books}</h2>`
-        
-       bookshtml='</div>';
-        html += bookshtml;
-
-        domElement.innerHTML = html;
-    });
+    html += `<h2>books</h2>`
+    myhome.books.forEach(books => {
+        const homehtml = `<div class= 'staff'><h3> ${books.name}</h3></div>`;
+        html += homehtml;
+    })
+    html += `<h2>clothes:</h2>`
+    myhome.clothes.forEach(clothes => {
+        const homehtml = `<div class= 'staff'><h3> ${clothes.name}</h3></div>`;
+        html += homehtml;
+    })
+    html += `<h2>movies:</h2>`
+    myhome.movies.forEach(movies => {
+        const homehtml = `<div class= 'staff'><h3> ${movies.name}</h3></div>`;
+        html += homehtml;
+    })
+    domElement.innerHTML = html;
 }
+const root = document.querySelector(`#roothome`);
+renderhome(myhome, root)
