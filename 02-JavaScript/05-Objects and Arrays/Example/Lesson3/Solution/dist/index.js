4861e3f02ffb23de0e74bb167bc99a91bb0c6694
Tal Yaron <tal.yaron@gmail.com>
2022-01-20 20:06:39 +0200

    sort-filter

--- 02-JavaScript/05-Objects and Arrays/Example/Lesson3/Solution/dist/index.js
+++ 02-JavaScript/05-Objects and Arrays/Example/Lesson3/Solution/dist/index.js
@@ -16,9 +16,25 @@ var talHome = {
         this.movies.forEach(function (movie) {
             html += "<div class='card'>\n            <p>" + movie.title + ", " + movie.year + "</p></div>";
         });
+        // console.log(html)
         domElement.innerHTML = html;
+    },
+    newMovies: function (year) {
+        return this.movies.filter(function (movie) { return movie.year > year; });
+    },
+    sortMovies: function (orderBy) {
+        if (orderBy === void 0) { orderBy = 'asc'; }
+        if (orderBy === 'asc') {
+            this.movies.sort(function (a, b) { return a.year - b.year; });
+        }
+        else if (orderBy === 'desc') {
+            this.movies.sort(function (a, b) { return b.year - a.year; });
+        }
     }
 };
+var numbers = [2, 4, 5, 66, 45, 23, 12];
+var lessThan20 = numbers.filter(function (elm) { return elm > 20; });
+console.log(lessThan20);
 function handleAddMovie(ev) {
     ev.preventDefault();
     console.dir(ev.target);
@@ -29,10 +45,13 @@ function handleAddMovie(ev) {
     talHome.renderMovies(rootMovies);
     ev.target.reset();
 }
+talHome.addMovie({ title: 'matrix-3', year: 2003 });
 talHome.addMovie({ title: 'matrix', year: 1999 });
 talHome.addMovie({ title: 'matrix-2', year: 2000 });
-talHome.addMovie({ title: 'matrix-3', year: 2003 });
-talHome.removeMovie('matrix-2');
 var rootMovies = document.getElementById('rootMovies');
 talHome.renderMovies(rootMovies);
 console.log(talHome);
+var newMovies = talHome.newMovies(1999);
+console.log(newMovies);
+talHome.sortMovies();
+console.log(talHome.movies);
