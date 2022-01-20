8fa7c5190d9bdd0b8d8c74c8a0decd5a05cb60a1
OmriAharonov <omri832@gmail.com>
2022-01-20 14:47:07 +0200

    omri myHome

--- 02-JavaScript/05-Objects and Arrays/OmriAharonov/homeWork/index.ts
+++ 02-JavaScript/05-Objects and Arrays/OmriAharonov/homeWork/index.ts
@@ -13,24 +13,20 @@ interface myHome {
 const myHome = {
     books: [
 
-        {arrayTitle: 'Books'},
-
         {
-            name: 'Troubled Blood',
+            title: 'Troubled Blood',
             numberOfPages: 869,
             author: 'Robert Galbraith'
         },
 
         {
-            name: 'Orphan X',
+            title: 'Orphan X',
             numberOfPages: 368,
             author: 'Gregg Hurwitz'
         }
     ],
 
     movies: [
-
-        {arrayTitle: 'Movies'},
 
         {
             title: 'The Pianist',
@@ -46,9 +42,6 @@ const myHome = {
     ],
 
     tvShows: [
-
-        {arrayTitle: 'TV-Shows'},
-
         {
             title: 'Narcos',
             seasons: 3,
@@ -62,52 +55,77 @@ const myHome = {
         }
     ],
 
-    addItem: function (item: Array<any>,newtitle:string, category1:any, category2: any) {
-        
-        if(this.books === item){
-            this.books.push({name: newtitle, numberOfPages: category1, author:category2 })
+    addItem: function (item: Array<any>, newtitle: string, category1: any, category2: any) {
+
+        if (this.books === item) {
+            this.books.push({ title: newtitle, numberOfPages: category1, author: category2 });
         }
-        else if(this.movies === item){
-            this.movies.push({title: newtitle, director: category1, lenghtMinutes:category2 })
+        else if (this.movies === item) {
+            this.movies.push({ title: newtitle, director: category1, lenghtMinutes: category2 });
         }
-        else{
-            
-            this.tvShows.push({title: newtitle, seasons: category1, topRate:category2 })
+        else {
+            this.tvShows.push({ title: newtitle, seasons: category1, topRate: category2 });
         }
         return this;
     },
 
-    // removeItem: function (item: Array<any>, titleToRemove: string): myHome {
-    //     let index = item.indexOf(titleToRemove)
-    //     item.splice(index);
-    //     return this;
-    // }
+    removeItem: function (item: Array<any>, titleToRemove: string): myHome {
+        if (this.books === item) {
+            for (let i = 1; i < this.books.length; i++) {
+                if (this.books[i].title === titleToRemove) {
+                    this.books.splice(i, 1);
+                }
+            }
+        }
+        else if (this.movies === item) {
+            for (let i = 1; i < this.movies.length; i++) {
+                if (this.movies[i].title === titleToRemove) {
+                    this.movies.splice(i, 1);
+                }
+            }
+        }
+        else {
+            for (let i = 1; i < this.tvShows.length; i++) {
+                if (this.tvShows[i].title === titleToRemove) {
+                    this.tvShows.splice(i, 1);
+                }
+            }
+        }
+        return this;
+    }
 
-}
+};
 
 
-myHome.addItem(myHome.books,'The chemist', 480, 'Stephenie Meyer')
-myHome.addItem(myHome.movies,'Forrest Gump', 'Robert Zemeckis', 142)
-myHome.addItem(myHome.tvShows,'Breaking bad', 5, 10 )
-console.log(myHome)
+myHome.addItem(myHome.books, 'The chemist', 480, 'Stephenie Meyer');
+myHome.addItem(myHome.movies, 'Forrest Gump', 'Robert Zemeckis', 142);
+myHome.addItem(myHome.tvShows, 'Breaking bad', 5, 10);
+// console.log(myHome['movies'][0].arrayTitle);
+// myHome.removeItem(myHome.books, 'Orphan X');
+// myHome.removeItem(myHome.tvShows, 'Breaking bad');
+// console.log(myHome);
 
 
+function renderMyHome(obj: myHome, domElem: any) {
+    let html: string = '';
+    for (let [array]  of Object.entries(obj)) {
+        if (obj[array][0] !== undefined){
+            let arrayHtml = `<div class = 'card'> <h2> ${[array]}: </h2>`;
 
+            obj[array].forEach((item,i) =>{
+                arrayHtml += `<h4> ${obj[array][i].title} </h4>`;
 
+            })
+            
+            arrayHtml += `</div>`;
+            html += arrayHtml;
+        }
 
+    }
 
-function renderMyHome(obj:myHome,domElem:any){
-    let html:string = '';
-
-  for(let array in myHome){
-       
-  }
     domElem.innerHTML = html;
 }
 
-const homeRoot = document.getElementById('rootHome');
-
-
-
-
-
+const homeRoot = document.querySelector('#rootHome');
+console.log(myHome)
+renderMyHome(myHome, homeRoot);
