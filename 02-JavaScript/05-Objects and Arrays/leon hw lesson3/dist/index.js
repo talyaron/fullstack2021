5460894dbcd06ed10f49531d6cf6a9f3b1405b36
leon <lmalul13@gmail.com>
2022-01-20 14:22:22 +0200

    645

--- 02-JavaScript/05-Objects and Arrays/leon hw lesson3/dist/index.js
+++ 02-JavaScript/05-Objects and Arrays/leon hw lesson3/dist/index.js
@@ -3,13 +3,29 @@ var myHome = {
     clothes: [],
     movies: [],
     addItems: function () {
-        this.books.push("harry potter", "king of jungle", "yolo");
-        this.clothes.push("jeans", "T shirts", "jacket");
-        this.movies.push("fast and furious", "the saw");
+        this.books.push({ name: "harry potter" }, { name: "god of war" }),
+            this.clothes.push({ name: "jeans" }, { name: "shirt" });
+        this.movies.push({ name: "name:fast and furious" }, { name: "the saw" });
         return this;
     }
 };
 console.log(myHome.addItems());
-function renderItems(myHome) {
-    var html = '';
+function renderItems(myHome, domElement) {
+    var html = "\n    <h1>My Home</h1>\n    <h1>books:</h1>";
+    myHome.books.forEach(function (book) {
+        var homeHtml = "<div class='card'><h2>" + book.name + "</h2> </div>";
+        html += homeHtml;
+    });
+    myHome.movies.forEach(function (movie) {
+        var homeHtml = "<div class='card'><h2>" + movie.name + "</h2> </div>";
+        html += homeHtml;
+    });
+    html += "<h1>clothes:</h1>";
+    myHome.clothes.forEach(function (clothes) {
+        var homeHtml = "<div class='card'><h2>" + clothes.name + "</h2> </div>";
+        html += homeHtml;
+    });
+    domElement.innerHtml = html;
 }
+var root = document.querySelector("#rootItems");
+renderItems(myHome, root);
--- 02-JavaScript/05-Objects and Arrays/leon hw lesson3/index.ts
+++ 02-JavaScript/05-Objects and Arrays/leon hw lesson3/index.ts
@@ -11,18 +11,45 @@ const myHome:any={
     clothes:[],
     movies:[],
     addItems:function(){
-        this.books.push(`harry potter`,`king of jungle`,`yolo`)
-        this.clothes.push(`jeans`,`T shirts`,`jacket`)
-        this.movies.push(`fast and furious`,`the saw`)
+        this.books.push({name:`harry potter`},{name:`god of war`}),
+        this.clothes.push({name:`jeans`},{name:`shirt`})
+        this.movies.push({name:`name:fast and furious`},{name:`the saw`})
         return this
     }
 }
 console.log(myHome.addItems())
 
 
-function renderItems(myHome){
-    let html: string = '';
+function renderItems(myHome,domElement){
+    let html: string = `
+    <h1>My Home</h1>
+    <h1>books:</h1>`
+    myHome.books.forEach(book=>{
+        let homeHtml = `<div class='card'><h2>${book.name}</h2> </div>`;    
+        html += homeHtml;
+     })
     
+    myHome.movies.forEach(movie=>{
+       let homeHtml = `<div class='card'><h2>${movie.name}</h2> </div>`;    
+       html += homeHtml;
+    })
+    html += `<h1>clothes:</h1>`
+    myHome.clothes.forEach(clothes=>{
+       let homeHtml = `<div class='card'><h2>${clothes.name}</h2> </div>`;  
+       html += homeHtml;
+    })
+
+    domElement.innerHtml = html;
 
 }
+const root = document.querySelector(`#rootItems`);
+renderItems(myHome,root)
+
+
+
+
+
+    
+
+
 
