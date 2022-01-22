962de5af5fe629feb88d91050dca744ea28f01b3
OrA99 <oravramashvili@gmail.com>
2022-01-22 22:01:10 +0200

    or Hw

--- 02-JavaScript/05-Objects and Arrays/Or/zap/zap.ts
+++ 02-JavaScript/05-Objects and Arrays/Or/zap/zap.ts
@@ -1,25 +1,59 @@
 interface list {
     items: Array<{ description: string, price: number }>;
-    addItem?: any
+    renderList?: any
+    sortListAsc(): any
+    sortListDesc(): any
+    filterList?(): any
 }
 const list: list = {
     items: [],
 
+    renderList(domElements) {
+        let HTML = '';
+        this.items.forEach(item => {
+            HTML += `<li><p> ${item.description} ${item.price}$</p></li>`
+        });
+        domElements.innerHTML = HTML
+    },
+    sortListAsc() {
+        this.items.sort((a, b) => a.price - b.price)
+    },
+    sortListDesc() {
+        this.items.sort((a, b) => b.price - a.price)
+    }
 
-    
 }
+const rootList = document.getElementById(`rootList`)
+list.renderList(rootList)
 
 
 function handleSubmit(ev) {
     console.log(ev.target.elements)
     ev.preventDefault();
-    // ev.target.reset();
     const description = ev.target.elements.description.value;
     const price = ev.target.elements.price.valueAsNumber;
-  
-        list.items.push({description,price});
-    
-    // list.addItem({description,price})
-    
+
+    list.items.push({ description, price });
+    list.renderList(rootList)
+    ev.target.reset();
+
+}
+function handleClick(ev) {
+    switch (ev.target.id) {
+        case `asc`: list.sortListAsc()
+            break;
+        case `desc`: list.sortListDesc()
+            break
+    }
+    list.renderList(rootList)
+}
+function handleFilter(ev) {
+    console.log(ev.target.value);
+    const maxPrice: number = ev.target.value;
+    list.items.filter((item) => {
+        return item.price > maxPrice;
+    })
+    console.log(list)
+    list.renderList(rootList)
 }
 console.log(list)
