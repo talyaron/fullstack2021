03c7a73205ffbe496fd34add9200e0b27c5e7779
ShaniRom <93940556+ShaniRom@users.noreply.github.com>
2022-01-22 19:15:44 +0200

    hw

--- 02-JavaScript/05-Objects and Arrays/shani/hw zap/dist/zap.js
+++ 02-JavaScript/05-Objects and Arrays/shani/hw zap/dist/zap.js
@@ -2,7 +2,38 @@ var zapPage = {
     products: [{ description: "clock", price: 20 }],
     addItem: function (product) {
         this.products.push(product);
+    },
+    renderItem: function (domElement) {
+        var html = "";
+        this.products.forEach(function (item) {
+            html += "<div class='card'>\n            <p>" + item.description + ", " + item.price + "</p></div>";
+        });
+        domElement.innerHTML = html;
+    },
+    sortAscending: function () {
+        this.products.sort(function (a, b) { return a.price - b.price; });
+    },
+    sortDescending: function () {
+        this.products.sort(function (a, b) { return b.price - a.price; });
     }
 };
+//render makes what i wrote in the ts show on the html
+//the handle submit makes the input responsive
+function handleSubmit(ev) {
+    ev.preventDefault();
+    console.dir(ev.target);
+    var description = ev.target.elements.description.value;
+    var price = ev.target.elements.price.valueAsNumber;
+    zapPage.addItem({ description: description, price: price });
+    var rootZap = document.getElementById('rootZap');
+    zapPage.renderItem(rootZap);
+    ev.target.reset();
+}
 zapPage.addItem({ description: "paper", price: 5 });
 console.log(zapPage);
+var rootZap = document.getElementById('rootZap');
+zapPage.renderItem(rootZap);
+zapPage.sortAscending();
+console.log(zapPage.products);
+zapPage.sortDescending();
+console.log(zapPage.products);
--- 02-JavaScript/05-Objects and Arrays/shani/hw zap/zap.ts
+++ 02-JavaScript/05-Objects and Arrays/shani/hw zap/zap.ts
@@ -1,20 +1,63 @@
-interface zap{
-    products:Array<item>,
-    addItem(product:item);
+interface zap {
+    products: Array<item>,
+    addItem(product: item);
+    renderItem(domElement: any);
+    sortAscending(orderBy?: string);
+    sortDescending(orderBy?: string);
 }
-interface item{
-    description:string;
-    price:number
+interface item {
+    description: string;
+    price: number
 }
 
 
-const zapPage:zap={
-    products:[{description:"clock",price:20}],
-    addItem(product:item){
+const zapPage: zap = {
+    products: [{ description: "clock", price: 20 }],
+    addItem(product: item) {
         this.products.push(product);
     },
+    renderItem(domElement) {
+        let html = "";
+        this.products.forEach(item => {
+            html += `<div class='card'>
+            <p>${item.description}, ${item.price}</p></div>`
+
+        })
+        domElement.innerHTML = html
+    },
+    sortAscending(){
+        this.products.sort((a,b)=>{return a.price-b.price})
+
+    },
+    sortDescending(){
+        this.products.sort((a,b)=>{return b.price-a.price})
+
+    },
+
+}
+//render makes what i wrote in the ts show on the html
+//the handle submit makes the input responsive
+function handleSubmit(ev) {
+    ev.preventDefault();
+    console.dir(ev.target)
+    const description = ev.target.elements.description.value;
+    const price: number = ev.target.elements.price.valueAsNumber;
+
+    zapPage.addItem({ description, price });
+    const rootZap = document.getElementById('rootZap');
+    zapPage.renderItem(rootZap);
+
+    ev.target.reset();
 }
 
 
-zapPage.addItem({description:"paper",price:5});
+zapPage.addItem({ description: "paper", price: 5 });
 console.log(zapPage)
+
+const rootZap = document.getElementById('rootZap');
+zapPage.renderItem(rootZap);
+
+zapPage.sortAscending();
+console.log(zapPage.products);
+zapPage.sortDescending();
+console.log(zapPage.products);
