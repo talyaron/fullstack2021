b683e868b82dd0d4ac10fc4df95fdda8afa50fda
ShaniRom <93940556+ShaniRom@users.noreply.github.com>
2022-01-22 22:17:42 +0200

    HW

--- 02-JavaScript/05-Objects and Arrays/shani/hw zap/dist/zap.js
+++ 02-JavaScript/05-Objects and Arrays/shani/hw zap/dist/zap.js
@@ -3,25 +3,38 @@ var zapPage = {
     addItem: function (product) {
         this.products.push(product);
     },
-    renderItem: function (domElement) {
+    renderItem: function (array) {
         var html = "";
-        this.products.forEach(function (item) {
+        array.forEach(function (item) {
             html += "<div class='card'>\n            <p>" + item.description + ", " + item.price + "</p></div>";
         });
-        domElement.innerHTML = html;
+        document.getElementById('rootZap').innerHTML = html;
     },
     sortAscending: function () {
         // const ascending = document.getElementById('Ascending')
+        var _this = this;
         // ascending.addEventListener('click', (product) => {
         //     this.product.sort((a, b) => { return a.price - b.price })
         // })
-        document.getElementById("ascending").addEventListener("click", this.products.sort(function (a, b) { return a.price - b.price; }));
+        document.getElementById("ascending").addEventListener("click", function () {
+            _this.products.sort(function (a, b) { return a.price - b.price; });
+            _this.renderItem(_this.products);
+        });
     },
     sortDescending: function () {
-        this.products.sort(function (a, b) { return b.price - a.price; });
+        var _this = this;
+        document.getElementById("descending").addEventListener("click", function () {
+            _this.products.sort(function (a, b) { return b.price - a.price; });
+            _this.renderItem(_this.products);
+        });
     },
-    lowerThan: function () {
-        this.products.filter(function (product) { return product.price > 20; });
+    lowerThan: function (input) {
+        if (input == "") {
+            this.renderItem(this.products);
+            return;
+        }
+        var filteredArray = this.products.filter(function (product) { return product.price < Number(input); });
+        this.renderItem(filteredArray);
     }
 };
 //render makes what i wrote in the ts show on the html
@@ -32,19 +45,19 @@ function handleSubmit(ev) {
     var description = ev.target.elements.description.value;
     var price = ev.target.elements.price.valueAsNumber;
     zapPage.addItem({ description: description, price: price });
-    var rootZap = document.getElementById('rootZap');
-    zapPage.renderItem(rootZap);
+    zapPage.renderItem(zapPage.products);
     //zapPage.sortAscending({description, price})
     ev.target.reset();
 }
 function handleInput(ev) {
     console.log(ev.target.value);
+    zapPage.lowerThan(ev.target.value);
 }
 zapPage.addItem({ description: "paper", price: 5 });
 console.log(zapPage);
 var rootZap = document.getElementById('rootZap');
-zapPage.renderItem(rootZap);
+zapPage.renderItem(zapPage.products);
 zapPage.sortAscending();
 console.log(zapPage.products);
-// zapPage.sortDescending();
+zapPage.sortDescending();
 // console.log(zapPage.products);
--- 02-JavaScript/05-Objects and Arrays/shani/hw zap/zap.ts
+++ 02-JavaScript/05-Objects and Arrays/shani/hw zap/zap.ts
@@ -1,7 +1,7 @@
 interface zap {
     products: Array<item>,
     addItem(product: item);
-    renderItem(domElement: any);
+    renderItem(array:any);
     sortAscending(orderBy?: any);
     sortDescending?(orderBy?: string);
     lowerThan
@@ -17,48 +17,56 @@ const zapPage: zap = {
     addItem(product: item) {
         this.products.push(product);
     },
-    renderItem(domElement) {
+    renderItem(array) {
         let html = "";
-        this.products.forEach(item => {
+        array.forEach(item => {
             html += `<div class='card'>
             <p>${item.description}, ${item.price}</p></div>`
 
         })
-        domElement.innerHTML = html
+        document.getElementById('rootZap').innerHTML = html
     },
-    sortAscending(){
+    sortAscending() {
         // const ascending = document.getElementById('Ascending')
-        
+
         // ascending.addEventListener('click', (product) => {
         //     this.product.sort((a, b) => { return a.price - b.price })
         // })
-        document.getElementById("ascending").addEventListener("click", this.products.sort((a, b) => { return a.price - b.price }));
-     
-        
+        document.getElementById("ascending").addEventListener("click", () => {
+            this.products.sort((a, b) => {return a.price - b.price;})
+            this.renderItem(this.products);
+        });
+
+
 
     },
-    sortDescending(){
-        this.products.sort((a,b)=>{return b.price-a.price})
+    sortDescending() {
+        document.getElementById("descending").addEventListener("click", () => {
+            this.products.sort((a, b) => {return b.price - a.price;})
+            this.renderItem(this.products);
+        });
+    },
+    lowerThan(input) {
+        if (input == ""){
+            this.renderItem(this.products);  
+            return;
+        }
+        let filteredArray = this.products.filter(product => product.price < Number(input))
+        this.renderItem(filteredArray);
+    },
 
-    },
-    lowerThan(){
-        this.products.filter(product=> {return product.price>20})
-        
-    },
-    
 
 }
 //render makes what i wrote in the ts show on the html
 //the handle submit makes the input responsive
 function handleSubmit(ev) {
     ev.preventDefault();
-   // console.dir(ev.target.value)
+    // console.dir(ev.target.value)
     const description = ev.target.elements.description.value;
     const price: number = ev.target.elements.price.valueAsNumber;
 
     zapPage.addItem({ description, price });
-    const rootZap = document.getElementById('rootZap');
-    zapPage.renderItem(rootZap);
+    zapPage.renderItem(zapPage.products);
 
     //zapPage.sortAscending({description, price})
 
@@ -67,8 +75,9 @@ function handleSubmit(ev) {
     ev.target.reset();
 }
 
-function handleInput(ev){
+function handleInput(ev) {
     console.log(ev.target.value)
+    zapPage.lowerThan(ev.target.value)
 }
 
 
@@ -77,11 +86,11 @@ zapPage.addItem({ description: "paper", price: 5 });
 console.log(zapPage)
 
 const rootZap = document.getElementById('rootZap');
-zapPage.renderItem(rootZap);
+zapPage.renderItem(zapPage.products);
 
 zapPage.sortAscending();
 console.log(zapPage.products);
-// zapPage.sortDescending();
+zapPage.sortDescending();
 // console.log(zapPage.products);
 
 
