68f2396334e3df66539120e1d0c8370890edd717
Chaim <chaimytcohen@gmail.com>
2022-01-23 13:35:04 +0200

    chaim

--- 02-JavaScript/05-Objects and Arrays/chaim/chaim1/index.ts
+++ 02-JavaScript/05-Objects and Arrays/chaim/chaim1/index.ts
@@ -1,6 +1,6 @@
 
 interface home {
-    books: Array<{ name: string }>,
+    books: Array<{ name: any }>,
     clothes: Array<{ name: string }>,
     movies: Array<{ name: string }>,
     addItems?: any
@@ -66,3 +66,13 @@ function renderhome(myhome, domElement: any) {
 }
 const root = document.querySelector(`#roothome`);
 renderhome(myhome, root)
+
+
+
+function handleAddbooks(ev){
+    ev.preventDefault();
+    console.dir(ev.target)
+    const title= ev.target.elements.title.value
+    const year= ev.target.elements.year.valueAsNumber;
+   
+}
