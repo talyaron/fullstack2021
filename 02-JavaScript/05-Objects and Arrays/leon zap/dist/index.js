039dd11df9f502a2556742b19d2c6d469eef83ea
leon <lmalul13@gmail.com>
2022-01-23 21:40:43 +0200

    4

--- 02-JavaScript/05-Objects and Arrays/leon zap/dist/index.js
+++ 02-JavaScript/05-Objects and Arrays/leon zap/dist/index.js
@@ -3,16 +3,32 @@ var zapitem = {
     addItem: function (addItem) {
         this.items.push(addItem);
     },
+    sortAssending: function () {
+        this.items.sort(function (a, b) { return a.price - b.price; });
+    },
+    sortdescending: function () {
+        this.items.sort(function (a, b) { return b.price - a.price; });
+    },
     renderItem: function (itemOnDom) {
         console.log(itemOnDom);
         console.log(this.items);
         var itemHtml = '';
         this.items.forEach(function (item) {
-            itemHtml = "<div class=\"card\"><p>" + item.company + ":" + item.price + "</p></div>";
+            itemHtml += "<div class=\"card\"><p>" + item.company + ":" + item.price + "</p></div>";
         });
-        itemOnDom.innerHTML += itemHtml;
+        itemOnDom.innerHTML = itemHtml;
     }
 };
+function handleAsc(event) {
+    event.preventDefault();
+    zapitem.sortAssending();
+    zapitem.renderItem(root);
+}
+function handleDesc(event) {
+    event.preventDefault();
+    zapitem.sortdescending();
+    zapitem.renderItem(root);
+}
 function handlesubmit(event) {
     event.preventDefault();
     // console.log(event);
