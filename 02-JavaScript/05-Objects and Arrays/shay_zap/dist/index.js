82afea524f115bcf479477bdc4582b4b729f7b2e
shayfoyer <93338189+shayfoyer@users.noreply.github.com>
2022-01-23 23:02:33 +0200

    shay

---
+++ 02-JavaScript/05-Objects and Arrays/shay_zap/dist/index.js
@@ -0,0 +1,60 @@
+var myProduct = {
+    zap: [],
+    addItem: function (products) {
+        this.zap.push(products);
+    },
+    sortAsc: function () {
+        this.zap.sort(function (x, y) { return y.price - x.price; });
+    },
+    sortDesc: function () {
+        this.zap.sort(function (x, y) { return x.price - y.price; });
+    },
+    renderZap: function (domElement) {
+        var html = "";
+        this.zap.forEach(function (element) {
+            html += "<div class = 'card'>\n        <p>product: " + element.product + ", price: " + element.price + "</p></div>";
+        });
+        domElement.innerHTML = html;
+    },
+    priceUnder: function (item) {
+        if (item == "") {
+            this.renderZap(this.Zap);
+            return;
+        }
+        var listFilter = this.zap.filter(function (element) { return element.price <= (item); });
+        console.log(listFilter);
+        myProduct.renderZap(listFilter);
+    }
+};
+function handlePriceChange(ev) {
+    ev.preventDefault();
+    console.dir(ev.target);
+    var product = ev.target.elements.title.value;
+    var price = ev.target.elements.price.valueAsNumber;
+    myProduct.addItem({ product: product, price: price });
+    var rootZap = document.getElementById("rootZap");
+    myProduct.renderZap(rootZap);
+    ev.target.reset();
+}
+function sort(ev) {
+    switch (ev.target.id) {
+        case "asc":
+            myProduct.sortAsc();
+            break;
+        case "desc": myProduct.sortDesc();
+    }
+    myProduct.renderZap(rootZap);
+}
+function handleFilter(ev) {
+    myProduct.priceUnder(ev.target.value);
+    console.log(ev.target.value);
+}
+myProduct.addItem({ product: 'tire', price: 105 });
+myProduct.addItem({ product: 'med', price: 15 });
+myProduct.addItem({ product: 'coockie', price: 20 });
+myProduct.addItem({ product: 'phone', price: 100 });
+// myProduct.sortAsc()
+// myProduct.sortDesc()
+console.log(myProduct);
+var rootZap = document.getElementById("rootZap");
+myProduct.renderZap(rootZap);
---
+++ 02-JavaScript/05-Objects and Arrays/shay_zap/index.ts
@@ -0,0 +1,89 @@
+interface Obj{
+    zap:Array< Product >,
+    addItem?:any,
+    sortAsc?:any,
+    sortDesc?:any,
+    renderZap?:any,
+     priceUnder
+}
+interface Product {
+    product: string;
+    price: number;
+}
+
+const myProduct: Obj={
+    zap:[],
+    addItem(products:Product){
+        
+        this.zap.push(products);
+    },
+    sortAsc(){
+        this.zap.sort((x, y)=> y.price -x.price)
+      
+    },
+    sortDesc(){
+        this.zap.sort((x, y)=> x.price -y.price)
+    },
+    renderZap(domElement){
+    let html = "";
+
+    this.zap.forEach(element => {
+        html+=`<div class = 'card'>
+        <p>product: ${element.product}, price: ${element.price}</p></div>`
+    });
+    domElement.innerHTML = html;
+},
+ priceUnder(item){
+     
+   if(item == ""){
+       this.renderZap(this.Zap);
+       return;
+   }
+    let listFilter = this.zap.filter(element=> element.price <= (item));
+    console.log(listFilter);
+    
+    myProduct.renderZap(listFilter);
+
+}
+};
+
+
+
+function handlePriceChange(ev) {
+    ev.preventDefault();
+  
+    console.dir(ev.target);
+    const product = ev.target.elements.title.value;
+    const price = ev.target.elements.price.valueAsNumber;
+
+    myProduct.addItem({ product, price });
+    const rootZap= document.getElementById("rootZap");
+    myProduct.renderZap(rootZap);
+    ev.target.reset();
+  }
+function sort(ev){
+    switch(ev.target.id){
+        case `asc`: myProduct.sortAsc();
+        break;
+        case `desc`: myProduct.sortDesc();
+    }
+    myProduct.renderZap(rootZap)
+}
+
+function handleFilter(ev){
+myProduct.priceUnder(ev.target.value)
+console.log(ev.target.value);
+    
+}
+
+
+  myProduct.addItem({product: 'tire', price: 105});
+myProduct.addItem({product: 'med', price: 15});
+myProduct.addItem({product: 'coockie',price: 20});
+myProduct.addItem({product: 'phone',price: 100});
+// myProduct.sortAsc()
+// myProduct.sortDesc()
+console.log(myProduct);
+const rootZap = document.getElementById("rootZap");
+ myProduct.renderZap(rootZap);
+
