f1d3021635dceea710e1826ae6bcd18c5c66d204
Tal Yaron <tal.yaron@gmail.com>
2022-01-27 19:21:29 +0200

    update

--- 02-JavaScript/09-formsAndObjects/02-update/dist/index.js
+++ 02-JavaScript/09-formsAndObjects/02-update/dist/index.js
@@ -1,4 +1,4 @@
-console.log('ok');
+console.log("ok");
 var uid = function () {
     return Date.now().toString(36) + Math.random().toString(36).substr(2);
 };
@@ -15,13 +15,21 @@ var myZap = {
     deleteItem: function (id) {
         this.data = this.data.filter(function (item) { return item.id !== id; });
     },
+    updateItem: function (id, newName) {
+        //find index
+        var index = this.data.findIndex(function (item) { return item.id === id; });
+        //update the spesific item's name
+        if (index >= 0) {
+            this.data[index].name = newName;
+        }
+    },
     filterByType: function (type) {
         return this.data.filter(function (item) { return item.type === type; });
     },
     render: function (list, domElement) {
-        var html = '';
+        var html = "";
         list.forEach(function (item) {
-            html += "<div class=\"item\">\n            <p>" + item.name + "</p>\n            <button onclick=\"handleDelete('" + item.id + "')\">Delete</button>\n            </div>";
+            html += "<div class=\"item\">\n            <p>" + item.name + "</p>\n            <button onclick=\"handleDelete('" + item.id + "')\">Delete</button>\n            <form onsubmit=\"handleUpdate(event, '" + item.id + "')\">\n            <input type=\"text\" name=\"newName\" placeholder=\"new name\">\n            <button type=\"submit\">Update</button>\n            </form>\n            </div>";
         });
         domElement.innerHTML = html;
     },
@@ -33,24 +41,24 @@ var myZap = {
         this.render(filterd, domElement);
     }
 };
-myZap.addItem('lenovo 50', 'computer');
-myZap.addItem('lenovo 60', 'computer');
-myZap.addItem('lenovo 70', 'computer');
-myZap.addItem('lenovo 80', 'computer');
-myZap.addItem('lenovo 100', 'computer');
-myZap.addItem('lenovo 120', 'computer');
-myZap.addItem('shirt', 'clothes');
-myZap.addItem('pants', 'clothes');
-myZap.addItem('skirt', 'clothes');
-myZap.addItem('trousers', 'clothes');
-myZap.addItem('scarf', 'clothes');
+myZap.addItem("lenovo 50", "computer");
+myZap.addItem("lenovo 60", "computer");
+myZap.addItem("lenovo 70", "computer");
+myZap.addItem("lenovo 80", "computer");
+myZap.addItem("lenovo 100", "computer");
+myZap.addItem("lenovo 120", "computer");
+myZap.addItem("shirt", "clothes");
+myZap.addItem("pants", "clothes");
+myZap.addItem("skirt", "clothes");
+myZap.addItem("trousers", "clothes");
+myZap.addItem("scarf", "clothes");
 console.dir(myZap.data);
-var root = document.getElementById('root');
+var root = document.getElementById("root");
 myZap.renderAllData(root);
 function handleSelect(ev) {
-    var root = document.getElementById('root');
+    var root = document.getElementById("root");
     var type = ev.target.value;
-    if (type === 'all') {
+    if (type === "all") {
         myZap.renderAllData(root);
     }
     else {
@@ -59,8 +67,14 @@ function handleSelect(ev) {
     }
 }
 function handleDelete(id) {
-    console.log(id);
-    var root = document.getElementById('root');
+    var root = document.getElementById("root");
     myZap.deleteItem(id);
+    myZap.renderAllData(root);
+}
+function handleUpdate(ev, itemId) {
+    ev.preventDefault();
+    var newName = ev.target.elements.newName.value;
+    myZap.updateItem(itemId, newName);
+    var root = document.getElementById("root");
     myZap.renderAllData(root);
 }
--- 02-JavaScript/09-formsAndObjects/02-update/index.ts
+++ 02-JavaScript/09-formsAndObjects/02-update/index.ts
@@ -1,93 +1,115 @@
-console.log('ok');
-const uid = function(){
-    return Date.now().toString(36) + Math.random().toString(36).substr(2);
+console.log("ok");
+const uid = function () {
+  return Date.now().toString(36) + Math.random().toString(36).substr(2);
 };
 
 interface Zap {
-    id?:number;
-    data: Array<Item>
-    addItem(name: string, type: "computer" | "clothes");
-    render(list: Array<Item>, domElement: any);
-    renderAllData(domElement: any);
-    filterByType(type: "computer" | "clothes");
-    renderByType(type: "computer" | "clothes", domElement: any);
-    deleteItem(id:string);
+  id?: number;
+  data: Array<Item>;
+  addItem(name: string, type: "computer" | "clothes");
+  render(list: Array<Item>, domElement: any);
+  renderAllData(domElement: any);
+  filterByType(type: "computer" | "clothes");
+  renderByType(type: "computer" | "clothes", domElement: any);
+  deleteItem(id: string);
+  updateItem(id:string, newName:string);
 }
 
 interface Item {
-    id:number;
-    name: string,
-    type: "computer" | "clothes"
+  id: number;
+  name: string;
+  type: "computer" | "clothes";
 }
 
 const myZap: Zap = {
-    // id:0,
-    data: [],
-    addItem(name, type) {
-        const id = uid();
-        this.data.push({id, name, type });
+  // id:0,
+  data: [],
+  addItem(name, type) {
+    const id = uid();
+    this.data.push({ id, name, type });
 
-        //sequntial id
-        // this.data.push({id:this.id, name, type });
-        // this.id++;
-    },
-    deleteItem(id){
-        this.data = this.data.filter(item=>item.id !== id);
-    },
-    filterByType(type) {
-        return this.data.filter(item => item.type === type);
-    },
-    render(list, domElement) {
-        let html = ''
-        list.forEach(item => {
-            html += `<div class="item">
+    //sequntial id
+    // this.data.push({id:this.id, name, type });
+    // this.id++;
+  },
+  deleteItem(id) {
+    this.data = this.data.filter((item) => item.id !== id);
+  },
+  updateItem(id, newName) {
+    //find index
+    const index = this.data.findIndex((item) => item.id === id);
+    //update the spesific item's name
+    if (index >= 0) {
+      this.data[index].name = newName;
+    }
+  },
+  filterByType(type) {
+    return this.data.filter((item) => item.type === type);
+  },
+  render(list, domElement) {
+    let html = "";
+    list.forEach((item) => {
+      html += `<div class="item">
             <p>${item.name}</p>
             <button onclick="handleDelete('${item.id}')">Delete</button>
-            </div>`
-        })
+            <form onsubmit="handleUpdate(event, '${item.id}')">
+            <input type="text" name="newName" placeholder="new name">
+            <button type="submit">Update</button>
+            </form>
+            </div>`;
+    });
 
-        domElement.innerHTML = html;
-    },
-    renderAllData(domElement) {
-        this.render(this.data, domElement);
-    },
-    renderByType(type, domElement) {
-        const filterd = this.filterByType(type);
-        this.render(filterd, domElement);
-    }
-}
+    domElement.innerHTML = html;
+  },
+  renderAllData(domElement) {
+    this.render(this.data, domElement);
+  },
+  renderByType(type, domElement) {
+    const filterd = this.filterByType(type);
+    this.render(filterd, domElement);
+  },
+};
 
-myZap.addItem('lenovo 50', 'computer');
-myZap.addItem('lenovo 60', 'computer');
-myZap.addItem('lenovo 70', 'computer');
-myZap.addItem('lenovo 80', 'computer');
-myZap.addItem('lenovo 100', 'computer');
-myZap.addItem('lenovo 120', 'computer');
-myZap.addItem('shirt', 'clothes');
-myZap.addItem('pants', 'clothes');
-myZap.addItem('skirt', 'clothes');
-myZap.addItem('trousers', 'clothes');
-myZap.addItem('scarf', 'clothes');
+myZap.addItem("lenovo 50", "computer");
+myZap.addItem("lenovo 60", "computer");
+myZap.addItem("lenovo 70", "computer");
+myZap.addItem("lenovo 80", "computer");
+myZap.addItem("lenovo 100", "computer");
+myZap.addItem("lenovo 120", "computer");
+myZap.addItem("shirt", "clothes");
+myZap.addItem("pants", "clothes");
+myZap.addItem("skirt", "clothes");
+myZap.addItem("trousers", "clothes");
+myZap.addItem("scarf", "clothes");
 
 console.dir(myZap.data);
-const root = document.getElementById('root');
-myZap.renderAllData(root)
-
+const root = document.getElementById("root");
+myZap.renderAllData(root);
 
 function handleSelect(ev) {
-    const root = document.getElementById('root');
-    const type = ev.target.value;
-    if (type === 'all') {
-        myZap.renderAllData(root);
-    } else {
-        console.log(type)
-        myZap.renderByType(type, root);
-    }
+  const root = document.getElementById("root");
+  const type = ev.target.value;
+  if (type === "all") {
+    myZap.renderAllData(root);
+  } else {
+    console.log(type);
+    myZap.renderByType(type, root);
+  }
 }
 
-function handleDelete(id){
-    console.log(id)
-    const root = document.getElementById('root');
-    myZap.deleteItem(id);
-    myZap.renderAllData(root);  
+function handleDelete(id) {
+ 
+  const root = document.getElementById("root");
+  myZap.deleteItem(id);
+  myZap.renderAllData(root);
+}
+
+function handleUpdate(ev:any, itemId:string){
+    ev.preventDefault();
+    const newName:string = ev.target.elements.newName.value;
+    myZap.updateItem(itemId, newName);
+    const root = document.getElementById("root");
+    
+    myZap.renderAllData(root);
+    
 }
