086655871eaeb93ec4a3681ee2bc16499f1c0fbf
Michael Dubovyk <93338459+mishalon@users.noreply.github.com>
2022-01-30 19:37:16 +0200

    1

--- תרגול/misha/30.1/index.ts
+++ תרגול/misha/30.1/index.ts
@@ -25,8 +25,30 @@
 // console.log(r);
 
 let str = prompt('input');
+let sum: number = 0;
 
 for (var i = 0; i < str.length; i++) {
-    console.log(i);
-  }
+    console.log(str.charAt(i));
+    sum += +str.charAt(i);
+}
+console.log('-----------');
 
+console.log(`sum is : ${sum}`);
+
+console.log('-----------');
+
+console.log(str.charAt(str.length-1));
+for (var i = 1; i < str.length - 1; i++) {
+    console.log(str.charAt(i));
+}
+console.log(str.charAt(0));
+
+console.log('-----------');
+
+// for (var i = str.length-1 ; i > 0; i--) {
+//     console.log(str.charAt(i));
+// }
+
+// str.split("").reverse().join("")
+
+console.log(str.split("").reverse().join(""));
