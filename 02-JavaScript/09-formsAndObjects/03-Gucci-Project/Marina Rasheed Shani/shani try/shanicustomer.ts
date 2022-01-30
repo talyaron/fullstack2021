c8b0627719c8ab0d45c4d71b56e1d3c566d72f6d
ShaniRom <93940556+ShaniRom@users.noreply.github.com>
2022-01-30 18:05:39 +0200

    gucci project

--- 02-JavaScript/09-formsAndObjects/03-Gucci-Project/Marina Rasheed Shani/shani try/shanicustomer.ts
+++ 02-JavaScript/09-formsAndObjects/03-Gucci-Project/Marina Rasheed Shani/shani try/shanicustomer.ts
@@ -1,24 +1,45 @@
-interface handbags{
-    listOfBags:Array<bag>;
-    addItem?:any;
-    sortByAscending?:any;
-    sortByDescending?:any;
-    filterLowerThan?:any;
+interface gucci {
+    listOfItems: Array<item>;
+    addItem(nameOfItem: string, type: "ring" | "bag", price: number);
+    filterByType(type: "ring" | "bag"),
+    //filterMaxPrice(price: number),
+    sortByAscending?: any;
+    sortByDescending?: any;
+    filterLowerThan?: any;
 }
 
-interface bag{
-    nameOfBag:string;
-    price:number;
-    id?:string;
-    type?:
+interface item {
+    nameOfItem: string;
+    price: number;
+    id?: string;
+    type?: "ring" | "bag"
 }
 
 
-const gucciBags:handbags={
-    listOfBags:[
-        {nameOfBag:"gucci gold bag",price:2000},
-        {nameOfBag:"gucci silver bag",price:2300}
+const uid = function(){
+    return Date.now().toString(36) + Math.random().toString(36).substr(2);
+};
+
+
+const gucciStore: gucci = {
+    listOfItems: [
+        { nameOfItem: "gucci gold bag", price: 2000 },
+        { nameOfItem: "gucci silver bag", price: 2300 }
     ],
-    
+    addItem(nameOfItem,price,type){
+        const id=uid();
+        this.listOfItems.push({nameOfItem,price,id,type});
+
+    },
+    filterByType(type){
+        
+
+    }
+
 
 }
+
+gucciStore.addItem("green gucci purse","bag",1000)
+gucciStore.addItem("gold diamond ","ring",2000)
+gucciStore.addItem("solid gold fannypack","bag",100000)
+console.log(gucciStore)
