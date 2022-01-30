13e8ca0e3279596a833519554a83f8bc40aab94c
Michael Dubovyk <93338459+mishalon@users.noreply.github.com>
2022-01-30 19:15:38 +0200

    1

---
+++ תרגול/misha/30.1/dist/index.js
@@ -0,0 +1,20 @@
+// const a: number = 15;
+// const b: number = 6;
+// let r = 0;
+// function gcd(x, y) {
+//     if (a && b > 0) {
+//         if (a > b) {
+//             r = a % b;
+//         }
+//         else {
+//             r = b % a;
+//         }
+//         return r;
+//     }
+// }
+// r = gcd(a, b);
+// console.log(r);
+var str = prompt('input');
+for (var i = 0; i < str.length; i++) {
+    console.log(i);
+}
---
+++ תרגול/misha/30.1/index.ts
@@ -0,0 +1,32 @@
+
+
+// const a: number = 15;
+// const b: number = 6;
+// let r = 0;
+
+// function gcd(x, y) {
+
+//     if (a && b > 0) {
+
+//         if (a > b) {
+//             r = a % b;
+//         }
+
+//         else {
+//             r = b % a;
+//         }
+
+//         return r;
+//     }
+// }
+
+// r = gcd(a, b);
+
+// console.log(r);
+
+let str = prompt('input');
+
+for (var i = 0; i < str.length; i++) {
+    console.log(i);
+  }
+
