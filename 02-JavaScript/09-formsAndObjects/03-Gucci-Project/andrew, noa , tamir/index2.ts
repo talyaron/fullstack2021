c4347675d3b1b226db835dcbc55838e8d4375b4e
AndrewLishafay <andrew567234@gmail.com>
2022-01-30 21:50:33 +0200

    currency half done

--- 02-JavaScript/09-formsAndObjects/03-Gucci-Project/andrew, noa , tamir/index2.ts
+++ 02-JavaScript/09-formsAndObjects/03-Gucci-Project/andrew, noa , tamir/index2.ts
@@ -9,4 +9,22 @@ function handleNavClick(){
     console.log('click');
 }
 
+let currencyButton =document.querySelector('#currency-button');
+let currencySelector = document.querySelector('.currency-selector');
+let currencyOption = document.querySelectorAll('.currency');
+currencyButton.addEventListener('click',handleCurrencySelectorClick);
+function handleCurrencySelectorClick(){
+    currencySelector.classList.toggle("visible");
+}
+currencyOption.forEach(currency => {
+    currency.addEventListener('click', handleCurrencyOptionClick)
+    
+});
+function handleCurrencyOptionClick(ev){
+    let id = ev.target.id;
+    console.log(id);
+    currencyButton.innerHTML = `ISRAEL (${id})`;
+    
+}
+
 
