240ebf29b14132664566dfa4add21609f92f00a4
leon <lmalul13@gmail.com>
2022-01-31 10:26:01 +0200

    342

--- 02-JavaScript/09-formsAndObjects/03-Gucci-Project/yahia,michael,leon/dist/index.js
+++ 02-JavaScript/09-formsAndObjects/03-Gucci-Project/yahia,michael,leon/dist/index.js
@@ -1,1 +1,14 @@
-console.log("hi");
+var nikeItems = {
+    items: [],
+    additems: function (list) {
+        this.items.push(list);
+        console.log(this.items);
+    }
+};
+function handleSubmit(event) {
+    event.preventDefault();
+    var name = event.target.elements.description.value;
+    var price = event.target.elements.price.value;
+    nikeItems.additems({ name: name, price: price });
+    event.target.reset();
+}
--- 02-JavaScript/09-formsAndObjects/03-Gucci-Project/yahia,michael,leon/index.ts
+++ 02-JavaScript/09-formsAndObjects/03-Gucci-Project/yahia,michael,leon/index.ts
@@ -1,1 +1,32 @@
-console.log("hi");
+interface Object {
+    items: Array<namePrice>,
+    additems(list:namePrice),
+
+}
+
+interface namePrice {
+    name: string;
+    price: number;
+}
+
+let nikeItems: object = {
+    items: [],
+    additems(list:namePrice){
+       
+        this.items.push(list)
+        console.log(this.items)
+    },
+    
+    
+}
+
+function handleSubmit(event) {
+    event.preventDefault();
+    const name = event.target.elements.description.value
+    const price = event.target.elements.price.value
+    nikeItems.additems({name, price})
+
+
+
+    event.target.reset()
+}
