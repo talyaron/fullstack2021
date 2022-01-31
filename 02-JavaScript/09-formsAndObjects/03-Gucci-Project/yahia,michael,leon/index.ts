36e775648686337ae32d4d946cedcd2748a2ce6c
yoelKraitman <yoelkraitman@gmail.com>
2022-01-31 12:40:07 +0200

    kjjbj

--- 02-JavaScript/09-formsAndObjects/03-Gucci-Project/yahia,michael,leon/index.ts
+++ 02-JavaScript/09-formsAndObjects/03-Gucci-Project/yahia,michael,leon/index.ts
@@ -1,33 +1,61 @@
+const uid = function () {
+    return Date.now().toString(36) + Math.random().toString(36).substr(2);
+};
+
 interface Object {
     items: Array<namePrice>,
-    additems(list:namePrice),
+    additems(name: string, price: number),
     sortAsc(),
     sortDes(),
-    renderAllData(root:any)
-    render(root:any,list:namePrice),
+    deleteItem(idItem: string),
+    renderAllData(root: any)
+    render(root: any, list: namePrice),
+
 }
 
 interface namePrice {
     name: string;
     price: number;
+    id: string;
 }
 
 let nikeItems: object = {
     items: [],
-    additems(list:namePrice){
-        this.items.push(list)
-        
-    },   
-    sortAsc(){
-        this.items.sort((a,b)=>a.price-b.price)
-        
+    additems(name, price) {
+
+
+        const idItem = uid();
+
+        this.items.push({ name, price, idItem })
+        console.log(this.items)
+
+
     },
-    sortDes(){
-        this.items.sort((a,b)=>b.price-a.price)
-        
+    sortAsc() {
+        this.items.sort((a, b) => a.price - b.price)
+
     },
-    renderAllData(root:any){
-        
+    sortDes() {
+        this.items.sort((a, b) => b.price - a.price)
+
+    },
+    deleteItem(idItem) {
+        this.items = this.items.filter(item => item.idItem !== idItem)
+        console.log(this.items);
+    },
+    renderAllData(root: any) {
+        const list = this.items;
+
+        this.render(root, list)
+    },
+    render(root: any, list) {
+        let html: string = '';
+
+        list.forEach(item => {
+            html += `<div class = 'card'> <p>${item.name}: ${item.price}</p></div>
+            <button onclick="handleDelete('${item.idItem}')">delete</button>`
+        })
+        root.innerHTML = html;
     }
 }
 
@@ -35,7 +63,7 @@ function handleSubmit(event) {
     event.preventDefault();
     const name = event.target.elements.description.value
     const price = event.target.elements.price.value
-    nikeItems.additems({name, price})
+    nikeItems.additems(name, price)
 
     const root = document.getElementById('root');
     nikeItems.renderAllData(root);
@@ -45,11 +73,25 @@ function handleSubmit(event) {
 
 }
 
-function handleAsce(){
-nikeItems.sortAsc()
+function handleAsce() {
+    nikeItems.sortAsc()
+    const root = document.getElementById('root');
+
+    nikeItems.renderAllData(root);
+
 
 }
-function handleDesce(){
+function handleDesce() {
     nikeItems.sortDes()
+    const root = document.getElementById('root');
+
+    nikeItems.renderAllData(root);
 }
 
+
+function handleDelete(id) {
+
+    nikeItems.deleteItem(id);
+    const root = document.getElementById('root');
+    nikeItems.renderAllData(root);
+}
