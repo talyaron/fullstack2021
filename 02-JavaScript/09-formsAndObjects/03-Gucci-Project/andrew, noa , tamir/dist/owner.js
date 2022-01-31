f91b88289c4c7e7bd465969c7a522a6fe7235064
noachasha <noachasha@gmail.com>
2022-01-31 19:30:18 +0200

    up

--- 02-JavaScript/09-formsAndObjects/03-Gucci-Project/andrew, noa , tamir/dist/owner.js
+++ 02-JavaScript/09-formsAndObjects/03-Gucci-Project/andrew, noa , tamir/dist/owner.js
@@ -3,14 +3,7 @@ var uid = function () {
     return Date.now().toString(36) + Math.random().toString(36).substr(2);
 };
 var stock = {
-    items: [
-        { name: 'DOUGLAS DAY 41', price: 45, img: "https://aviatorwatch.swiss/assets/catalog/douglas-day-date-41/AVIATOR-WATCH--DOUGLAS-DAY-DATE-41--V.3.35.0.278.4.webp" },
-        { name: 'DOUGLAS DAY-DATE 41', price: 55, img: 'https://aviatorwatch.swiss/assets/catalog/douglas-day-date-41/AVIATOR-WATCH--DOUGLAS-DAY-DATE-41--V.3.35.0.276.4.webp' },
-        { name: 'DOUGLAS', price: 35, img: 'https://aviatorwatch.swiss/assets/catalog/douglas-dc-3/AVIATOR-WATCH--DOUGLAS-DC-3--V.3.32.2.237.4.webp' },
-        { name: 'tamir', price: 57, img: 'https://aviatorwatch.swiss/assets/catalog/douglas-day-date-41/AVIATOR-WATCH--DOUGLAS-DAY-DATE-41--V.3.35.2.277.4.webp' },
-        { name: 'avi', price: 140, img: 'https://aviatorwatch.swiss/assets/catalog/douglas-day-date-41/AVIATOR-WATCH--DOUGLAS-DAY-DATE-41--V.3.35.2.280.4.webp' },
-        { name: 'nir', price: 60, img: 'https://aviatorwatch.swiss/assets/catalog/douglas-day-date-41/AVIATOR-WATCH--DOUGLAS-DAY-DATE-41--V.3.35.0.274.4.webp' }
-    ],
+    items: [],
     addItem: function (newItem) {
         newItem.id = uid();
         this.items.push(newItem);
@@ -18,11 +11,14 @@ var stock = {
         this.renderStock(this.items);
     },
     deleteItem: function (id) {
+        this.items = this.items.filter(function (item) { return item.id !== id; });
+    },
+    updateItem: function (item) {
     },
     renderStock: function (items) {
         var HTML = '';
         items.forEach(function (item) {
-            HTML += " <div class=\"card\"\n            <p> name: " + item.name + "</p>\n            <p> price: " + item.price + "</p>\n            <img src=\"" + item.img + "\">\n            <p> group: " + item.group + "</p>\n            <p> collection: " + item.Collection + "</p>\n            <p> price: " + item["function"] + "</p>\n            <p> price: " + item.movement + "</p>\n            <p> price: " + item["case"] + "</p>\n            <p> price: " + item.diameter + "</p>\n            <p> price: " + item.dial + "</p>\n            <p> price: " + item.bracelet + "</p>\n           </div> ";
+            HTML += " <div class=\"card\"\n            <p> name: " + item.name + "</p>\n            <p> price: " + item.price + "</p>\n            <img src=\"" + item.img + "\">\n            <p> group: " + item.group + "</p>\n            <p> collection: " + item.Collection + "</p>\n            <p> function: " + item["function"] + "</p>\n            <p> movement: " + item.movement + "</p>\n            <p> case: " + item["case"] + "</p>\n            <p> diameter: " + item.diameter + "</p>\n            <p> dial: " + item.dial + "</p>\n            <p> bracelet: " + item.bracelet + "</p>\n            <button onclick=\"handleDelete('" + item.id + "')\">Delete</button>\n            <button onclick=\"handleUpdate()\">Update</button>\n           </div> ";
         });
         var rootHTML = document.getElementById('root');
         rootHTML.innerHTML = HTML;
@@ -72,3 +68,8 @@ function handleSubmit(ev) {
     }
     stock.addItem(newItem);
 }
+function handleDelete(id) {
+    stock.deleteItem(id);
+    stock.renderStock(stock.items);
+}
+stock.addItem({ name: 'DOUGLAS DAY 41', price: 45, img: "https://aviatorwatch.swiss/assets/catalog/douglas-day-date-41/AVIATOR-WATCH--DOUGLAS-DAY-DATE-41--V.3.35.0.278.4.webp", group: "AUTOMATIC WATCHES", Collection: "BRISTOL", "function": "Chronograph", movement: "Quartz", "case": "Gold PVD", diameter: "42 mm", dial: "Ivory", bracelet: "Leather", id: 0 });
