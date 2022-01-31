7a9cb955e38c1382ef5589cd3d0196214440522f
artur121255 <arturyaski96@gmail.com>
2022-01-31 20:04:36 +0200

    s

--- 02-JavaScript/09-formsAndObjects/03-Gucci-Project/Yossi_Yael_Will_Artur/dist/index.js
+++ 02-JavaScript/09-formsAndObjects/03-Gucci-Project/Yossi_Yael_Will_Artur/dist/index.js
@@ -1,1 +1,3 @@
-innerHeight;
+var uid = function () {
+    return Date.now().toString(36) + Math.random().toString(36).substr(2);
+};
--- 02-JavaScript/09-formsAndObjects/03-Gucci-Project/Yossi_Yael_Will_Artur/index.ts
+++ 02-JavaScript/09-formsAndObjects/03-Gucci-Project/Yossi_Yael_Will_Artur/index.ts
@@ -1,1 +1,6 @@
-innerHeight
+const uid = function(){
+    return Date.now().toString(36) + Math.random().toString(36).substr(2);
+};
+interface Gucci {
+    
+}
