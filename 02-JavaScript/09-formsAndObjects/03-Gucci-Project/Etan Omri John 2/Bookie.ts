ace8dd39721aeb327748b48d95fc21e3230fbb95
Etanhey <etan@heyman.net>
2022-02-03 09:58:21 -0600

    .

---
+++ 02-JavaScript/09-formsAndObjects/03-Gucci-Project/Etan Omri John 2/Bookie.ts
@@ -0,0 +1,9 @@
+let backToTop = document.querySelector('[data-back-to-top]');
+console.dir(backToTop);
+function handleTop(ev){
+    ev.preventDefault();
+    document.documentElement.scrollTop = 0   
+}
+function scrollToTop(){
+
+}
---
+++ 02-JavaScript/09-formsAndObjects/03-Gucci-Project/Etan Omri John 2/dist/Bookie.js
@@ -0,0 +1,8 @@
+var backToTop = document.querySelector('[data-back-to-top]');
+console.dir(backToTop);
+function handleTop(ev) {
+    ev.preventDefault();
+    document.documentElement.scrollTop = 0;
+}
+function scrollToTop() {
+}
