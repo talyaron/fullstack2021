05ea99bbaa7682909ae84133f08ec841ab6da31e
ShaniRom <93940556+ShaniRom@users.noreply.github.com>
2022-02-08 16:29:17 +0200

    gucci items

--- 02-JavaScript/09-formsAndObjects/03-Gucci-Project/Marina Rasheed Shani/rasheed-gucci-/dist/index.js
+++ 02-JavaScript/09-formsAndObjects/03-Gucci-Project/Marina Rasheed Shani/rasheed-gucci-/dist/index.js
@@ -17,6 +17,58 @@ var gucci = {
             type: "dresses"
         },
         {
+            name: "Silk mohair sweater with feathers ",
+            price: 2500,
+            img: "https://media.gucci.com/style/DarkGray_South_0_160_316x316/1640195126/670625_XKB1V_1043_001_100_0000_Light-Silk-mohair-sweater-with-feathers.jpg",
+            department: "clothes",
+            gender: "women",
+            type: "sweaters"
+        },
+        {
+            name: "Leather ankle boot with belt",
+            price: 1190,
+            img: "https://media.gucci.com/style/DarkGray_South_0_160_316x316/1537468210/550036_DKS00_1000_001_100_0000_Light-Leather-ankle-boot-with-belt.jpg",
+            department: "shoes",
+            gender: "women",
+            type: "boots"
+        },
+        {
+            name: "Women's ankle boot with Horsebit",
+            price: 790,
+            img: "https://media.gucci.com/style/DarkGray_South_0_160_316x316/1633042815/670407_JCCP0_3154_001_100_0000_Light-Womens-ankle-boot-with-Horsebit.jpg",
+            department: "shoes",
+            gender: "women",
+            type: "boots"
+        },
+        {
+            name: "Baby cat print linen coat",
+            price: 1150,
+            img: "https://media.gucci.com/style/DarkGray_South_0_160_316x316/1634315425/680518_ZAIDY_9376_001_100_0000_Light-Baby-cat-print-cotton-dress.jpg",
+            department: "children",
+            type: "clothes"
+        },
+        {
+            name: "Double G cotton jacket",
+            price: 635,
+            img: "https://media.gucci.com/style/DarkGray_South_0_160_316x316/1633043710/671075_XWARC_3472_001_100_0000_Light-Childrens-Double-G-cotton-jacket.jpg",
+            department: "children",
+            type: "clothes"
+        },
+        {
+            name: "GG cotton hat with ear flaps",
+            price: 295,
+            img: "https://media.gucci.com/style/DarkGray_South_0_160_316x316/1632819610/683064_3HAG1_4969_001_100_0000_Light-Baby-GG-cotton-hat-with-ear-flaps.jpg",
+            department: "children",
+            type: "hats"
+        },
+        {
+            name: "Straw effect hat with ribbon",
+            price: 300,
+            img: "https://media.gucci.com/style/DarkGray_South_0_160_316x316/1634251518/684920_3HAHA_9266_001_100_0000_Light-Baby-straw-effect-hat-with-ribbon.jpg",
+            department: "children",
+            type: "hats"
+        },
+        {
             name: "The North Face x",
             price: 950,
             img: "https://media.gucci.com/style/DarkGray_South_0_160_316x316/1638553515/663766_ZLX45_3475_001_100_0000_Light-The-North-Face-x-Gucci-pant.jpg",
@@ -33,6 +85,14 @@ var gucci = {
             type: "shoes"
         },
         {
+            name: "GG leather pant",
+            price: 4600,
+            img: "https://media.gucci.com/style/DarkGray_South_0_160_316x316/1636738256/669071_XNAPN_1000_001_100_0000_Light-GG-leather-pant.jpg",
+            department: "clothes",
+            gender: "men",
+            type: "pants"
+        },
+        {
             name: "Cotton piquet polo ",
             price: 690,
             img: "https://media.gucci.com/style/DarkGray_South_0_160_316x316/1634749231/653380_XJDF8_3495_001_100_0000_Light-Cotton-piquet-polo-with-Interlocking-G.jpg",
@@ -41,18 +101,12 @@ var gucci = {
             type: "shirts"
         },
         {
-            name: "Double G flower",
-            price: 390,
-            img: "https://media.gucci.com/style/DarkGray_South_0_160_316x316/1522084508/527394_J8474_8517_001_100_0000_Light-Double-G-flower-ring.jpg",
-            department: "jewelry",
-            type: "rings"
-        },
-        {
-            name: "Gucci Diana small crocodile",
-            price: 35000,
-            img: "https://media.gucci.com/style/DarkGray_South_0_160_316x316/1618321510/660195_EZINT_2582_001_067_0000_Light-Gucci-Diana-small-crocodile-tote-bag.jpg",
-            department: "bags",
-            type: "purses"
+            name: "GG kaleidoscope silk bowling shirt",
+            price: 1250,
+            img: "https://media.gucci.com/style/DarkGray_South_0_160_316x316/1638550829/675446_ZAH3W_1128_001_100_0000_Light-GG-kaleidoscope-silk-bowling-shirt.jpg",
+            department: "clothes",
+            gender: "men",
+            type: "shirts"
         },
         {
             name: "Grip watch, 38mm",
@@ -62,19 +116,81 @@ var gucci = {
             type: "rolex"
         },
         {
-            name: "Leather ankle boot with belt",
-            price: 1190,
-            img: "https://media.gucci.com/style/DarkGray_South_0_160_316x316/1537468210/550036_DKS00_1000_001_100_0000_Light-Leather-ankle-boot-with-belt.jpg",
-            department: "shoes",
-            gender: "women",
-            type: "boots"
+            name: "Grip watch, 35mm",
+            price: 1700,
+            img: "https://media.gucci.com/style/DarkGray_South_0_160_316x316/1567584903/584271_ICBA0_8576_001_100_0000_Light-Grip-watch-35mm.jpg",
+            department: "watches",
+            type: "omega"
         },
         {
-            name: "Baby cat print linen coat",
+            name: "GUCCI 25H watch, 40mm",
+            price: 9500,
+            img: "https://media.gucci.com/style/DarkGray_South_0_160_316x316/1627285507/673127_I1600_1108_001_100_0000_Light-GUCCI-25H-watch-40mm.jpg",
+            department: "watches",
+            type: "rolex"
+        },
+        {
+            name: "Gucci Tiger G-Timeless, 38 mm",
+            price: 1020,
+            img: "https://media.gucci.com/style/DarkGray_South_0_160_316x316/1634838345/692078_ICZA0_8521_001_100_0000_Light-Gucci-Tiger-G-Timeless-watch-38-mm.jpg",
+            department: "watches",
+            type: "omega"
+        },
+        {
+            name: "Double G flower",
+            price: 390,
+            img: "https://media.gucci.com/style/DarkGray_South_0_160_316x316/1522084508/527394_J8474_8517_001_100_0000_Light-Double-G-flower-ring.jpg",
+            department: "jewelry",
+            type: "rings"
+        },
+        {
+            name: "Beaded necklace with flower",
+            price: 2300,
+            img: "https://media.gucci.com/style/DarkGray_South_0_160_316x316/1626885905/675291_J1D50_8061_001_100_0000_Light-Beaded-necklace-with-flower.jpg",
+            department: "jewelry",
+            type: "necklace"
+        },
+        {
+            name: "Lion head ring with crystal",
+            price: 430,
+            img: "https://media.gucci.com/style/DarkGray_South_0_160_316x316/1628098205/675305_I6658_8489_001_100_0000_Light-Lion-head-ring-with-crystal.jpg",
+            department: "jewelry",
+            type: "rings"
+        },
+        {
+            name: "Interlocking G crystal hoops",
             price: 1150,
-            img: "https://media.gucci.com/style/DarkGray_South_0_160_316x316/1634315425/680518_ZAIDY_9376_001_100_0000_Light-Baby-cat-print-cotton-dress.jpg",
-            department: "children",
-            type: "clothes"
+            img: "https://media.gucci.com/style/DarkGray_South_0_160_316x316/1629877504/680275_J3F42_8162_001_100_0000_Light-Interlocking-G-crystal-hoop-earrings.jpg",
+            department: "jewelry",
+            type: "earrings"
+        },
+        {
+            name: "Gucci Diana small crocodile",
+            price: 35000,
+            img: "https://media.gucci.com/style/DarkGray_South_0_160_316x316/1618321510/660195_EZINT_2582_001_067_0000_Light-Gucci-Diana-small-crocodile-tote-bag.jpg",
+            department: "bags",
+            type: "purses"
+        },
+        {
+            name: "Backpack with Interlocking G",
+            price: 2500,
+            img: "https://media.gucci.com/style/DarkGray_South_0_160_316x316/1632265210/674147_UQHGE_8679_001_081_0000_Light-Backpack-with-Interlocking-G.jpg",
+            department: "bags",
+            type: "backpack"
+        },
+        {
+            name: "Valentine's Day small heart",
+            price: 1800,
+            img: "https://media.gucci.com/style/DarkGray_South_0_160_316x316/1638432995/678131_UP2AV_5575_001_060_0000_Light-Valentines-Day-small-heart-bag.jpg",
+            department: "bags",
+            type: "purses"
+        },
+        {
+            name: "Ophidia GG small backpack",
+            price: 1790,
+            img: "https://media.gucci.com/style/DarkGray_South_0_160_316x316/1533573917/547965_9U8BT_8994_001_089_0000_Light-Ophidia-GG-small-backpack.jpg",
+            department: "bags",
+            type: "backpack"
         },
     ],
     storeData: function () {
