b99ed4eb0dd5dd75096b0a53b9ef9b8731353e43
noachasha <noachasha@gmail.com>
2022-02-08 23:24:31 +0200

    up

--- תרגול/08.02.22/noa/index.ts
+++ תרגול/08.02.22/noa/index.ts
@@ -1,18 +1,18 @@
 
 const radio = document.querySelectorAll("input[type='radio']")
-const amount=document.getElementById('Amount')
+const amount = document.getElementById('Amount')
 
 
 for (let i = 0; i < radio.length; i++) {
     console.log(radio[i]);
 
     radio[i].addEventListener('click', ev => {
-        if (ev.target.value == 'Partial Withdrawal'){
+        if (ev.target.value == 'Partial Withdrawal') {
             console.log(ev.target.value);
-            amount.style.display='block';
+            amount.style.display = 'block';
         }
-        else{
-            amount.style.display='none';
+        else {
+            amount.style.display = 'none';
         }
-})
+    })
 }
