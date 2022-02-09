7161f85f5e7b344e5011eb986052e9e2edf56c2b
noachasha <noachasha@gmail.com>
2022-02-10 00:21:25 +0200

    up

--- תרגול/08.02.22/noa/dist/index.js
+++ תרגול/08.02.22/noa/dist/index.js
@@ -1,5 +1,8 @@
 var radio = document.querySelectorAll("input[type='radio']");
 var amount = document.getElementById('Amount');
+var checkbox = document.querySelector('.checkbox');
+var checkboxinput = document.querySelectorAll("input[type='checkbox']");
+var tax = document.querySelector('.tax');
 for (var i = 0; i < radio.length; i++) {
     console.log(radio[i]);
     radio[i].addEventListener('click', function (ev) {
@@ -9,6 +12,25 @@ for (var i = 0; i < radio.length; i++) {
         }
         else {
             amount.style.display = 'none';
+        }
+        if (ev.target.value == 'Autometed Monthly Withdrawal') {
+            checkbox.style.cssText =
+                "display: grid;\n            grid-template-columns: 20% 80%;\n            margin: 5px;";
+        }
+        else {
+            checkbox.style.display = 'none';
+        }
+    });
+}
+for (var i = 0; i < checkboxinput.length; i++) {
+    checkboxinput[i].addEventListener('click', function (ev) {
+        if (ev.target.value == 'Payment withdrawal') {
+            tax.style.cssText =
+                "  grid-column: 1/3;\n            display: flex;\n            margin-left: 20px;\n            margin-top: 10px;";
+        }
+        else if (ev.target.value == 'Corresponding to loan') {
+            tax.style.display = 'none';
+            checkboxinput[0].checked = false;
         }
     });
 }
--- תרגול/08.02.22/noa/index.ts
+++ תרגול/08.02.22/noa/index.ts
@@ -1,7 +1,9 @@
 
 const radio = document.querySelectorAll("input[type='radio']")
 const amount = document.getElementById('Amount')
-
+const checkbox = document.querySelector('.checkbox')
+const checkboxinput = document.querySelectorAll("input[type='checkbox']")
+const tax = document.querySelector('.tax')
 
 for (let i = 0; i < radio.length; i++) {
     console.log(radio[i]);
@@ -14,5 +16,33 @@ for (let i = 0; i < radio.length; i++) {
         else {
             amount.style.display = 'none';
         }
+
+        if (ev.target.value == 'Autometed Monthly Withdrawal') {
+            checkbox.style.cssText =
+                `display: grid;
+            grid-template-columns: 20% 80%;
+            margin: 5px;`
+        }
+        else {
+            checkbox.style.display = 'none';
+        }
     })
+}
+
+for (let i = 0; i < checkboxinput.length; i++) {
+    checkboxinput[i].addEventListener('click', ev => {
+        if (ev.target.value == 'Payment withdrawal') {
+            tax.style.cssText=
+            `  grid-column: 1/3;
+            display: flex;
+            margin-left: 20px;
+            margin-top: 10px;`
+           
+        }
+        else if (ev.target.value == 'Corresponding to loan') {
+            tax.style.display = 'none';
+            checkboxinput[0].checked = false;
+
+        }
+    }
 }
