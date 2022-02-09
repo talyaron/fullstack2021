bee0095aa005466f6f5fbaf936594c6ed4cfd8fb
ShaniRom <93940556+ShaniRom@users.noreply.github.com>
2022-02-09 22:03:49 +0200

    gucci project

--- 02-JavaScript/09-formsAndObjects/03-Gucci-Project/Marina Rasheed Shani/rasheed-gucci-/index.ts
+++ 02-JavaScript/09-formsAndObjects/03-Gucci-Project/Marina Rasheed Shani/rasheed-gucci-/index.ts
@@ -6,47 +6,32 @@ interface Store {
     name: string,
     price: number,
     img: any,
-    department:
-      | "clothes"
-      | "watches"
-      | "jewelry"
-      | "bags"
-      | "shoes"
-      | "children",
-    gender: "men" | "women",
+    gender: "men" | "women" | "children" | "menWatches" | "womenJewlery" | "womenBags",
     type: string,
     id: any
   ): any;
   removeItems(itemName: string): any;
   updateItems(id: any, newPrice: number, itemName: string): any;
-  filterMaxPrice(price: number);
+  filterMaxPriceAndGender(gender: string, price: number);
   sortByAscending(price: number);
   sortByDescending(price: number);
   render(list: any, domElement: any): any;
   renderAllitems(domElement: any): any;
-  renderMaxPrice(filtered: Array<Item>, domElement);
-  filterByGenderAndTypeAndDepartment(
-    type: string,
-    gender: string,
-    department: string
-  );
+  renderMaxPrice(filtered: Array<Item>, domElement: any): any;
   filterByGenderAndType(type: string, gender: string);
-  filterByDepartment(department: string);
+  renderFilterByGenderAndType(filterGender: any, filterType: any, domElement: any): any;
   filterByGender(gender: string);
-  filterByType(type: string); 
-  renderByDepartment(department: string, domElement: any): any;
   renderByGender(gender: string, domElement: any): any;
-  renderByType(type: string, domElement: any): any;  
-  
-  
+
+
+
 }
 
 interface Item {
   name: string;
   price: number;
   img: any;
-  department: "clothes" | "watches" | "jewelry" | "bags" | "shoes" | "children";
-  gender?: "men" | "women";
+  gender: "men" | "women" | "children" | "menWatches" | "womenJewlery" | "womenBags";
   type: string;
   id?: any;
 }
@@ -57,7 +42,6 @@ const gucci: Store = {
       name: "Retro tweed dress",
       price: 3800,
       img: "https://media.gucci.com/style/DarkGray_South_0_160_316x316/1632265215/674469_ZAH8I_6535_001_100_0000_Light-Retro-tweed-dress-with-velvet-trims.jpg",
-      department: "clothes",
       gender: "women",
       type: "dresses",
     },
@@ -65,7 +49,6 @@ const gucci: Store = {
       name: "Pleated dress ",
       price: 5980,
       img: "//media.gucci.com/style/DarkGray_South_0_160_316x316/1634249776/669260_ZAH0J_9782_001_100_0000_Light-Pleated-dress-with-chevron-vintage-Web.jpg",
-      department: "clothes",
       gender: "women",
       type: "dresses",
     },
@@ -73,7 +56,7 @@ const gucci: Store = {
       name: "Silk mohair sweater with feathers ",
       price: 2500,
       img: "https://media.gucci.com/style/DarkGray_South_0_160_316x316/1640195126/670625_XKB1V_1043_001_100_0000_Light-Silk-mohair-sweater-with-feathers.jpg",
-      department: "clothes",
+
       gender: "women",
       type: "sweaters",
     },
@@ -81,7 +64,7 @@ const gucci: Store = {
       name: "Leather ankle boot with belt",
       price: 1190,
       img: "https://media.gucci.com/style/DarkGray_South_0_160_316x316/1537468210/550036_DKS00_1000_001_100_0000_Light-Leather-ankle-boot-with-belt.jpg",
-      department: "shoes",
+
       gender: "women",
       type: "boots",
     },
@@ -89,7 +72,7 @@ const gucci: Store = {
       name: "Women's ankle boot with Horsebit",
       price: 790,
       img: "https://media.gucci.com/style/DarkGray_South_0_160_316x316/1633042815/670407_JCCP0_3154_001_100_0000_Light-Womens-ankle-boot-with-Horsebit.jpg",
-      department: "shoes",
+
       gender: "women",
       type: "boots",
     },
@@ -97,35 +80,39 @@ const gucci: Store = {
       name: "Baby cat print linen coat",
       price: 1150,
       img: "https://media.gucci.com/style/DarkGray_South_0_160_316x316/1634315425/680518_ZAIDY_9376_001_100_0000_Light-Baby-cat-print-cotton-dress.jpg",
-      department: "children",
+
+      gender: "children",
       type: "clothes",
     },
     {
       name: "Double G cotton jacket",
       price: 635,
       img: "https://media.gucci.com/style/DarkGray_South_0_160_316x316/1633043710/671075_XWARC_3472_001_100_0000_Light-Childrens-Double-G-cotton-jacket.jpg",
-      department: "children",
+
+      gender: "children",
       type: "clothes",
     },
     {
       name: "GG cotton hat with ear flaps",
       price: 295,
       img: "https://media.gucci.com/style/DarkGray_South_0_160_316x316/1632819610/683064_3HAG1_4969_001_100_0000_Light-Baby-GG-cotton-hat-with-ear-flaps.jpg",
-      department: "children",
+
+      gender: "children",
       type: "hats",
     },
     {
       name: "Straw effect hat with ribbon",
       price: 300,
       img: "https://media.gucci.com/style/DarkGray_South_0_160_316x316/1634251518/684920_3HAHA_9266_001_100_0000_Light-Baby-straw-effect-hat-with-ribbon.jpg",
-      department: "children",
+
+      gender: "children",
       type: "hats",
     },
     {
       name: "The North Face x",
       price: 950,
       img: "https://media.gucci.com/style/DarkGray_South_0_160_316x316/1638553515/663766_ZLX45_3475_001_100_0000_Light-The-North-Face-x-Gucci-pant.jpg",
-      department: "clothes",
+
       gender: "men",
       type: "pants",
     },
@@ -133,7 +120,7 @@ const gucci: Store = {
       name: "Gucci Jordaan crocodile loafer",
       price: 3200,
       img: "//media.gucci.com/style/DarkGray_South_0_160_470x470/1459942203/406994_EC200_1000_001_100_0000_Light.jpg",
-      department: "clothes",
+
       gender: "men",
       type: "shoes",
     },
@@ -141,7 +128,7 @@ const gucci: Store = {
       name: "GG leather pant",
       price: 4600,
       img: "https://media.gucci.com/style/DarkGray_South_0_160_316x316/1636738256/669071_XNAPN_1000_001_100_0000_Light-GG-leather-pant.jpg",
-      department: "clothes",
+
       gender: "men",
       type: "pants",
     },
@@ -149,7 +136,7 @@ const gucci: Store = {
       name: "Cotton piquet polo ",
       price: 690,
       img: "https://media.gucci.com/style/DarkGray_South_0_160_316x316/1634749231/653380_XJDF8_3495_001_100_0000_Light-Cotton-piquet-polo-with-Interlocking-G.jpg",
-      department: "clothes",
+
       gender: "men",
       type: "shirts",
     },
@@ -157,7 +144,7 @@ const gucci: Store = {
       name: "GG kaleidoscope silk bowling shirt",
       price: 1250,
       img: "https://media.gucci.com/style/DarkGray_South_0_160_316x316/1638550829/675446_ZAH3W_1128_001_100_0000_Light-GG-kaleidoscope-silk-bowling-shirt.jpg",
-      department: "clothes",
+
       gender: "men",
       type: "shirts",
     },
@@ -165,85 +152,101 @@ const gucci: Store = {
       name: "Grip watch, 38mm",
       price: 1900,
       img: "https://media.gucci.com/style/DarkGray_South_0_160_316x316/1567584905/596509_I8600_8740_001_100_0000_Light-Grip-watch-38mm.jpg",
-      department: "watches",
+
+      gender: "menWatches",
       type: "rolex",
     },
     {
       name: "Grip watch, 35mm",
       price: 1700,
       img: "https://media.gucci.com/style/DarkGray_South_0_160_316x316/1567584903/584271_ICBA0_8576_001_100_0000_Light-Grip-watch-35mm.jpg",
-      department: "watches",
+
+      gender: "menWatches",
       type: "omega",
     },
     {
       name: "GUCCI 25H watch, 40mm",
       price: 9500,
       img: "https://media.gucci.com/style/DarkGray_South_0_160_316x316/1627285507/673127_I1600_1108_001_100_0000_Light-GUCCI-25H-watch-40mm.jpg",
-      department: "watches",
+
+      gender: "menWatches",
       type: "rolex",
     },
     {
       name: "Gucci Tiger G-Timeless, 38 mm",
       price: 1020,
       img: "https://media.gucci.com/style/DarkGray_South_0_160_316x316/1634838345/692078_ICZA0_8521_001_100_0000_Light-Gucci-Tiger-G-Timeless-watch-38-mm.jpg",
-      department: "watches",
+
+      gender: "menWatches",
       type: "omega",
     },
     {
       name: "Double G flower",
       price: 390,
       img: "https://media.gucci.com/style/DarkGray_South_0_160_316x316/1522084508/527394_J8474_8517_001_100_0000_Light-Double-G-flower-ring.jpg",
-      department: "jewelry",
+
+      gender: "womenJewlery",
       type: "rings",
+
     },
     {
       name: "Beaded necklace with flower",
       price: 2300,
       img: "https://media.gucci.com/style/DarkGray_South_0_160_316x316/1626885905/675291_J1D50_8061_001_100_0000_Light-Beaded-necklace-with-flower.jpg",
-      department: "jewelry",
-      type: "necklace",
+
+      gender: "womenJewlery",
+      type: "necklaces",
+
     },
     {
       name: "Lion head ring with crystal",
       price: 430,
       img: "https://media.gucci.com/style/DarkGray_South_0_160_316x316/1628098205/675305_I6658_8489_001_100_0000_Light-Lion-head-ring-with-crystal.jpg",
-      department: "jewelry",
+
+      gender: "womenJewlery",
       type: "rings",
+
     },
     {
       name: "Interlocking G crystal hoops",
       price: 1150,
       img: "https://media.gucci.com/style/DarkGray_South_0_160_316x316/1629877504/680275_J3F42_8162_001_100_0000_Light-Interlocking-G-crystal-hoop-earrings.jpg",
-      department: "jewelry",
+
+      gender: "womenJewlery",
       type: "earrings",
+
     },
 
     {
       name: "Gucci Diana small crocodile",
       price: 35000,
       img: "https://media.gucci.com/style/DarkGray_South_0_160_316x316/1618321510/660195_EZINT_2582_001_067_0000_Light-Gucci-Diana-small-crocodile-tote-bag.jpg",
-      department: "bags",
+
+      gender: "womenBags",
       type: "purses",
     },
     {
       name: "Backpack with Interlocking G",
       price: 2500,
       img: "https://media.gucci.com/style/DarkGray_South_0_160_316x316/1632265210/674147_UQHGE_8679_001_081_0000_Light-Backpack-with-Interlocking-G.jpg",
-      department: "bags",
+
+      gender: "womenBags",
       type: "backpack",
     },
     {
       name: "Valentine's Day small heart",
       price: 1800,
       img: "https://media.gucci.com/style/DarkGray_South_0_160_316x316/1638432995/678131_UP2AV_5575_001_060_0000_Light-Valentines-Day-small-heart-bag.jpg",
-      department: "bags",
+
+      gender: "womenBags",
       type: "purses",
     },
     {
       name: "Ophidia GG small backpack",
       price: 1790,
       img: "https://media.gucci.com/style/DarkGray_South_0_160_316x316/1533573917/547965_9U8BT_8994_001_089_0000_Light-Ophidia-GG-small-backpack.jpg",
-      department: "bags",
+
+      gender: "womenBags",
       type: "backpack",
     },
 
@@ -259,9 +262,9 @@ const gucci: Store = {
       this.items = clothesStorage;
     }
   },
-  addItems(name, price, img, department, gender, type) {
+  addItems(name, price, img, gender, type) {
     const id = uid();
-    this.items.push({ name, price, img, department, gender, type, id });
+    this.items.push({ name, price, img, gender, type, id });
     this.storeData();
   },
   removeItems(itemName: string) {
@@ -282,8 +285,16 @@ const gucci: Store = {
     }
   },
 
-  filterMaxPrice(price) {
-    return this.items.filter((item) => item.price < price);
+  filterMaxPriceAndGender(gender, price) {
+
+    return this.items
+      .filter((item) => item.gender === gender)
+      .filter((item) => item.price < price);
+
+
+
+
+
   },
 
   sortByAscending(price) {
@@ -295,7 +306,7 @@ const gucci: Store = {
     this.items.sort((a, b) => {
       return b.price - a.price;
     });
-  },  
+  },
   render(list, domElement) {
     let html = "";
     list.forEach((product: any) => {
@@ -307,19 +318,13 @@ const gucci: Store = {
         </div>`;
     });
     domElement.innerHTML = html;
-  },  
+  },
   renderAllitems(domElement) {
     const items = this.items;
     this.render(items, domElement);
   },
   renderMaxPrice(filtered, domElement) {
     this.render(filtered, domElement);
-  },  
-  filterByGenderAndTypeAndDepartment(type, gender, department) {
-    return this.items
-      .filter((item) => item.type === type)
-      .filter((item) => item.gender === gender)
-      .filter((item) => item.department === department);
   },
   filterByGenderAndType(type, gender) {
     console.log(type, gender);
@@ -327,30 +332,25 @@ const gucci: Store = {
       .filter((item) => item.type === type)
       .filter((item) => item.gender === gender);
   },
-  filterByDepartment(department) {
-    return this.items.filter((item) => item.department === department);
+  renderFilterByGenderAndType(filterGender, filterType, domElement) {
+    const filterByGenderAndType = this.filterByGenderAndType(filterGender, filterType);
+    this.render(filterByGenderAndType, domElement)
+
+
+
   },
   filterByGender(gender) {
     return this.items.filter((item) => item.gender === gender);
-  },
-  filterByType(type) {
-    return this.items.filter((item) => item.type === type);
-  },
-  renderByDepartment(department, domElement) {
-    const filterByDepartment = this.filterByDepartment(department);
-    this.render(filterByDepartment, domElement);
   },
   renderByGender(gender, domElement) {
     const filterByGender = this.filterByGender(gender);
     this.render(filterByGender, domElement);
   },
-  renderByType(type, domElement) {
-    const filterByType = this.filterByType(type);
-    this.render(filterByType, domElement);
-  },
-  
+
+
 };
 gucci.storeData(); /// delete later
+
 
 function handleShowItems() {
   console.log(gucci.items);
@@ -368,11 +368,10 @@ function handleAddItems(ev) {
   const name = ev.target.name.value;
   const price = ev.target.price.value;
   const img = ev.target[2].value;
-  const department = ev.target[3].value;
-  const gender = ev.target[4].value;
-  const type = ev.target[5].value;
+  const gender = ev.target[3].value;
+  const type = ev.target[4].value;
   let id = uid;
-  gucci.addItems(name, price, img, department, gender, type, id);
+  gucci.addItems(name, price, img, gender, type, id);
 
   const root = document.getElementById("root");
   gucci.renderAllitems(root);
@@ -407,43 +406,65 @@ function handleUpdate(ev, id) {
 
 function handleFilterByPrice(ev) {
   ev.preventDefault();
+
   const price = ev.target.valueAsNumber;
+  const gender = ev.target.dataset.gender;
   const root = document.querySelector("#root");
+  const filtered = gucci.filterMaxPriceAndGender(gender, price);
+
+
+
   if (price) {
-    const filtered = gucci.filterMaxPrice(price);
+
     gucci.renderMaxPrice(filtered, root);
+
   } else {
-    gucci.renderAllitems(root);
+    gucci.renderByGender(gender, root);
   }
 }
 
 
-function handlePriceAsc(price) {
+function handlePriceAsc(ev, price) {
   gucci.sortByAscending(price);
   const root = document.getElementById("root");
-  gucci.renderAllitems(root);
+
+  const gender = ev.target.dataset.gender;
+  console.dir(ev)
+  gucci.renderByGender(gender, root);
 }
 
-function handlePriceDesc(price) {
+function handlePriceDesc(ev, price) {
   gucci.sortByDescending(price);
   const root = document.getElementById("root");
-  gucci.renderAllitems(root);
+
+  const gender = ev.target.dataset.gender;
+  console.dir(ev)
+  gucci.renderByGender(gender, root);
 }
+
+
+
+
 
 
 function handleSelect(ev) {
+
   const gender = ev.target.dataset.gender;
   const type = ev.target.value;
-
   const root = document.getElementById("root");
-  const filterd = gucci.filterByGenderAndType(type, gender);
+
+
+
+  if (type === "all") {
+    gucci.renderByGender(gender, root)
+
+  } else {
+    gucci.renderFilterByGenderAndType(type, gender, root)
+
+  }
 
 }
 
-// gucci.getData();
-
-// const root = document.getElementById("root");
-// gucci.renderAllitems(root);
 
 function handleRenderByGender(gender: string) {
   const root = document.getElementById("root");
@@ -451,23 +472,5 @@ function handleRenderByGender(gender: string) {
   gucci.renderByGender(gender, root);
 }
 
-function handleRenderByDepartment(department: string) {
-  const root = document.getElementById("root");
-  gucci.getData();
-  gucci.renderByDepartment(department, root);
-}
 
 
-function handleShowDropDown(ev) {
-  console.log(ev);
-
-  const id = ev.target.id;
-
-  const dropDown = document.getElementById(`${id}-dropdown`);
-  if (ev.type === "mouseleave") {
-    // dropDown.classList.replace("show", "hide");
-  } else if (ev.type === "mouseenter") {
-    //all dropdowns class hide
-    dropDown.classList.replace("hide", "show");
-  }
-}
