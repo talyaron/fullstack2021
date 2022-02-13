a65b6d36964d47599b1ea1f090bf098921b39a36
MichaelKhe <Michills@protonmail.com>
2022-02-13 17:49:27 +0200

    ju

--- 02-JavaScript/08-forms/MichZap/dist/index.js
+++ 02-JavaScript/08-forms/MichZap/dist/index.js
@@ -3,12 +3,21 @@ var Items = {
     addItems: function (item) {
         this.items.push(item);
     },
+    removeItems: function (itemName) {
+        var index = this.items.findIndex(function (item) { return item.itemName === itemName; });
+        if (index >= 0) {
+            this.items.splice(index, 1);
+        }
+    },
     renderItems: function (domElement) {
         var html = '';
         this.items.forEach(function (item) {
-            html += "<div class='card'>\n        <p>" + item.itemName + "," + item.itemPrice + "</p></div>";
+            html += "<div class='card'>\n        <p>" + item.itemName + "," + item.itemPrice + "</p></div> \n        <input type=\"button\" value=\"delete\">";
         });
         domElement.innerHTML = html;
+    },
+    sortPrice: function () {
+        this.items.sort(function (a, b) { return a.itemPrice - b.itemPrice; });
     }
 };
 function handleSubmit(ev) {
@@ -21,3 +30,9 @@ function handleSubmit(ev) {
     Items.renderItems(rootItems);
     ev.target.reset();
 }
+Items.addItems({ itemName: 'bbb', itemPrice: 12 });
+Items.addItems({ itemName: 'ccc', itemPrice: 4444 });
+Items.addItems({ itemName: 'eee', itemPrice: 5 });
+// Items.removeItems('ccc');
+// Items.sortPrice();
+// console.log(Items)
