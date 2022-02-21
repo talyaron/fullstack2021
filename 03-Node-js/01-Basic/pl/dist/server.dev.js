8c3dd86cc0a6e886faef8992291206dcae91c720
leon <lmalul13@gmail.com>
2022-02-21 07:30:28 +0200

    63

---
+++ 03-Node-js/01-Basic/pl/dist/server.dev.js
@@ -0,0 +1,15 @@
+"use strict";
+
+var express = require('express');
+
+var app = express();
+var port = process.env.PORT || 3000;
+app.get('/', function (req, res) {
+  res.send('Hello World');
+});
+app.get('/get-user', function (req, res) {
+  res.send('Hello jhghjvhjd');
+});
+app.listen(port, function () {
+  console.log("server listen on port", port);
+});
