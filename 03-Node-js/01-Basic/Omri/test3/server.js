6d3920158b7e368e5eb0c7128c15d1bc949fbdf7
OmriAharonov <omri832@gmail.com>
2022-02-21 14:13:57 +0200

    test 3 omri

---
+++ 03-Node-js/01-Basic/Omri/test3/server.js
@@ -0,0 +1,10 @@
+const express = require('express')
+const app = express()
+const port = process.env.port || 3000
+
+app.get('/', function (req, res) {
+  res.send('Hello World')
+})
+
+
+app.listen(port, () => {console.log('server listen on port',port)})
