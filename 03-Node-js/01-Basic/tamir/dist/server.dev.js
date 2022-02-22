f951eb9a1c224fdc713c1a119c728b17e04e8eb8
tamir-dadon <tamir.dadon63@gmail.com>
2022-02-22 13:58:03 +0200

    ז

--- 03-Node-js/01-Basic/tamir/dist/server.dev.js
+++ 03-Node-js/01-Basic/tamir/dist/server.dev.js
@@ -4,8 +4,19 @@ var express = require('express');
 
 var app = express();
 var port = process.env.port || 3000;
-app.get('/', function (req, res) {
-  res.send('Hello tamir');
+app.use(express["static"]('public'));
+app.get('/me', function (req, res) {
+  res.send({
+    name: 'tamir',
+    age: '24'
+  });
+  console.log('hey');
+});
+app.get('/friend', function (req, res) {
+  res.send({
+    name: 'yanir',
+    age: '25'
+  });
 });
 app.listen(port, function () {
   console.log('server listen on port', port);
