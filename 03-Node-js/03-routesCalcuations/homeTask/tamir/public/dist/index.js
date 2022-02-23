cae7c4fdfd4b0fdf90692530d3eb3e6b917ca2b5
tamir-dadon <tamir.dadon63@gmail.com>
2022-02-23 15:47:28 +0200

    ז

--- 03-Node-js/03-routesCalcuations/homeTask/tamir/public/dist/index.js
+++ 03-Node-js/03-routesCalcuations/homeTask/tamir/public/dist/index.js
@@ -4,28 +4,41 @@ axios.get('/work')
     console.log(data);
 });
 function handleselect(ev) {
-    var year = ev.target.valu;
-    var img = ev.target.value;
     var view = ev.target.value;
     console.log(view);
     if (view) {
-        try {
-            axios.get("/work?view=" + view + "&&?img=" + img)
-                .then(function (_a) {
-                var data = _a.data;
-                console.log(data);
-                var root = document.getElementById('root');
-                var html = '';
-                if (Array.isArray(data)) {
-                    data.forEach(function (work) {
-                        html += "<p>" + work.img + "</P>";
-                    });
-                    root.innerHTML = html;
-                }
-            });
-        }
-        catch (err) {
-            throw new Error("eroor");
-        }
+        axios.get("/work?view=" + view)
+            .then(function (_a) {
+            var data = _a.data;
+            console.log(data);
+            var root = document.getElementById('root');
+            var html = '';
+            if (Array.isArray(data)) {
+                data.forEach(function (work) {
+                    html += "\n                            <p><img src=\"" + work.img + "\" alt=\"\" class='img'></P>";
+                });
+                root.innerHTML = html;
+            }
+        });
+    }
+}
+function handlechenge(ev) {
+    var year = ev;
+    console.dir(ev);
+    console.log(year);
+    if (year) {
+        axios.get("/work?year=" + year)
+            .then(function (_a) {
+            var data = _a.data;
+            console.log();
+            var root2 = document.getElementById('root2');
+            var html = '';
+            if (Array.isArray(data)) {
+                data.forEach(function (work) {
+                    html + ("\n                            <p><img src=\"" + work.img + "\" alt=\"\" class='img'></P>");
+                });
+                root2.innerHTML = html;
+            }
+        });
     }
 }
