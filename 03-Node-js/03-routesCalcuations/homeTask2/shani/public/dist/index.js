7066820cfe719025818db0357ffb51815afa990c
ShaniRom <93940556+ShaniRom@users.noreply.github.com>
2022-02-26 19:18:22 +0200

    node

--- 03-Node-js/03-routesCalcuations/homeTask2/shani/public/dist/index.js
+++ 03-Node-js/03-routesCalcuations/homeTask2/shani/public/dist/index.js
@@ -6,4 +6,19 @@ axios
 function handleSearch(ev) {
     var search = ev.target.value;
     console.log(search);
+    axios.get("/getPics?view=" + search).then(function (_a) {
+        var data = _a.data;
+        var rootArticle = document.getElementById('rootArticle');
+        var html = "";
+        if (Array.isArray(data)) // this is like try catch to make sure it recieves the array
+         {
+            data.forEach(function (article) {
+                html += "\n                <h1>" + article.title + "</h1>\n                <h3>" + article.content + "</h3>";
+            });
+            rootArticle.innerHTML = html;
+        }
+        else {
+            console.log("the function doesnt recieve the array cause it isnt an array");
+        }
+    });
 }
--- 03-Node-js/03-routesCalcuations/homeTask2/shani/public/index.ts
+++ 03-Node-js/03-routesCalcuations/homeTask2/shani/public/index.ts
@@ -6,5 +6,27 @@ axios
 
 function handleSearch(ev){
     const search=ev.target.value;
-    console.log(search)
+    console.log(search);
+   
+
+    axios.get(`/getPics?view=${search}`).then(({data})=>{
+
+        const rootArticle=document.getElementById('rootArticle');
+        let html="";
+        if(Array.isArray(data)) // this is like try catch to make sure it recieves the array
+        {
+            data.forEach((article)=>{
+                html+=`
+                <h1>${article.title}</h1>
+                <h3>${article.content}</h3>`
+                })
+                rootArticle.innerHTML=html;
+        }else{
+            console.log("the function doesnt recieve the array cause it isnt an array")
+        }
+
+
+    });
+    
+    
 }
