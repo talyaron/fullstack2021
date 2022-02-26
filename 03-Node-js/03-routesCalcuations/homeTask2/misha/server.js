a3ff82fdb0b1a2a2a759af69dd74df9ce419efeb
Michael Dubovyk <93338459+mishalon@users.noreply.github.com>
2022-02-26 14:08:12 +0200

    1

---
+++ 03-Node-js/03-routesCalcuations/homeTask2/misha/server.js
@@ -0,0 +1,16 @@
+var express = require('express');
+var app = express();
+var port = process.env.PORT || 3000;
+
+
+
+app.use(express.static('public')) //connects the public folder 
+
+app.get('/get-article', function (req, res) {
+    res.send(photos);
+});
+
+
+app.listen(port,()=>{
+    return console.log(`Express is listening at http://localhost:${port}`);
+});
