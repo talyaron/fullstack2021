acfa1a38924f5cc58e67aa3fb8a6283cf70411e5
Ido Gino <davegino220@gmail.com>
2022-02-26 20:32:15 +0200

    lala

--- 03-Node-js/03-routesCalcuations/homeTask2/ido/public/dist/index.js
+++ 03-Node-js/03-routesCalcuations/homeTask2/ido/public/dist/index.js
@@ -1,27 +1,24 @@
-axios.get("/player").then(function (_a) {
+axios.get("/data").then(function (_a) {
     var data = _a.data;
     console.log(data);
 });
-var btns = document.querySelectorAll('.main_form-btns');
-btns.forEach(function (btn) {
-    btn.addEventListener('click', displayPlayer);
-});
-function displayPlayer(ev) {
-    ev.preventDefault();
-    try {
-        var year = ev.target.value;
-        var display_1 = document.querySelector('.main_display');
-        var html_1 = '';
-        if (year) {
-            axios.get("/get-player?year=" + year).then(function (_a) {
-                var data = _a.data;
-                console.log(data);
-                html_1 = "<h3 class=\"main_display_playerName\">" + data[0].name + "</h3>\n                <span class=\"main_display-year\">Winner Ballon d'Or in " + data[0].year + " </span><br>\n                <span class=\"main_display-national\">Nationality : " + data[0].nationality + " </span>";
-                display_1.style.backgroundImage = "url(" + data[0].url + ")";
-                display_1.innerHTML = html_1;
-            });
-        }
-    }
-    finally {
-    }
+function renderItems() {
+    var display = document.querySelector('.main_display');
+    var html = '';
+    axios.get("/data").then(function (_a) {
+        var data = _a.data;
+        data.forEach(function (item) {
+            html += "<div class=\"main_display-card\">\n        <img class=\"main_display-card-img\" src=\"" + item.picUrl + "\" alt=\"Avatar\" style=\"width:100%\">\n        <div class=\"main_display-card-details\">\n          <h4 class=\"main_display-card-details-title detailsInCard\"><b>" + item.title + "</b><br>\n            <span class=\"main_display-card-details-description detailsInCard\">" + item.description + "</span></h4> \n          <p class=\"main_display-card-details-price detailsInCard\">$" + item.price + "</p>\n        </div>\n      </div>";
+        });
+        display.innerHTML = html;
+    });
+}
+function displayByInput(ev) {
+    var display = document.querySelector('.main_display');
+    var dataList = document.querySelector('.main_header-search-dataList');
+    var html = '';
+    var inputValue = ev.target.value;
+    axios.get("/searched-list?input=" + inputValue).then(function (_a) {
+        var data = _a.data;
+    });
 }
