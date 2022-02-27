86c5771ff1ad0dbbc320aaaffa0c6acc8b956c81
tamir-dadon <tamir.dadon63@gmail.com>
2022-02-27 17:19:19 +0200

    z

---
+++ 03-Node-js/03-routesCalcuations/homeTask/tamir2/server.js
@@ -0,0 +1,21 @@
+const express = require('express');
+const { title } = require('process');
+const app = express()
+const port = process.env.PORT || 3000;
+
+const data1 = [{
+    title: 'facbook',
+    text: 'in facbook we heve a lot of friends'
+}]
+
+
+app.use(express.static("public"));
+
+app.get('/tamir', function (req, res) {
+    
+    res.send(data1)
+    }),
+
+    app.listen(port, () => {
+        return console.log(`Express is listening at http://localhost:${port}`);
+    });
