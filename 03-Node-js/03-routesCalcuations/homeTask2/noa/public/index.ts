2b3bdd089ddaf21dc0ef6c152b47a0a0a8e74b99
noachasha <noachasha@gmail.com>
2022-02-27 23:35:05 +0200

    up

--- 03-Node-js/03-routesCalcuations/homeTask2/noa/public/index.ts
+++ 03-Node-js/03-routesCalcuations/homeTask2/noa/public/index.ts
@@ -5,13 +5,21 @@ axios.get("/get-google").then(({ data }) => {
 
 function handleSearch(ev) {
     const search = ev.target.value;
-   
+
     axios.get(`/get-google?search=${search}`).then(({ data }) => {
-        console.log(data)
+     
+        const root = document.querySelector("#root")
+
+        let html = '';
+
+        data.forEach(element => {
+            html += `<div class="card"> 
+            <h2> ${element.title}</h2>
+            <p> ${element.article}</p>`
+        });
+        root.innerHTML = html
 
     })
+}
 
-} 
 
-
-const root=document.querySelector("#root")
