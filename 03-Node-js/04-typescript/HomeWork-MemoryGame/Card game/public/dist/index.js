bf1e2d0ddaf7d464797ad7b747dd68c2d8670341
ShaniRom <93940556+ShaniRom@users.noreply.github.com>
2022-03-07 20:54:47 +0200

    try

--- 03-Node-js/04-typescript/HomeWork-MemoryGame/Card game/public/dist/index.js
+++ 03-Node-js/04-typescript/HomeWork-MemoryGame/Card game/public/dist/index.js
@@ -90,22 +90,4 @@ function renderGame(cards) {
 function handleNewGame() {
     var openingPage = document.querySelector(".openingPage");
     openingPage.style.display = "none";
-    // let gameCards:HTMLElement=document.querySelector("#root");
-    // let title:HTMLElement=document.querySelector(".title")
-    // let displayCards=gameCards.style.display
-    // let startButton:HTMLElement=document.querySelector(".startButton");
-    // if(displayCards ==='block'){
-    //     startButton.style.display="none";
-    //     gameCards.style.display="grid";
-    //     title.style.display="none";
-    //     document.body.style.backgroundImage='url("https://media.sketchfab.com/models/bde62298ac8640588d5a2928b8e113dc/thumbnails/cea9e18e2bc84efdab1c881a6e6d82c6/a778dd7953b34ab9b9bc92dc2a499598.jpeg")';
-    //     document.body.style.backgroundRepeat="no-repeat";
-    //     document.body.style.backgroundSize="cover";
-    // }else {
-    //    startButton.style.display="inline";
-    //    gameCards.style.display="none";
-    //    document.body.style.backgroundImage='url("https://thumbs.gfycat.com/HardHoarseBarasinga-size_restricted.gif")';
-    //    document.body.style.backgroundRepeat="no-repeat";
-    //    document.body.style.backgroundSize="cover";
-    // }
 }
