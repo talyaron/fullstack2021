fc0e3cf42615bc88cac4d0ac887e4feebf69d503
OmriAharonov <omri832@gmail.com>
2022-03-08 23:11:02 +0200

    gameStore

--- 03-Node-js/04-typescript/follow/Omri/GameStore/dist/server.js
+++ 03-Node-js/04-typescript/follow/Omri/GameStore/dist/server.js
@@ -9,13 +9,26 @@ const port = process.env.PORT || 3002;
 app.use(express_1.default.static("public"));
 app.use(express_1.default.json());
 const gamer = {
-    games: [],
+    games: [
+        {
+            id: Math.random().toString(36).slice(-8),
+            category: 'Action',
+            title: ' The King Of Fighter XV',
+            price: 239,
+            img: 'https://cdn.cloudflare.steamstatic.com/steam/apps/1498570/capsule_184x69.jpg?t=1645084787'
+        },
+    ],
     gameById() {
     },
-    addGame() {
+    addGame(category, title, price, img) {
+        const id = uid();
+        this.games.push({ id, category, title, price, img });
     },
     updateGame() {
-    },
+    }
+};
+const uid = function () {
+    return Math.random().toString(36).slice(-8);
 };
 app.listen(port, () => {
     return console.log(`Express is listening at http://localhost:${port}`);
