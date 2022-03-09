f52159e375f2f3d20033350fc1a9b06c0b35c378
OmriAharonov <omri832@gmail.com>
2022-03-09 22:58:47 +0200

    gmaeStore

--- 03-Node-js/04-typescript/follow/Omri/GameStore/public/dist/index.js
+++ 03-Node-js/04-typescript/follow/Omri/GameStore/public/dist/index.js
@@ -55,10 +55,10 @@ function getGames() {
     });
 }
 function renderGames(data, root) {
-    var html = '';
+    var html = "\n        <div class = \"store\">\n            <div class = \"store__header\"><h2>Inventory List</h2></div>\n        </div>";
     if (Array.isArray(data)) {
         data.forEach(function (game) {
-            html += "\n        <div class = \"store\">\n            <div class = \"store__header\"><h2>Inventory List</h2></div>\n            <div class = \"store__gamesList\">\n                <div class = \"store__gamesList--img\"><img src = \"" + game.img + "\"><div>\n                <div class = \"store__gamesList--title\">Title: \"" + game.title + "\"<div>\n                <div class = \"store__gamesList--price\">price: " + game.price + "<div>\n                <div class = \"store__gamesList--category\">Category: \"" + game.category + "\"<div>\n            <div>\n        </div>";
+            html += "\n            <div class = \"store__gamesList\">\n                <div class = \"store__gamesList--img\"><img src = " + game.img + "><div>\n                <div class = \"store__gamesList--title\">Title: " + game.title + "<div>\n                <div class = \"store__gamesList--price\">price: " + game.price + "<div>\n                <div class = \"store__gamesList--category\">Category: " + game.category + "<div>\n            <div>";
         });
         root.innerHTML = html;
     }
--- 03-Node-js/04-typescript/follow/Omri/GameStore/public/index.ts
+++ 03-Node-js/04-typescript/follow/Omri/GameStore/public/index.ts
@@ -1,4 +1,4 @@
-function appInit(){
+function appInit() {
     getGames()
 }
 
@@ -6,26 +6,26 @@ async function getGames() {
     const { data } = await axios.get("/get-games");
     const rootGames = document.querySelector(".rootGames")
     if (data) {
-        renderGames(data,rootGames);
+        renderGames(data, rootGames);
     }
 }
 
-function renderGames(data: Array<any>,root) {
-    let html = '';
+function renderGames(data: Array<any>, root) {
+    let html = `
+        <div class = "store">
+            <div class = "store__header"><h2>Inventory List</h2></div>
+        </div>`;
     if (Array.isArray(data)) {
         data.forEach(game => {
             html += `
-        <div class = "store">
-            <div class = "store__header"><h2>Inventory List</h2></div>
             <div class = "store__gamesList">
-                <div class = "store__gamesList--img"><img src = "${game.img}"><div>
-                <div class = "store__gamesList--title">Title: "${game.title}"<div>
+                <div class = "store__gamesList--img"><img src = ${game.img}><div>
+                <div class = "store__gamesList--title">Title: ${game.title}<div>
                 <div class = "store__gamesList--price">price: ${game.price}<div>
-                <div class = "store__gamesList--category">Category: "${game.category}"<div>
-            <div>
-        </div>`
+                <div class = "store__gamesList--category">Category: ${game.category}<div>
+            <div>`
         })
         root.innerHTML = html;
     }
-    
+
 }
