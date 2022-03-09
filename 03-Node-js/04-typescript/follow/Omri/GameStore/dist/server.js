664687f937c36fa87a59181088062ddaa9b6e2c6
OmriAharonov <omri832@gmail.com>
2022-03-09 14:43:04 +0200

    gameStore

--- 03-Node-js/04-typescript/follow/Omri/GameStore/dist/server.js
+++ 03-Node-js/04-typescript/follow/Omri/GameStore/dist/server.js
@@ -5,11 +5,32 @@ var __importDefault = (this && this.__importDefault) || function (mod) {
 Object.defineProperty(exports, "__esModule", { value: true });
 const express_1 = __importDefault(require("express"));
 const app = express_1.default();
-const port = process.env.PORT || 3002;
+const port = process.env.PORT || 3007;
 app.use(express_1.default.static("public"));
 app.use(express_1.default.json());
 const gamer = {
     games: [
+        {
+            id: Math.random().toString(36).slice(-8),
+            category: 'Action',
+            title: ' The King Of Fighter XV',
+            price: 239,
+            img: 'https://cdn.cloudflare.steamstatic.com/steam/apps/1498570/capsule_184x69.jpg?t=1645084787'
+        },
+        {
+            id: Math.random().toString(36).slice(-8),
+            category: 'Action',
+            title: ' The King Of Fighter XV',
+            price: 239,
+            img: 'https://cdn.cloudflare.steamstatic.com/steam/apps/1498570/capsule_184x69.jpg?t=1645084787'
+        },
+        {
+            id: Math.random().toString(36).slice(-8),
+            category: 'Action',
+            title: ' The King Of Fighter XV',
+            price: 239,
+            img: 'https://cdn.cloudflare.steamstatic.com/steam/apps/1498570/capsule_184x69.jpg?t=1645084787'
+        },
         {
             id: Math.random().toString(36).slice(-8),
             category: 'Action',
@@ -30,6 +51,13 @@ const gamer = {
 const uid = function () {
     return Math.random().toString(36).slice(-8);
 };
+app.get("/get-games", (req, res) => {
+    const gamer = getGames();
+    res.send(gamer);
+});
+function getGames() {
+    return gamer.games;
+}
 app.listen(port, () => {
     return console.log(`Express is listening at http://localhost:${port}`);
 });
--- 03-Node-js/04-typescript/follow/Omri/GameStore/server.ts
+++ 03-Node-js/04-typescript/follow/Omri/GameStore/server.ts
@@ -1,6 +1,6 @@
 import express from 'express';
 const app = express();
-const port = process.env.PORT || 3002;
+const port = process.env.PORT || 3007;
 app.use(express.static("public"));
 app.use(express.json());
 
@@ -28,6 +28,27 @@ const gamer: gameStore = {
             price: 239,
             img: 'https://cdn.cloudflare.steamstatic.com/steam/apps/1498570/capsule_184x69.jpg?t=1645084787'
         },
+        {
+            id: Math.random().toString(36).slice(-8),
+            category:'Action',
+            title:' The King Of Fighter XV',
+            price: 239,
+            img: 'https://cdn.cloudflare.steamstatic.com/steam/apps/1498570/capsule_184x69.jpg?t=1645084787'
+        },
+        {
+            id: Math.random().toString(36).slice(-8),
+            category:'Action',
+            title:' The King Of Fighter XV',
+            price: 239,
+            img: 'https://cdn.cloudflare.steamstatic.com/steam/apps/1498570/capsule_184x69.jpg?t=1645084787'
+        },
+        {
+            id: Math.random().toString(36).slice(-8),
+            category:'Action',
+            title:' The King Of Fighter XV',
+            price: 239,
+            img: 'https://cdn.cloudflare.steamstatic.com/steam/apps/1498570/capsule_184x69.jpg?t=1645084787'
+        },
     ],
 
     gameById() {
@@ -44,8 +65,19 @@ const gamer: gameStore = {
     }
 }
 
+
+
 const uid = function(){
     return Math.random().toString(36).slice(-8);
+}
+
+app.get("/get-games", (req, res) => {
+    const gamer = getGames();
+    res.send(gamer);
+  });
+  
+function getGames(){
+    return gamer.games
 }
 
 app.listen(port, () => {
