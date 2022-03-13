85469739d6413f948c8d9b7111ce4491e87c1711
shayfoyer <93338189+shayfoyer@users.noreply.github.com>
2022-03-13 19:30:55 +0200

    shay

--- 03-Node-js/04-typescript/HomeWork-MemoryGame/shay/dist/server.js
+++ 03-Node-js/04-typescript/HomeWork-MemoryGame/shay/dist/server.js
@@ -8,46 +8,50 @@ const app = express_1.default();
 const port = process.env.PORT || 3000;
 app.use(express_1.default.static("public"));
 app.use(express_1.default.json());
-const games = [{ photo: "https://publish.one37pm.net/wp-content/uploads/2020/05/spiderman-mobile.jpg", pairdId: 1, id: "" },
-    { photo: "https://publish.one37pm.net/wp-content/uploads/2020/05/spiderman-mobile.jpg", pairdId: 1, id: "" },
-    { photo: "https://cdn.vox-cdn.com/thumbor/ymxjRNyfsvewCNWNTwWL0KyX8O8=/0x389:2732x1819/fit-in/1200x630/cdn.vox-cdn.com/uploads/chorus_asset/file/22802887/IMG_DE272BF718E8_1.jpeg", pairdId: 2, id: "" },
-    { photo: "https://cdn.vox-cdn.com/thumbor/ymxjRNyfsvewCNWNTwWL0KyX8O8=/0x389:2732x1819/fit-in/1200x630/cdn.vox-cdn.com/uploads/chorus_asset/file/22802887/IMG_DE272BF718E8_1.jpeg", pairdId: 2, id: "" },
-    { photo: "https://www.dccomics.com/sites/default/files/Char_Gallery_Flash_758_6055049612af35.61135649.jpg", pairdId: 3, id: "" },
-    { photo: "https://www.dccomics.com/sites/default/files/Char_Gallery_Flash_758_6055049612af35.61135649.jpg", pairdId: 3, id: "" },
-    { photo: "https://m.media-amazon.com/images/M/MV5BMTYzODQzYjQtNTczNC00MzZhLTg1ZWYtZDUxYmQ3ZTY4NzA1XkEyXkFqcGdeQXVyODE5NzE3OTE@._V1_.jpg", pairdId: 4, id: "" },
-    { photo: "https://m.media-amazon.com/images/M/MV5BMTYzODQzYjQtNTczNC00MzZhLTg1ZWYtZDUxYmQ3ZTY4NzA1XkEyXkFqcGdeQXVyODE5NzE3OTE@._V1_.jpg", pairdId: 4, id: "" },
-    { photo: "https://www.entertainmentearth.com/news/wp-content/uploads/2014/10/iron-man-wrap.jpg", pairdId: 5, id: "" },
-    { photo: "https://www.entertainmentearth.com/news/wp-content/uploads/2014/10/iron-man-wrap.jpg", pairdId: 5, id: "" },
-    { photo: "https://www.pinkvilla.com/imageresize/mark-ruffalo-hulk.jpg?width=752&format=webp&t=pvorg", pairdId: 6, id: "" },
-    { photo: "https://www.pinkvilla.com/imageresize/mark-ruffalo-hulk.jpg?width=752&format=webp&t=pvorg", pairdId: 6, id: "" }];
+function uid() {
+    return Date.now().toString(36) + Math.random().toString(36).substr(2);
+}
+let games = [
+    { photo: "https://publish.one37pm.net/wp-content/uploads/2020/05/spiderman-mobile.jpg" },
+    { photo: "https://cdn.vox-cdn.com/thumbor/ymxjRNyfsvewCNWNTwWL0KyX8O8=/0x389:2732x1819/fit-in/1200x630/cdn.vox-cdn.com/uploads/chorus_asset/file/22802887/IMG_DE272BF718E8_1.jpeg" },
+    { photo: "https://www.dccomics.com/sites/default/files/Char_Gallery_Flash_758_6055049612af35.61135649.jpg" },
+    { photo: "https://m.media-amazon.com/images/M/MV5BMTYzODQzYjQtNTczNC00MzZhLTg1ZWYtZDUxYmQ3ZTY4NzA1XkEyXkFqcGdeQXVyODE5NzE3OTE@._V1_.jpg" },
+    { photo: "https://upload.wikimedia.org/wikipedia/he/7/77/Iron_Man_Bleeding_Edge_Armor.jpg" },
+    { photo: "https://www.pinkvilla.com/imageresize/mark-ruffalo-hulk.jpg?width=752&format=webp&t=pvorg" },
+];
+function doubleCards(games) {
+    let doubleArray = [];
+    games.forEach((card) => {
+        const tempObj = { photo: card.photo, pairID: uid() };
+        const card1 = Object.assign({}, tempObj);
+        const card2 = Object.assign({}, tempObj);
+        card1.uniqueID = uid();
+        card2.uniqueID = uid();
+        doubleArray = [...doubleArray, card1, card2];
+    });
+    return doubleArray;
+}
+const deck = doubleCards(games);
 app.get('/', (req, res) => {
     res.send('Hello World!');
 });
 app.get("/newGame", (req, res) => {
-    const cards = shuffleCards();
-    res.send(cards);
+    shuffleCards(deck);
+    console.log(deck);
+    res.send(deck);
 });
 app.listen(port, () => {
     return console.log(`Express is listening at http://localhost:${port}`);
 });
-app.post("/add-card", (req, res) => {
-    console.log(req.body);
-    const newCard = req.body.name;
-    const newPairedId = req.body.name;
-    const cards = addPhoto(newCard, newPairedId);
-    res.send(cards);
-});
-function addPhoto(photo, pairdId) {
-    const id = Math.random().toString(36);
-    games.push({ photo, pairdId, id });
-    return games;
-}
-function shuffleCards() {
-    for (let i = games.length - 1; i > 0; i--) {
-        let j = Math.floor(Math.random() * i);
-        let temp = games[i];
+function shuffleCards(games) {
+    for (let i = 0; i < games.length; i++) {
+        const j = Math.floor(Math.random() * (i + 1));
+        const temp = games[i];
         games[i] = games[j];
         games[j] = temp;
     }
-    return games;
 }
+app.post("/isPair", (req, res) => {
+    const pairId = req.body.name;
+    res.send(pairId);
+});
