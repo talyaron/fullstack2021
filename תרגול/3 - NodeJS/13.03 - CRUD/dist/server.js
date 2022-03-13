3ed22c04045d349050cded29c08c8c58ee1624ec
Katya Rukosuev <72752681+RuKatya@users.noreply.github.com>
2022-03-13 17:45:50 +0200

    תרגול 13.03

---
+++ תרגול/3 - NodeJS/13.03 - CRUD/dist/server.js
@@ -0,0 +1,19 @@
+"use strict";
+var __importDefault = (this && this.__importDefault) || function (mod) {
+    return (mod && mod.__esModule) ? mod : { "default": mod };
+};
+Object.defineProperty(exports, "__esModule", { value: true });
+const express_1 = __importDefault(require("express"));
+const app = express_1.default();
+const port = 30001;
+app.use(express_1.default.json());
+app.use(express_1.default.static("public"));
+let tasks = [
+    { id: "1", title: "one" },
+    { id: "2", title: "two" },
+    { id: "3", title: "three" },
+    { id: "4", title: "four" }
+];
+app.listen(port, () => {
+    console.log(`Server listen on port http://localhost:${port}`);
+});
