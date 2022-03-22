d7cf57d88f8aae01c92f896880fdee7c9af63690
Katya Rukosuev <72752681+RuKatya@users.noreply.github.com>
2022-03-22 21:58:40 +0200

    2203

--- תרגול/3 - NodeJS/1 - EJS/server.ts
+++ תרגול/3 - NodeJS/1 - EJS/server.ts
@@ -1,11 +1,17 @@
 import express from "express";
+import path from 'path'
 const app = express();
 const port: number = 3000;
+
+app.set('view engine', 'ejs') //connecting ejs
+console.log(app.get('view engine'))
+// app.set('views', path.resolve(__dirname, 'pages'))
 
 interface User {
     name: string,
     age: number
 }
+
 const users: Array<User> = [
     { name: "aaa", age: 15 },
     { name: "bbb", age: 23 },
@@ -13,6 +19,20 @@ const users: Array<User> = [
     { name: "ddd", age: 56 },
     { name: "eee", age: 78 }
 ]
+
+app.get('/', (req, res) => {
+    res.render('index', {
+        title: "Home Page",
+        users
+    })
+})
+
+app.get('/about', (req, res) => {
+    res.render('about', {
+        title: "ABOUT"
+    })
+})
+
 
 
 app.listen(port, () => {
