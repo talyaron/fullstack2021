873d655c0331090067771226ccb178b05f8d57f0
Alex <pencilandjelly@gmail.com>
2022-03-27 11:37:37 +0300

    alex

---
+++ 98-Projects/Mongoose-Project/SpotifyApp/routes/userRoutes.ts
@@ -0,0 +1,12 @@
+import express from 'express';
+const router = express.Router();
+
+
+
+router.get()
+
+
+
+
+
+export default router;
--- 98-Projects/Mongoose-Project/SpotifyApp/server.ts
+++ 98-Projects/Mongoose-Project/SpotifyApp/server.ts
@@ -7,45 +7,43 @@ app.use(express.static("public"));
 app.use(express.json());
 
 mongoose.connect('mongodb+srv://alexroz:KwBuns8lVDNUTHhq@cluster0.x62d1.mongodb.net/spotifyApp?retryWrites=true&w=majority')
-mongoose.connect('mongodb+srv://leon93:ym965874632541@cluster0.umet4.mongodb.net/spotifyApp?retryWrites=true&w=majority')
+
 const UserSchema = new mongoose.Schema({
   searches: String,
   topGenres: String,
   
   })
 const spotifyApi = new mongoose.Schema({
-  searches: String,
-  topGenres: String,
+  // method: 'GET',
+  // url: 'https://spotify23.p.rapidapi.com/search/',
+  // params: {
+  //   q: 'arcade fire',
+  //   type: 'multi',
+  //   offset: '0',
+  //   limit: '10',
+  //   numberOfTopResults: '5'
+  // },
+  // headers: {
+  //   'X-RapidAPI-Host': 'spotify23.p.rapidapi.com',
+  //   'X-RapidAPI-Key': 'bfca630f50msh9fbc18f37cdf463p18b09bjsn7f35bfc71860'
+  // }
   
   })
+
+
 const User = mongoose.model('userdata', UserSchema)
 const spotify = mongoose.model('spotifydata', spotifyApi)
 
 
 
-var axios = require("axios").default;
 
-var options = {
-  method: 'GET',
-  url: 'https://spotify23.p.rapidapi.com/search/',
-  params: {
-    q: 'arcade fire',
-    type: 'multi',
-    offset: '0',
-    limit: '10',
-    numberOfTopResults: '5'
-  },
-  headers: {
-    'X-RapidAPI-Host': 'spotify23.p.rapidapi.com',
-    'X-RapidAPI-Key': 'bfca630f50msh9fbc18f37cdf463p18b09bjsn7f35bfc71860'
-  }
-};
 
-axios.request(options).then(function (response) {
-	console.log(response.data);
-}).catch(function (error) {
-	console.error(error);
+
+app.get('/get-music', async (req,res)=>{
+const musicArchive = await spotify.find({});
+res.send({ok:true, musicArchive})
 });
+
 
 
 
