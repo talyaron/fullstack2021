2bd9ab4e447c2c65fc5fdea98d97ccae883793f5
shayfoyer <93338189+shayfoyer@users.noreply.github.com>
2022-03-27 20:59:26 +0300

    shay

--- 98-Projects/Mongoose-Project/Airbnb/airbnb new/server.ts
+++ 98-Projects/Mongoose-Project/Airbnb/airbnb new/server.ts
@@ -38,9 +38,29 @@ const PlacesSchema = new mongoose.Schema({
   cancellation_policy: String,
   address: Object,
 });
-const { MongoClient } = require("mongodb");
-const url = "mongodb://localhost:27017";
+const { MongoClient } = require('mongodb');
+const url = 'mongodb://localhost:27017/example';
 const client = new MongoClient(url);
+const dbName = 'myProject';
+
+async function main() {
+  // Use connect method to connect to the server
+  await client.connect();
+  console.log('Connected successfully to server');
+  const db = client.db(dbName);
+  const collection = db.collection('documents');
+  const findResult = await collection.find({}).toArray();
+  // the following code examples can be pasted here...
+    console.log(findResult);
+    
+  return 'done.';
+}
+
+main()
+  .then(console.log)
+  .catch(console.error)
+  .finally(() => client.close());
+
 
 const userSchema = new mongoose.Schema({
   name: String,
