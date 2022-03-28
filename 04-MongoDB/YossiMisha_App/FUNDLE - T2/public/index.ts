eb1fdbd9ba46546cafa4b3407eacabba890addfb
Michael Dubovyk <93338459+mishalon@users.noreply.github.com>
2022-03-28 19:40:48 +0300

    1

--- 04-MongoDB/YossiMisha_App/FUNDLE - T2/public/index.ts
+++ 04-MongoDB/YossiMisha_App/FUNDLE - T2/public/index.ts
@@ -109,7 +109,63 @@ function handleShowStats() {
         }
   }
 
-
-
   document.body.addEventListener('click', handleDisplayNone, true); 
+
+  //////////////////////////// LOGIN - REGISTER ///////////////////////////////////////////
+
+  function handleNotAMember(){
+    const register:any = document.querySelector(".registerwrapper")
+    const login:any = document.querySelector(".loginwrapper")
+    register.style.display = 'block'
+    login.style.display = 'none'
+}
+function handleAlreadyAMember(){
+    const register:any = document.querySelector(".registerwrapper")
+    const login:any = document.querySelector(".loginwrapper")
+    register.style.display = 'none'
+    login.style.display = 'block'
+}
+
+
+async function handleRegister(ev) {
+    try {
+
+        ev.preventDefault();
+
+        let { username, password, confirmPassword, email, confirmEmail } = ev.target.elements
+
+        username = username.value;
+        password = password.value;
+        confirmPassword = confirmPassword.value;
+        email = email.value;
+        confirmEmail = confirmEmail.value;
+
+        if (password !== confirmPassword) {
+            window.alert('passwords dont match')
+        }
+        if (email !== confirmEmail) {
+            window.alert('emails dont match')
+        }
+        if (password === confirmPassword && email === confirmEmail) {
+            const { data } = axios.post('/add-user', { username, password, email })
+            return data;
+        }
+
+    }
+
+    catch (error) {
+        console.log(error);
+    }
+}
+
+
+async function handleLogin(ev) {
+    ev.preventDefault();
+    let { username, password } = ev.target.elements
+    username = username.value;
+    password = password.value;
+    const { data } = await axios.get(`/get-user?username=${username}&password=${password}`)
+    console.log(data)
+}
+
   
