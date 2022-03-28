d4af514d22ad835bcd5267dae6e6100526990229
shayfoyer <93338189+shayfoyer@users.noreply.github.com>
2022-03-28 16:56:22 +0300

    shay

--- 98-Projects/Mongoose-Project/Airbnb/airbnb new/server.ts
+++ 98-Projects/Mongoose-Project/Airbnb/airbnb new/server.ts
@@ -16,28 +16,28 @@ app.set("views", path.resolve(__dirname, "pages"));
 app.get("/", (req, res) => {
   res.send("Hello World!");
 });
-const PlacesSchema = new mongoose.Schema({
-  name: String,
-  summary: String,
-  checkIn: String,
-  checkOut: String,
-  accommodates: Number,
-  amenities: Array,
-  bedrooms: Number,
-  beds: Number,
-  number_of_reviews: Number,
-  price: Number,
-  cancle: String,
-  bathrooms: Number,
-  images: String,
-  host: Object,
-  space: String,
-  description: String,
-  bed_type: String,
-  reviews: Array,
-  cancellation_policy: String,
-  address: Object,
-});
+// const PlacesSchema = new mongoose.Schema({
+//   name: String,
+//   summary: String,
+//   checkIn: String,
+//   checkOut: String,
+//   accommodates: Number,
+//   amenities: Array,
+//   bedrooms: Number,
+//   beds: Number,
+//   number_of_reviews: Number,
+//   price: Number,
+//   cancle: String,
+//   bathrooms: Number,
+//   images: String,
+//   host: Object,
+//   space: String,
+//   description: String,
+//   bed_type: String,
+//   reviews: Array,
+//   cancellation_policy: String,
+//   address: Object,
+// });
 const { MongoClient } = require('mongodb');
 const url = 'mongodb://localhost:27017/example';
 const client = new MongoClient(url);
@@ -62,86 +62,86 @@ main()
   .finally(() => client.close());
 
 
-const userSchema = new mongoose.Schema({
-  name: String,
-  password: String,
-});
-const Users = mongoose.model("users", userSchema);
-const Places = mongoose.model("listingsAndReviews", PlacesSchema);
-app.get("/getPlaces", async (req, res) => {
-  try {
-    const places = await Places.find({});
-    res.send({ ok: true, places });
-  } catch (error) {
-    console.error(error);
-    res.send({ error: "error in app.get/getPlaces" });
-  }
-});
-app.post("/addPlaces", async (req, res) => {
-  try {
-    let {
-      name,
-      summary,
-      checkIn,
-      checkOut,
-      accommodates,
-      amenities,
-      bedrooms,
-      beds,
-      number_of_reviews,
-      price,
-      cancle,
-      bathrooms,
-      images,
-      host,
-      space,
-      description,
-      bed_type,
-      reviews,
-      cancellation_policy,
-      address,
-    } = req.body;
-    const newPlace = new Places({
-      name,
-      summary,
-      checkIn,
-      checkOut,
-      accommodates,
-      amenities,
-      bedrooms,
-      beds,
-      number_of_reviews,
-      price,
-      cancle,
-      bathrooms,
-      images,
-      host,
-      space,
-      description,
-      bed_type,
-      reviews,
-      cancellation_policy,
-      address,
-    });
+// const userSchema = new mongoose.Schema({
+//   name: String,
+//   password: String,
+// });
+// const Users = mongoose.model("users", userSchema);
+// const Places = mongoose.model("listingsAndReviews", PlacesSchema);
+// app.get("/getPlaces", async (req, res) => {
+//   try {
+//     const places = await Places.find({});
+//     res.send({ ok: true, places });
+//   } catch (error) {
+//     console.error(error);
+//     res.send({ error: "error in app.get/getPlaces" });
+//   }
+// });
+// app.post("/addPlaces", async (req, res) => {
+//   try {
+//     let {
+//       name,
+//       summary,
+//       checkIn,
+//       checkOut,
+//       accommodates,
+//       amenities,
+//       bedrooms,
+//       beds,
+//       number_of_reviews,
+//       price,
+//       cancle,
+//       bathrooms,
+//       images,
+//       host,
+//       space,
+//       description,
+//       bed_type,
+//       reviews,
+//       cancellation_policy,
+//       address,
+//     } = req.body;
+//     const newPlace = new Places({
+//       name,
+//       summary,
+//       checkIn,
+//       checkOut,
+//       accommodates,
+//       amenities,
+//       bedrooms,
+//       beds,
+//       number_of_reviews,
+//       price,
+//       cancle,
+//       bathrooms,
+//       images,
+//       host,
+//       space,
+//       description,
+//       bed_type,
+//       reviews,
+//       cancellation_policy,
+//       address,
+//     });
 
-    const result = await newPlace.save();
-    res.send({ ok: true, result });
-  } catch (error) {
-    console.error(error);
-    res.send({ error: error.massage });
-  }
-});
-app.post("/addUsers", async (req, res) => {
-  try {
-    let { user, password } = req.body;
-    const newUser = new Users({ user, password });
-    const result = await newUser.save();
-    res.send({ ok: true, result });
-  } catch (error) {
-    console.error(error);
-    res.send({ error: error.massage });
-  }
-});
+//     const result = await newPlace.save();
+//     res.send({ ok: true, result });
+//   } catch (error) {
+//     console.error(error);
+//     res.send({ error: error.massage });
+//   }
+// });
+// app.post("/addUsers", async (req, res) => {
+//   try {
+//     let { user, password } = req.body;
+//     const newUser = new Users({ user, password });
+//     const result = await newUser.save();
+//     res.send({ ok: true, result });
+//   } catch (error) {
+//     console.error(error);
+//     res.send({ error: error.massage });
+//   }
+// });
 // const options:any = {
 //   method: 'GET',
 //   url: 'https://hotels-com-free.p.rapidapi.com/pde/property-details/v1/hotels.com/106346',
@@ -163,26 +163,26 @@ app.post("/addUsers", async (req, res) => {
 // }).catch(function (error) {
 //   console.error(error);
 // });
-app.get("/goToPlace", (req, res) => {
-  try {
-    const { placeId } = req.body;
-    const __id = req.body;
-    if (__id == placeId) {
-      res.send(placeId);
-    } else {
-      throw new Error("placeId is not the same as the __id");
-    }
-  } catch (error) {
-    console.log(error.error);
-    res.send({ error: error.massage });
-  }
-});
-app.post("/findPlaceMap", async (req, res) => {
-  let coordinates: Array<any> = req.body;
-  //  const center: google.maps.LatLngLiteral = {lat: 30, lng: -110};
-  // initMap(center)
-  res.send(coordinates);
-});
+// app.get("/goToPlace", (req, res) => {
+//   try {
+//     const { placeId } = req.body;
+//     const __id = req.body;
+//     if (__id == placeId) {
+//       res.send(placeId);
+//     } else {
+//       throw new Error("placeId is not the same as the __id");
+//     }
+//   } catch (error) {
+//     console.log(error.error);
+//     res.send({ error: error.massage });
+//   }
+// });
+// app.post("/findPlaceMap", async (req, res) => {
+//   let coordinates: Array<any> = req.body;
+//   //  const center: google.maps.LatLngLiteral = {lat: 30, lng: -110};
+//   // initMap(center)
+//   res.send(coordinates);
+// });
 // let map: google.maps.Map;
 
 // function initMap(center): void {
@@ -202,18 +202,18 @@ app.post("/findPlaceMap", async (req, res) => {
 //     const filteredCity=filteredCitySearch(search);
 //     res.send(filteredCity)
 // })
-app.get('/search-airbnb', async(req,res)=>{
-    const search=req.query.search;
-    const checkIn=req.query.checkIn;
-    const checkOut=req.query.checkOut;
-    const adults=req.query.adults;
-    const children=req.query.children;
-    const infants=req.query.infants;
-    const pets=req.query.pets;
-    const foundLocation=await Places.find({address:search});
-    console.log(foundLocation)
-    //res.send({city:filteredCity})
-})
+// app.get('/search-airbnb', async(req,res)=>{
+//     const search=req.query.search;
+//     const checkIn=req.query.checkIn;
+//     const checkOut=req.query.checkOut;
+//     const adults=req.query.adults;
+//     const children=req.query.children;
+//     const infants=req.query.infants;
+//     const pets=req.query.pets;
+//     const foundLocation=await Places.find({address:search});
+//     console.log(foundLocation)
+//     //res.send({city:filteredCity})
+// })
 
 // function filteredCitySearch(search) {
    
@@ -226,6 +226,8 @@ app.get('/search-airbnb', async(req,res)=>{
   
   
 // }
+import userRoutes from './routes/userRoutes'
+app.use('/users', userRoutes)
 
 app.listen(port, () => {
   return console.log(`Express is listening at http://localhost:${port}`);
