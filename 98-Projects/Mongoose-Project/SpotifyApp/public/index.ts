cb54d7131ed9ac48f1e9b71a172882f2103edae9
leon <lmalul13@gmail.com>
2022-03-30 19:16:35 +0300

    54

--- 98-Projects/Mongoose-Project/SpotifyApp/public/index.ts
+++ 98-Projects/Mongoose-Project/SpotifyApp/public/index.ts
@@ -1,3 +1,5 @@
+import songs from "../model/songsModel";
+
 function handleGetSearch() {
     // const { data } = await axios.get('/get-music')
     // console.log(data);
@@ -19,7 +21,9 @@ async function handleUpload(ev) {
     console.log(song, picture, genre, youtube)
 
     const { data } = await axios.post('/songsOwner/upload-song', { song, picture, genre, youtube })
+    const songs={data}
     console.log(data)
+    renderSongs(songs)
 }
 
 async function handleRegistration(ev) {
@@ -48,3 +52,28 @@ async function handleLogIn(ev) {
     const { data } = await axios.post('/songsUser/sign-user', { name, password })
     console.log(data)
 }
+
+
+
+function renderSongs(songs) {
+    const root =document.querySelector('#likedSongs')
+    try {
+        const html = songs.map(songs => {
+            // console.log(songs);
+            
+            return `<div class="card"><h2>${songs.song} :${songs.picture} ,${songs.genre} ,${songs.youtube}</h2>
+            <div><input type='text' placeholder='type' value='${songs.genre}' onblur='handleupdate(event,"${songs._id}")'/></div>
+            <button onclick='handleDelete("${songs._id}")'>Delete</button>
+            <div><img src="${songs.img}" alt=""></div></div>`  
+        }).join('')
+  
+        root.innerHTML = html
+  
+        if (!root) throw new Error("no root in rendersongs");
+  
+    } catch (error) {
+        console.error(error.message);
+  
+    }
+  
+  }
