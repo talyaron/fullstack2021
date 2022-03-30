d823ff8a7aac5393329f937aaefa9b445e77d3e5
shayfoyer <93338189+shayfoyer@users.noreply.github.com>
2022-03-30 21:04:39 +0300

    shay

--- 98-Projects/Mongoose-Project/Airbnb/airbnb new/public/index.ts
+++ 98-Projects/Mongoose-Project/Airbnb/airbnb new/public/index.ts
@@ -263,7 +263,7 @@ try{
  </div> 
 </div>
 <div id="map">
-
+<iframe src="https://www.google.com/maps/embed?pb=!1m14!1m12!1m3!1d26081603.294420466!2d-95.677068!3d37.06250000000001!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!5e0!3m2!1siw!2s!4v1648657793371!5m2!1siw!2s" width="600" height="450" style="border:0;" allowfullscreen="" loading="lazy" referrerpolicy="no-referrer-when-downgrade"></iframe>
 </div>
 <div class="aboutHost">
     <div class="aboutHost--left">
