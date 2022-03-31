c6cecf1c4caf61b72293908df7532854636afb47
JeniaMayor <jenia50@gmail.com>
2022-03-31 20:27:41 +0300

    ///

--- 98-Projects/Mongoose-Project/Jenia-Ido/routes/routes/dist/imagesRoute.js
+++
@@ -1,9 +0,0 @@
-"use strict";
-exports.__esModule = true;
-var express_1 = require("express");
-var router = express_1["default"].Router();
-var imagesCont_1 = require("../controlers/imagesCont");
-router
-    .get('/get-images', imagesCont_1.getImages)
-    .post('/add-images', imagesCont_1.addImages);
-exports["default"] = router;
