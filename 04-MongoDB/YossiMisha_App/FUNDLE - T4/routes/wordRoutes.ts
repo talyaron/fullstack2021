96a9235a3dab3425ec64a30688dce689d47232ad
Yossi A <asnafy.junior@gmail.com>
2022-04-03 21:00:38 +0300

    FUNDLE - T4

---
+++ 04-MongoDB/YossiMisha_App/FUNDLE - T4/routes/wordRoutes.ts
@@ -0,0 +1,12 @@
+import express from 'express';
+const router = express.Router();
+
+import {addToDB, getDailyWord, wordExists} from "../controllers/wordsCont"
+
+router
+.get('/get-word', getDailyWord)
+.get('/get-guessCheck', wordExists)
+
+addToDB()
+
+export default router;
