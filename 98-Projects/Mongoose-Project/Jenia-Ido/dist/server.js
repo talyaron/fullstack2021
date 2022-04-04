d85c66fb0e8a97dce4595a7614441ebae70289ae
Ido Gino <davegino220@gmail.com>
2022-04-04 20:42:33 +0300

    l

--- 98-Projects/Mongoose-Project/Jenia-Ido/dist/server.js
+++ 98-Projects/Mongoose-Project/Jenia-Ido/dist/server.js
@@ -6,7 +6,9 @@ Object.defineProperty(exports, "__esModule", { value: true });
 const express_1 = __importDefault(require("express"));
 const mongoose_1 = __importDefault(require("mongoose"));
 const app = express_1.default();
-const port = process.env.PORT || 3003;
+const port = process.env.PORT || 5000;
+const cookieParser = require('cookie-parser');
+app.use(cookieParser());
 app.use(express_1.default.static("public"));
 app.use(express_1.default.json());
 mongoose_1.default.connect('mongodb+srv://igino11:kktgqbLMCE3mtTN6@cluster0.zfewx.mongodb.net/myFirstDatabase?retryWrites=true&w=majority').then(res => {
