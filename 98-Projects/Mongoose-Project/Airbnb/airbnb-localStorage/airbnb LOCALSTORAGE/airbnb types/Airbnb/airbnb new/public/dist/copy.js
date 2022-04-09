964ee9b381d8af1a8e780915c1b88eb7247bb4d0
ShaniRom <93940556+ShaniRom@users.noreply.github.com>
2022-04-09 23:34:57 +0300

    local storage try

---
+++ 98-Projects/Mongoose-Project/Airbnb/airbnb-localStorage/airbnb LOCALSTORAGE/airbnb types/Airbnb/airbnb new/public/dist/copy.js
@@ -0,0 +1,92 @@
+/** @class */ (function () {
+    function class_1() {
+    }
+    return class_1;
+}());
+"airbnbOptions";
+onclick = "location.href='place.ejs'" >
+     % ;
+if (getplaces) {
+     %  >
+         % getplaces.forEach(function (place) {
+             %  >
+                /** @class */ (function () {
+                    function class_2() {
+                    }
+                    return class_2;
+                }());
+            "airbnbOptions__container";
+            onclick = "handleGoToPlace(_id)" >
+                /** @class */ (function () {
+                    function class_3() {
+                    }
+                    return class_3;
+                }());
+            "airbnbOptions__container__img" >
+                src;
+            "<%- place.images %>" > /img>
+                < /div>
+                < div;
+            var default_1 = /** @class */ (function () {
+                function default_1() {
+                }
+                return default_1;
+            }());
+            "airbnbOptions__container__content" >
+                /** @class */ (function () {
+                    function class_4() {
+                    }
+                    return class_4;
+                }());
+            "airbnbOptions__container__content__namePlace" >
+                 % -place.name %  > /h3>
+                < h5 > accommodates;
+             % -place.accommodates %  > /h5>
+                < p >
+                 % -place.address_country_code %  > ,  % -place.address_country %  >
+                /p>
+                < /div>
+                < div;
+            var default_2 = /** @class */ (function () {
+                function default_2() {
+                }
+                return default_2;
+            }());
+            "airbnbOptions__container__content__description" >
+                 % -place.description %  >
+                /p>
+                < /div>
+                < div;
+            var default_3 = /** @class */ (function () {
+                function default_3() {
+                }
+                return default_3;
+            }());
+            "airbnbOptions__container__content__priceRating" >
+                /** @class */ (function () {
+                    function class_5() {
+                    }
+                    return class_5;
+                }());
+            "btn btn-outline" >
+                 % -place.price %  >
+                /button>
+                < button;
+            var default_4 = /** @class */ (function () {
+                function default_4() {
+                }
+                return default_4;
+            }());
+            "btn btn-outline" >
+                 % -place.reviews_rating %  >
+                /button>
+                < /div>
+                < /div>
+                < /div>
+                <  % ;
+        }) %  >
+         % ;
+}
+ %  >
+    /div>
+    < /div>;
