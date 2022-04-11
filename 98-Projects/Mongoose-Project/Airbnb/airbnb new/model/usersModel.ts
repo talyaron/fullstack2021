3168d7b1fd200780cb401d3e041d7512156ae5e3
shayfoyer <93338189+shayfoyer@users.noreply.github.com>
2022-04-11 17:16:08 +0300

    shjay

--- 98-Projects/Mongoose-Project/Airbnb/airbnb new/model/usersModel.ts
+++
@@ -1,9 +0,0 @@
-import mongoose from "mongoose";
-const userSchema = new mongoose.Schema({
-    name: String,
-    password: String,
-    role:String
-  });
-  const Users = mongoose.model("users", userSchema);
-  export default Users;
-  
