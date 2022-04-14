5f82f856524c899c0add1457f0fad72a161a393b
AndrewLishafay <andrew567234@gmail.com>
2022-04-14 17:27:40 +0300

    123

--- 03-Node-js/04-typescript/follow/Andrew/cookies/Michael_Andrew/routes/dist/artRoutes.js
+++ 03-Node-js/04-typescript/follow/Andrew/cookies/Michael_Andrew/routes/dist/artRoutes.js
@@ -3,11 +3,12 @@ exports.__esModule = true;
 var express_1 = require("express");
 var router = express_1["default"].Router();
 var artCont_1 = require("../conroler/artCont");
+var hasFunds_1 = require("../middleware/hasFunds");
 router
     .post("/add-art", artCont_1.addArtToUser)
     .get("/get-user-art", artCont_1.getMyArt)
     .patch("/putArtOnSale", artCont_1.putArtOnSale)
     .patch("/cancelSale", artCont_1.cancelSale)
     .get("/art-for-sale", artCont_1.getForSale)
-    .patch('/buy-and-sell', artCont_1.buyAndSell);
+    .patch('/buy-and-sell', hasFunds_1.hasFunds, artCont_1.buyAndSell);
 exports["default"] = router;
--- 03-Node-js/04-typescript/follow/Andrew/cookies/Michael_Andrew/routes/userRoutes.ts
+++ 03-Node-js/04-typescript/follow/Andrew/cookies/Michael_Andrew/routes/userRoutes.ts
@@ -1,6 +1,7 @@
 import express from 'express';
 const router = express.Router();
 import {addUser, findUser, updateUser, addArtToUser, buyAndSell} from '../conroler/userCont'
+import { hasFunds } from '../middleware/hasFunds';
 
 
 
@@ -8,6 +9,6 @@ router
 .post('/add-user', addUser)
 .get('/log-user', findUser)
 .patch('/update-user', updateUser)
-.patch('/buy-and-sell', buyAndSell)
+.patch('/buy-and-sell',hasFunds, buyAndSell)
 
 export default router;
