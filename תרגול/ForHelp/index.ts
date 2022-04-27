b1c2cbf9703a8a1c64e789bd3b2d53f17594696c
Katya Rukosuev <72752681+RuKatya@users.noreply.github.com>
2022-04-27 19:21:10 +0300

    react

---
+++ תרגול/ForHelp/index.ts
@@ -0,0 +1,9 @@
+// https://animate.style/
+// https://animista.net/play/basic/slide-fwd
+// https://www.restapitutorial.com/httpstatuscodes.html
+// https://autoprefixer.github.io/
+// https://html5up.net/
+// https://codepen.io/Anna_Batura/pen/dypeoxj
+// http://www.layerstyles.org/builder.html
+
+// 
