b818a7337126c89caf1f238f47e1853a24573478
OmriAharonov <omri832@gmail.com>
2022-04-28 20:01:53 +0300

    room 8

--- 05-React/01-basic/Example2/omri/src/view/components/Box.tsx
+++ 05-React/01-basic/Example2/omri/src/view/components/Box.tsx
@@ -1,13 +1,18 @@
-interface BoxProps{
-    text: string,
-    title:string
+interface BoxProps {
+    text: string;
+    title: string;
+    img: string;
 }
 
-const Box = (props:BoxProps) => {
-    const {text}= props;
-  return (
-    <div className='box'>Box: {text}</div>
-  )
-}
+const Box = (props: BoxProps) => {
+    const { text, title, img } = props;
+    return (
+        <div className='box'>
+            <img src={img} />
+            <div className='title'>{title}</div>
+            <div className='text'>{text} </div>
+        </div>
+    );
+};
 
 export default Box
