ee47c4071586df92d19c913dda11bd0cc5a4a40a
Chaim <chaimytcohen@gmail.com>
2022-04-28 12:49:36 +0300

    chaim

---
+++ 05-React/01-basic/followups/chaim-react/src/Chaim.tsx
@@ -0,0 +1,11 @@
+interface BoxProps{
+  text: string
+}
+const box = (props:BoxProps) => {
+  const {text}=props
+  return (
+    <div className="box">box: {text}</div>
+  )
+}
+
+export default box
