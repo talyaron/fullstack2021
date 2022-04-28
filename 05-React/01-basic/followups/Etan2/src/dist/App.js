365bce1185b7fd1f2d3bd5d42e60fc6310f94456
Etanhey <etan@heyman.net>
2022-04-28 12:30:32 -0500

    .

--- 05-React/01-basic/followups/Etan2/src/dist/App.js
+++ 05-React/01-basic/followups/Etan2/src/dist/App.js
@@ -2,17 +2,15 @@
 exports.__esModule = true;
 var react_1 = require("react");
 var logo_svg_1 = require("./logo.svg");
-require("./App.scss");
-var StatusState_1 = require("./StatusState");
-var name = 'Etan';
-var status = document.readyState;
+require("./View/styles/");
+// import './View/styles/gl'
+var Box_1 = require("./View/components/Box");
 function App() {
     return (react_1["default"].createElement("div", { className: "App" },
         react_1["default"].createElement("header", { className: "App-header" },
             react_1["default"].createElement("img", { src: logo_svg_1["default"], className: "App-logo", alt: "logo" }),
-            react_1["default"].createElement("div", null,
-                name,
-                react_1["default"].createElement(StatusState_1["default"], { status: status })),
+            react_1["default"].createElement("p", null,
+                react_1["default"].createElement(Box_1["default"], { text: "hi" })),
             react_1["default"].createElement("a", { className: "App-link", href: "https://reactjs.org", target: "_blank", rel: "noopener noreferrer" }, "Learn React"))));
 }
 exports["default"] = App;
