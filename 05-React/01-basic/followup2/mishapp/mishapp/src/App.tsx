35d1e63d7fe64616e88be5816d369eedd219ad8e
Michael Dubovyk <93338459+mishalon@users.noreply.github.com>
2022-04-29 17:39:11 +0400

    1

--- 05-React/01-basic/followup2/mishapp/mishapp/src/App.tsx
+++ 05-React/01-basic/followup2/mishapp/mishapp/src/App.tsx
@@ -10,14 +10,11 @@ interface BoxProps {
 const boxes: Array<BoxProps> = [
 
   {img:'https://upload.wikimedia.org/wikipedia/en/thumb/1/17/Bugs_Bunny.svg/1200px-Bugs_Bunny.svg.png'},
-  {img:'https://www.google.com/url?sa=i&url=https%3A%2F%2Far.linkedin.com%2Fcompany%2Fimg-golf-course-management&psig=AOvVaw3dsbk8BbwrFaKO5uQdTq8l&ust=1651317362387000&source=images&cd=vfe&ved=0CAwQjRxqFwoTCNDTttiSufcCFQAAAAAdAAAAABAI'},
-  {img:'https://www.google.com/url?sa=i&url=https%3A%2F%2Far.linkedin.com%2Fcompany%2Fimg-golf-course-management&psig=AOvVaw3dsbk8BbwrFaKO5uQdTq8l&ust=1651317362387000&source=images&cd=vfe&ved=0CAwQjRxqFwoTCNDTttiSufcCFQAAAAAdAAAAABAI'},
-  {img:'https://www.google.com/url?sa=i&url=https%3A%2F%2Far.linkedin.com%2Fcompany%2Fimg-golf-course-management&psig=AOvVaw3dsbk8BbwrFaKO5uQdTq8l&ust=1651317362387000&source=images&cd=vfe&ved=0CAwQjRxqFwoTCNDTttiSufcCFQAAAAAdAAAAABAI'},
-  {img:'https://www.google.com/url?sa=i&url=https%3A%2F%2Far.linkedin.com%2Fcompany%2Fimg-golf-course-management&psig=AOvVaw3dsbk8BbwrFaKO5uQdTq8l&ust=1651317362387000&source=images&cd=vfe&ved=0CAwQjRxqFwoTCNDTttiSufcCFQAAAAAdAAAAABAI'},
-  {img:'https://www.google.com/url?sa=i&url=https%3A%2F%2Far.linkedin.com%2Fcompany%2Fimg-golf-course-management&psig=AOvVaw3dsbk8BbwrFaKO5uQdTq8l&ust=1651317362387000&source=images&cd=vfe&ved=0CAwQjRxqFwoTCNDTttiSufcCFQAAAAAdAAAAABAI'},
-  {img:'https://www.google.com/url?sa=i&url=https%3A%2F%2Far.linkedin.com%2Fcompany%2Fimg-golf-course-management&psig=AOvVaw3dsbk8BbwrFaKO5uQdTq8l&ust=1651317362387000&source=images&cd=vfe&ved=0CAwQjRxqFwoTCNDTttiSufcCFQAAAAAdAAAAABAI'},
-  {img:'https://www.google.com/url?sa=i&url=https%3A%2F%2Far.linkedin.com%2Fcompany%2Fimg-golf-course-management&psig=AOvVaw3dsbk8BbwrFaKO5uQdTq8l&ust=1651317362387000&source=images&cd=vfe&ved=0CAwQjRxqFwoTCNDTttiSufcCFQAAAAAdAAAAABAI'},
-  {img:'https://www.google.com/url?sa=i&url=https%3A%2F%2Far.linkedin.com%2Fcompany%2Fimg-golf-course-management&psig=AOvVaw3dsbk8BbwrFaKO5uQdTq8l&ust=1651317362387000&source=images&cd=vfe&ved=0CAwQjRxqFwoTCNDTttiSufcCFQAAAAAdAAAAABAI'}
+  {img:'https://upload.wikimedia.org/wikipedia/en/thumb/1/17/Bugs_Bunny.svg/1200px-Bugs_Bunny.svg.png'},
+  {img:'https://upload.wikimedia.org/wikipedia/en/thumb/1/17/Bugs_Bunny.svg/1200px-Bugs_Bunny.svg.png'},
+  {img:'https://upload.wikimedia.org/wikipedia/en/thumb/1/17/Bugs_Bunny.svg/1200px-Bugs_Bunny.svg.png'},
+  {img:'https://upload.wikimedia.org/wikipedia/en/thumb/1/17/Bugs_Bunny.svg/1200px-Bugs_Bunny.svg.png'},
+
 
 ]
 
