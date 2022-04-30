2a6e45a19418e9bf474f12267533646d96c326f5
AndrewLishafay <andrew567234@gmail.com>
2022-04-30 12:16:54 +0300

    Revert "123"
    This reverts commit ae32d39d858c791dc4e7080cc6c9cc614913b63f.

--- 05-React/01-basic/followups/Andrew/my-app/src/dist/Box.js
+++
@@ -1,9 +0,0 @@
-"use strict";
-exports.__esModule = true;
-var Box = function (props) {
-    var text = props.text, title = props.title;
-    return (React.createElement("div", null,
-        React.createElement("div", null, text),
-        React.createElement("div", null, title)));
-};
-exports["default"] = Box;
