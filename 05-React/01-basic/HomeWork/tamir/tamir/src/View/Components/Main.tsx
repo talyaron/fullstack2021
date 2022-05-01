d6a2e496b34b8c6c03344aa630879eb2ee7dd791
tamir-dadon <tamir.dadon63@gmail.com>
2022-05-01 17:19:39 +0300

    tamir

---
+++ 05-React/01-basic/HomeWork/tamir/tamir/src/View/Components/Main.tsx
@@ -0,0 +1,14 @@
+const Main = () => {
+    return (
+        <div className="Main">
+            <div className="Main__pic"> <img src="https://user-images.githubusercontent.com/73748430/106888894-37014d00-670d-11eb-860c-2c2dced9b0c7.png" alt="" /></div>
+            <div className="Main__pic"> <img src="https://th.bing.com/th/id/R.3419b5364e574fb516b7bb974ed7e1c0?rik=8%2f1PCNho1BwYtQ&riu=http%3a%2f%2f2.bp.blogspot.com%2f-F3KHWGxblfM%2fUU90Z11uikI%2fAAAAAAAAJuo%2fNMNyPHd16mQ%2fs1600%2ffunny-ctrl-z-cartoon.jpg&ehk=CEzIns2YACIBbH2U5ZGPFZD%2fR4tW2Pv1pi0Q4LYcVPk%3d&risl=&pid=ImgRaw&r=0" alt="" /></div>
+            <div className="Main__pic"> <img src="https://images-cdn.9gag.com/photo/aW87vWx_700b.jpg" alt="" /></div>
+            <div className="Main__pic"> <img src="https://i.pinimg.com/originals/9d/d5/52/9dd552fda78b7f923c63e082cee4ccb9.jpg" alt="" /></div>
+            <div className="Main__pic"> <img src="https://i.pinimg.com/originals/bb/f9/a1/bbf9a1f243bc0c1fb8d5c6dd5d6f5ff0.jpg" alt="" /></div>
+            <div className="Main__pic"> <img src="https://i.pinimg.com/originals/bf/f5/66/bff566dcacc83ada2ddfe178eccb0f8f.jpg" alt="" /></div>
+        </div>
+    )
+}
+
+export default Main
