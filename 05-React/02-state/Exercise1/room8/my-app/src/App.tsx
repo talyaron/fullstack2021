6cf785f24d321cf6c7a41eb85f406104f7a5d5fc
ohad gur <ohadgur22@gmail.com>
2022-05-02 20:14:43 +0300

    a

--- 05-React/02-state/Exercise1/room8/my-app/src/App.tsx
+++ 05-React/02-state/Exercise1/room8/my-app/src/App.tsx
@@ -1,49 +1,45 @@
 import React, { useState } from 'react';
 import logo from './logo.svg';
 import './App.css';
-interface Img{
-  text:string;
-  img:string;
+interface Img {
+  text: string;
+  img: string;
 }
 
 function App() {
   const [text, setText] = useState<string>('Start writing somthing');
   const [image, setImage] = useState<string>('');
-  const [arr,setArr]=useState<Array<Img>>([]);
-   
-  function handleAddImage(ev:any){
-    ev.preventDeafult();
-    const text=ev.target.text.value;
-    const img=ev.target.img.value;
-    const obj = {text,img};
-    setArr([...arr,obj]);
+  const [arr, setArr] = useState<Array<Img>>([]);
+
+  function handleAddImage(ev: any) {
+    ev.preventDefault();
+    const text = ev.target.text.value;
+    const img = ev.target.img.value;
+    const obj = { text, img };
+    setArr([...arr, obj]);
+
   }
 
-  function handleText(ev:any){
+  function handleText(ev: any) {
     setText(ev.target.value);
   }
 
-  function handleImage(ev:any){
+  function handleImage(ev: any) {
     setImage(ev.target.value);
   }
   return (
 
     <div className="App">
-      
-        
-      <p><input type="text"  onKeyUp={handleImage}/></p>
-      <img src={image} />
-    
-      <input type="text" onKeyUp={handleText} />
+
+      <form onSubmit={handleAddImage} >
+
+        <p>img:<input type="text" name='img' onKeyUp={handleImage} required={true} /></p>
+        <p> text:<input type="text" name='text' onKeyUp={handleText} required={true} /></p>
         <p>Text: {text}</p>
-
-
+        <img height='200px' width='200px' src={image} />
         <input type="submit" value="Add Image" />
-
-        
-        <form onSubmit={handleAddImage} >
-        </form>
-
+      </form>
+      {boxes.map((box,i)=>{return <Box key={i} img={box.img} author={box.author} text={box.text}/> })}
 
     </div>
   );
