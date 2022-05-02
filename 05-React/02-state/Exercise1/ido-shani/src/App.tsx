f446588c90d4709f17c4e003ce8acf5a4e3c766c
ShaniRom <93940556+ShaniRom@users.noreply.github.com>
2022-05-02 20:46:40 +0300

    react

--- 05-React/02-state/Exercise1/ido-shani/src/App.tsx
+++ 05-React/02-state/Exercise1/ido-shani/src/App.tsx
@@ -1,23 +1,29 @@
 import React from "react";
 
 import { useState } from "react";
+
 import "./Views/styles/global.scss";
 
 import Card from "./Views/Components/Card";
 
+interface cardProps {
+  imgUrl: string;
+  imgName: string;
+}
 
 function App() {
-  const [imgUrl, setImageUrl] = useState([]);
-  const [imgName, setImageName] = useState([]);
+  const [set, setMyArray] = useState<Array<cardProps>>([]);
+  const uuidv4 = require("uuid/v4")
 
   function handleCreatCard(ev: any) {
     ev.preventDefault();
-    setImageUrl(ev.target.elements.imgUrl.value);
-    setImageName(ev.target.elements.imgName.value);
 
-    
-    setMyArray(oldArray => [...oldArray, newElement]);
+    const imgUrl = ev.target.elements.imgUrl.value;
+    const imgName = ev.target.elements.imgName.value;
 
+    setMyArray([...set, { imgUrl, imgName }]);
+
+    console.log(set);
   }
 
   return (
@@ -28,7 +34,15 @@ function App() {
         <button type="submit">Submit</button>
       </form>
 
-      <Card imgUrl={imgUrl} imgName={imgName} />
+      <div className="imageCards">
+        {set.map((card, i) => (
+          <div className="card">
+            <img src={card.imgUrl} alt="" />
+            <h2>{card.imgName}</h2>
+            <button type="button" onClick="handleDelete"></button>
+          </div>
+        ))}
+      </div>
     </div>
   );
 }
