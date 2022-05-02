6d8e4ca619e98c5e7a038256d061890233c6a7bb
Etanhey <etan@heyman.net>
2022-05-02 12:33:29 -0500

    .

--- 05-React/02-state/Exercise1/etan/src/View/Components/Card.tsx
+++ 05-React/02-state/Exercise1/etan/src/View/Components/Card.tsx
@@ -1,14 +1,13 @@
+
 interface CardProps {
   imgSrc: any;
   text: any;
+
 }
 
 function Card(props: CardProps) {
- const {imgSrc, text }= props;
-  return <div className="card">
-      <img src={`${imgSrc}`}alt={text} />
-      <p>{text}</p>
-  </div>;
+ const {imgSrc, text}= props;
+  return ;
 }
 
 export default Card;
