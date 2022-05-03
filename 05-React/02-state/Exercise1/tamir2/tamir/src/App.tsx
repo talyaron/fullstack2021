45af0fe078ea8c85d1260e1e4667709e09b5f099
tamir-dadon <tamir.dadon63@gmail.com>
2022-05-03 13:04:37 +0300

    tamir

--- 05-React/02-state/Exercise1/tamir2/tamir/src/App.tsx
+++ 05-React/02-state/Exercise1/tamir2/tamir/src/App.tsx
@@ -2,15 +2,19 @@ import logo from './logo.svg';
 import { useState } from 'react';
 import Box from './View/Components/Box';
 import './View/styles/Global.scss'
+import { Boxprops } from './View/Components/Box' //import the interface
 
+const boxes: Array<Boxprops> = [
+  { title: 'wow', text: 'tamir', img: 'https://th.bing.com/th/id/OIP.hJXOGFnk2koif7ps727eXQHaHa?w=210&h=210&c=7&r=0&o=5&dpr=1.25&pid=1.7' },
+  { title: 'wow', text: 'aviel', img: "https://th.bing.com/th/id/OIP.hJXOGFnk2koif7ps727eXQHaHa?w=210&h=210&c=7&r=0&o=5&dpr=1.25&pid=1.7" },
+  { title: 'wow', text: 'shoval', img: 'https://th.bing.com/th/id/OIP.hJXOGFnk2koif7ps727eXQHaHa?w=210&h=210&c=7&r=0&o=5&dpr=1.25&pid=1.7' },
+  { title: 'wow', text: 'dganit', img: 'https://th.bing.com/th/id/OIP.hJXOGFnk2koif7ps727eXQHaHa?w=210&h=210&c=7&r=0&o=5&dpr=1.25&pid=1.7' }
+]
 
-function App() { 
+function App() {
   return (
     <div className="App">
-      <Box
-        title='new title'
-        text='new text'
-      />
+      {boxes.map((box, i) => <Box key={i} img={box.img} title={box.title} text={box.text} />)}
     </div>
   );
 
--- 05-React/02-state/Exercise1/tamir2/tamir/src/View/Components/Box.tsx
+++ 05-React/02-state/Exercise1/tamir2/tamir/src/View/Components/Box.tsx
@@ -1,11 +1,13 @@
-interface Boxprops {
+export interface Boxprops {
+    img:string;
     title: string;
     text: string;
 }
 const Box = (props: Boxprops) => {
-    const { title, text } = props;
+    const { title, text,img} = props;
     return (
         <div>
+            <img src={img}/>
             <h1>{title}</h1>
             <h3>{text}</h3>
         </div>
