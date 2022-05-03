d23bc03279e4cbeb4fcb8863e012a05da1232938
Etanhey <etan@heyman.net>
2022-05-03 16:44:08 -0500

    .

--- 05-React/02-state/Exercise1/etan/src/App.tsx
+++ 05-React/02-state/Exercise1/etan/src/App.tsx
@@ -1,4 +1,8 @@
 import React, { useState } from "react";
+import { Button, TextField, AppBar } from "@mui/material";
+import { ThemeProvider, createTheme } from "@mui/material/styles";
+import { red, green } from "@mui/material/colors";
+import DeleteIcon from "@mui/icons-material/Delete";
 
 import "./View/styles/global.scss";
 import Card from "./View/Components/Card";
@@ -12,6 +16,19 @@ interface CardsProps {
 function App() {
   const [cards, setCards] = useState<Array<CardsProps>>([]);
   const [text, setText] = useState<String>();
+  const [state, toggle] = useState(true);
+  // const {x}  = useSpring({from: {x:0}, x: state? 1: 0, config: {duration:1000}})
+  const theme = createTheme({
+    palette: {
+      primary: {
+        main: red[200],
+      },
+      secondary: {
+        main: green[500],
+      },
+    },
+  });
+
   function uniqueId() {
     return Date.now().toString(36) + Math.random().toString(36).substring(2);
   }
@@ -26,6 +43,7 @@ function App() {
   }
   function handleDelete(id: string) {
     let arr = cards.filter((card) => card.id !== id);
+    const deleted = document.querySelector(`[data-id=${id}]`)?.parentElement;
     setCards([...arr]);
   }
   function handleUpdate(ev: any) {
@@ -53,78 +71,92 @@ function App() {
   }
 
   return (
-    <div className="App">
-      <header className="App-header">
-        <form onSubmit={handleAddCard}>
-          <input
-            type="text"
-            name="img"
-            id="img"
-            placeholder="Input an image source"
-          />
-          <input
-            type="text"
-            name="text"
-            id="text"
-            placeholder="Write an img title"
-          />
-          <input type="submit" value="Submit a new card" />
-        </form>
-        <div className="grid">
-          {cards.map((card, i) => {
-            return (
-              <div key={i} className="card">
-                <img src={`${card.img}`} />
-                <p>{card.text}</p>
-                <p>{text}</p>
-                <button
-                  onClick={(id) => {
-                    handleDelete(card.id);
-                  }}
-                >
-                  Delete
-                </button>
-                <input
-                  type="text"
-                  placeholder={card.text}
-                  data-id={`${card.id}`}
-                  id="text"
-                />
-                <input
-                  type="submit"
-                  value="Update image title"
-                  onClick={(ev) => {
-                    handleUpdate(ev);
-                  }}
-                />
-                <input
-                  type="text"
-                  id="img"
-                  data-id={`${card.id}`}
-                  placeholder={card.img}
-                />
-                <input
-                  type="submit"
-                  value="Update image source"
-                  onClick={(ev) => {
-                    handleUpdate(ev);
-                  }}
-                />
-                <input
-                  onChange={(ev) => {
-                    setText(ev.target.value);
-                  }}
-                  value={`${text}`}
-                  type="text"
-                  name="liveChange"
-                  id="liveChange"
-                />
-              </div>
-            );
-          })}
-        </div>
-      </header>
-    </div>
+    <ThemeProvider theme={theme}>
+      <div className="App">
+        <AppBar position="static">Mui App</AppBar>
+        <header className="App-header">
+          <form onSubmit={handleAddCard}>
+            <input
+              type="text"
+              name="img"
+              id="img"
+              placeholder="Input an image source"
+            />
+            <input
+              type="text"
+              name="text"
+              id="text"
+              placeholder="Write an img title"
+            />
+            <input type="submit" value="Submit a new card" />
+          </form>
+          <div className="grid">
+            {cards.map((card, i) => {
+              return (
+                <div key={i} className="card">
+                  <img src={`${card.img}`} />
+                  <p>{card.text}</p>
+                  <p>{text}</p>
+                  <Button
+                    startIcon={<DeleteIcon />}
+                    size="small"
+                    style={{
+                      fontSize: "10px",
+                      width: 100,
+                    }}
+                    variant="outlined"
+                    color="error"
+                    onClick={(id) => {
+                      handleDelete(card.id);
+                    }}
+                  >
+                    Delete
+                  </Button>
+                  <input
+                    type="text"
+                    placeholder={card.text}
+                    data-id={`${card.id}`}
+                    id="text"
+                  />
+                  <Button
+                    value="Update image title"
+                    onClick={(ev) => {
+                      handleUpdate(ev);
+                    }}
+                  >
+                    Update text
+                  </Button>
+                  <input
+                    type="text"
+                    id="img"
+                    data-id={`${card.id}`}
+                    placeholder={card.img}
+                  />
+                  <Button
+                    type="submit"
+                    value="Update image source"
+                    onClick={(ev) => {
+                      handleUpdate(ev);
+                    }}
+                  >
+                    Update image
+                  </Button>
+                  <input
+                    onChange={(ev) => {
+                      setText(ev.target.value);
+                    }}
+                    value={`${text}`}
+                    type="text"
+                    name="liveChange"
+                    id="liveChange"
+                  />
+                </div>
+              );
+            })}
+          </div>
+        </header>
+      </div>
+    </ThemeProvider>
   );
 }
 
