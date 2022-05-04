feee6c81f6d72340ca1fba6cdcc8bfb7d3c43260
OmriAharonov <omri832@gmail.com>
2022-05-04 11:22:10 +0300

    update

--- 05-React/02-state/Exercise1/omri/src/App.tsx
+++ 05-React/02-state/Exercise1/omri/src/App.tsx
@@ -4,6 +4,7 @@ import "./view/styles/App.scss";
 interface images {
   imgSrc: string
   Name: string
+  id:string
 }
 function App() {
   // const [imgName, setImgName] = useState<string>("");
@@ -20,10 +21,19 @@ function App() {
     ev.preventDefault();
     const imgSrc = ev.target.imgSrc.value;
     const Name = ev.target.Name.value;
-    const obj = { imgSrc, Name };
+    const obj = { imgSrc, Name, id:uid() };
 
     setarr([...arr, obj]);
     console.log(arr)
+  }
+
+  function uid() {
+    return Date.now().toString(36) + Math.random().toString(36).substring(2);
+  }
+
+  function HandleUpdate(id:string){
+    const photo = arr.findIndex(index=>index.id===id);
+    arr[photo].Name
   }
 
 
@@ -37,10 +47,24 @@ function App() {
 
         </form>
       </div>
-      <div>
 
-
+      <div className='App__grid'>
+        {arr.map((arr, i) => {
+          return (
+            <div key={i} className='App__card'>
+              <img src={arr.imgSrc} />
+              <h3 className='App__text'>{arr.Name}</h3>
+              {/* <button onClick={() => handledelete(arr.id)}>delete</button> */}
+              {/* <button onClick={() => HandleUpdate(arr.id)}>update name</button> */}
+              <form onSubmit={() => HandleUpdate(arr.id)}>
+                <input type = 'text' name = {arr.Name} placeholder="Enter new Name"/>
+                <input type="submit" value="update" />
+              </form>
+            </div>
+          )
+        })}
       </div>
+    
       {/* <div className="photo">
         <img src={imgSrc}></img>
         <p>img name:{Name}</p>
