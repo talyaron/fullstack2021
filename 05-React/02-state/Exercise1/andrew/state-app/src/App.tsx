401cfaa7fe449c3fa71dae96e66d9664e0940b27
AndrewLishafay <andrew567234@gmail.com>
2022-05-04 21:36:53 +0300

    HW with 3d animation

--- 05-React/02-state/Exercise1/andrew/state-app/src/App.tsx
+++ 05-React/02-state/Exercise1/andrew/state-app/src/App.tsx
@@ -1,65 +1,64 @@
 import { useState } from 'react';
+import './view/style/global.scss'
 import React from 'react';
-import logo from './logo.svg';
+import Library from './view/components/Library';
+import Form from './view/components/Form'
 import './App.css';
 
-interface Img {
+export interface Img {
   text: string
   img: string
   id: string
 }
 
 function App() {
+
   const [arr, setArr] = useState<Array<Img>>([])
 
   function uid() {
     return Math.random().toString(36).slice(-6);
   }
 
-  function handleAddImage(ev: any) {
+  async function handleAddImage(ev: any) {
     ev.preventDefault();
     const text = ev.target.text.value;
     const img = ev.target.img.value;
     const obj = { text, img, id: uid() };
-    setArr([...arr, obj]);
+    await setArr([...arr, obj]);
+
+    const images = document.querySelectorAll('.image');
+    images[images.length - 1].classList.toggle('float-in');
+
+    setTimeout(() => {
+      images.forEach(image => {
+        image.classList.remove('float-in');
+      });
+    }, 700);
+
   }
 
   function handleDelete(id: string) {
-    setArr(arr.filter(img => img.id !== id));
+    const index = arr.findIndex(img => img.id === id);
+    const images = document.querySelectorAll('.image');
+    images[index].classList.toggle('float-out');
+    setTimeout(() => {
+      setArr(arr.filter(img => img.id !== id));
+    }, 700);
   }
 
   function handleUpdate(id: string) {
-    const text:any = prompt('Enter a new name for the picture');
-    // const text: any = "test";
+    const text: any = prompt('Enter a new name for the picture');
     const index = arr.findIndex(img => img.id === id);
-    // console.log(arr);
     arr[index].text = text
     setArr([...arr]);
-    // console.log();
   }
+
 
   return (
     <div className="App">
       <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <form onSubmit={handleAddImage}>
-          <label htmlFor="text">Input a name for the image</label>
-          <input type="text" name='text' id='text' />
-          <label htmlFor="img">Input the image URL</label>
-          <input type="text" name='img' id='img' />
-          <input type="submit" />
-        </form>
-        <div className='grid'>
-          {arr.map((img, i) => {
-            return (
-              <div key={i} className='image'>
-                <h3>{img.text}</h3>
-                <img src={img.img} alt={img.text} />
-                <button onClick={() => handleDelete(img.id)}>Delete</button>
-                <button onClick={() => handleUpdate(img.id)}>update</button>
-              </div>)
-          })}
-        </div>
+        <Form addImageFunction={handleAddImage} />
+        <Library pictures={arr} deleteFunction={handleDelete} updateFunction={handleUpdate} />
       </header >
     </div >
   );
