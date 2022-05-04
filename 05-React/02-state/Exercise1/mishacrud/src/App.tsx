6a99eaf0cd1cf95bdecd05dfdd28ad260382f5e3
Michael Dubovyk <93338459+mishalon@users.noreply.github.com>
2022-05-04 17:16:09 +0400

    87

--- 05-React/02-state/Exercise1/mishacrud/src/App.tsx
+++ 05-React/02-state/Exercise1/mishacrud/src/App.tsx
@@ -1,36 +1,106 @@
-import React, {useState} from 'react';
-import './App.css';
+import React, { useState } from 'react';
+import './App.scss';
+import TextField from '@mui/material/TextField';
 
-interface PicObject{
-  name:String,
-  pic:String
+interface PicObject {
+  name: string;
+  pic: string;
+  id: string;
 }
 
 function App() {
 
-  const [arr, setArr]= useState<String>('')
+  const classs = 'field';
 
-  function handleSubmit(ev:any){
+  const uid = function () {
+    return Date.now().toString(36) + Math.random().toString(36).substr(2);
+  };
+
+  const [arr, setArr] = useState<Array<PicObject>>([]);
+
+
+  function handleSubmit(ev: any) {
     ev.preventDefault();
-    console.log(ev.target.elements.name.value)
-    console.log(ev.target.elements.pic.value)
+
     const name = ev.target.elements.name.value;
     const pic = ev.target.elements.pic.value;
-    const obj:PicObject = {name, pic}
+    const id = uid();
+    setArr([...arr, { name, pic, id }]);
 
-    setArr([...arr, obj])
+    ev.target.reset()
   }
+
+  function handleDelete(ev: any) {
+    const cardId = ev.target.id;
+    setArr(arr.filter(card => { return card.id !== cardId }))
+
+  }
+
+  function handleUpdate(ev: any) {
+
+    ev.preventDefault()
+
+    const index = arr.findIndex(obj => obj.id === ev.target.id)
+
+    const name = ev.target.elements.name.value
+    const pic = ev.target.elements.pic.value
+
+    arr[index].name = name;
+    arr[index].pic = pic;
+
+    setArr([...arr])
+
+    ev.target.reset()
+  }
+
 
   return (
     <div className="App">
 
-      <form onSubmit={handleSubmit}>
-       <label>NAME</label>
-       <input type="text" name='name' placeholder='Enter Name' />
-       <label>PIC</label>
-       <input type="text" name='pic' placeholder='PicURL...' />
-       <button type='submit'>SUBMIT</button>
+      <form noValidate autoComplete='off' onSubmit={handleSubmit}>
+        <TextField sx={{
+          marginTop: 2,
+          marginBottom: 2,
+          display: 'block'
+        }}
+          required
+          variant='outlined'
+          id="outlined-required"
+          label="Picture Name"
+          fullWidth />
+        <TextField
+          sx={{
+            marginTop: 2,
+            marginBottom: 2,
+            display: 'block'
+          }}
+          required
+          id="outlined-required"
+          label="Picture URL"
+          fullWidth />
+        <button type='submit'>SUBMIT</button>
       </form>
+
+      {arr.map((card, i) => (
+        <div>
+          <div>
+            {card.name}
+          </div>
+          <div>
+            <img src={card.pic} alt='pic' />
+          </div>
+          <div>
+            <button onClick={handleDelete} id={card.id}>Delete</button>
+            <form onSubmit={handleUpdate} id={card.id}>
+              <label>NAME</label>
+              <input type="text" name='name' placeholder='Enter Name' />
+              <label>PIC</label>
+              <input type="text" name='pic' placeholder='PicURL...' />
+              <button type='submit'>Update</button>
+            </form>
+          </div>
+        </div>
+      ))}
 
     </div>
   );
