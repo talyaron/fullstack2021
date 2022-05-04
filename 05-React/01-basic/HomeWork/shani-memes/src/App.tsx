7d8dad993e43791c30a5b69ecbcd0e4e2f3f03d3
ShaniRom <93940556+ShaniRom@users.noreply.github.com>
2022-05-04 11:38:53 +0300

    gag hw try

--- 05-React/01-basic/HomeWork/shani-memes/src/App.tsx
+++ 05-React/01-basic/HomeWork/shani-memes/src/App.tsx
@@ -1,12 +1,12 @@
-import React from 'react';
+import React from "react";
 
+import "./View/styles/global.scss";
 
+import Header from "./View/Pages/Header";
+import Footer from "./View/Pages/Footer";
+import Container from "./View/Components/Container";
 
-import './View/styles/global.scss'
-
-import Header from './View/Pages/Header';
-import Footer from './View/Pages/Footer';
-import Container from './View/Components/Container';
+import { useState } from "react";
 
 
 
@@ -14,13 +14,13 @@ function App() {
   return (
     <div className="App">
       <header className="App-header">
+        <Header pageName="Fullstack Development Memes" logoUrl="" />
+        
+        
+     
+        <Container />
 
-        <Header pageName='Fullstack Development Memes' logoUrl=""/>
-
-
-        <Container/>
-
-        <Footer footerText="<Footer footerText='No more memes to see here...get back to coding '/>"/>
+        <Footer footerText="<Footer footerText='No more memes to see here...get back to coding '/>" />
       </header>
     </div>
   );
