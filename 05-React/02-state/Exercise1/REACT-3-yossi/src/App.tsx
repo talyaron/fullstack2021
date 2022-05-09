64771df5fcc530e8fd53e1d62d94da0a5dab8904
Yossi A <asnafy.junior@gmail.com>
2022-05-09 17:34:25 +0300

    REACT-3-yossi

---
+++ 05-React/02-state/Exercise1/REACT-3-yossi/src/App.tsx
@@ -0,0 +1,15 @@
+
+import './view/styles/global.scss';
+import Images from "./Images";
+
+
+function App() {
+
+  return (
+    <div className="App">
+      <Images />
+    </div>
+  );
+}
+
+export default App;
