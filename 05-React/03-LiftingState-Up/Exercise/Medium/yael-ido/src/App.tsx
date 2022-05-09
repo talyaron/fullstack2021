9bfc1ba6a0f7a6091b79603364a2af5034b81c8c
Ido Gino <davegino220@gmail.com>
2022-05-09 20:32:09 +0300

    lala

---
+++ 05-React/03-LiftingState-Up/Exercise/Medium/yael-ido/src/App.tsx
@@ -0,0 +1,17 @@
+import React from 'react';
+import { useState } from 'react';
+import logo from './logo.svg';
+import './App.scss';
+import Form from './Components/Form'
+
+
+function App() {
+  const [flowers , setFlowers] = useState('')
+  return (
+    <div className="App">
+      
+    </div>
+  );
+}
+
+export default App;
---
+++ 05-React/03-LiftingState-Up/Exercise/Medium/yael-ido/src/Components/Form.tsx
@@ -0,0 +1,31 @@
+import React from 'react'
+import PropTypes from 'prop-types'
+interface FlowerProps{
+    setImgUrl:Function
+    setName:Function
+
+}
+
+const Form = (props:FlowerProps) => {
+    function handleFlowers(ev:any){
+        const {setImgUrl , setName} = props;
+        const imgUrl = ev.target.elements.img.value
+        const name = ev.target.elements.name.value
+
+        setImgUrl(imgUrl)
+        setName(name)
+    }
+  return (
+    <form onSubmit={handleFlowers}>
+
+        <input type="text" name="img" id="img" placeholder='enter img URL'/>
+        <input type="text" name="name" id="name" placeholder='enter flower Name'/>
+        <input type="submit" value="add" />
+
+    </form>
+  )
+}
+
+
+
+export default Form
