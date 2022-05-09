c08ea2abba999cbbf658633070515712fe5bf50c
Tal Yaron <tal.yaron@gmail.com>
2022-05-09 18:29:17 +0300

    lifiting state up

---
+++ 05-React/03-LiftingState-Up/Example/src/App.tsx
@@ -0,0 +1,28 @@
+import { useState } from "react";
+import "./App.css";
+
+import Form from "./Form";
+
+function App() {
+  const [image, setImage] = useState("");
+  const [title, setTitle] = useState("");
+
+  //functions with state
+  // function updateImage(title: string, image: string) {
+  //   setImage(image);
+  //   setTitle(title);
+  // }
+
+  return (
+    <div className="App">
+     
+      <header className="App-header">
+      {image?<img src={image} alt='some user input' />:null}
+      {title?<p>{title}</p>:null}
+        <Form  setTitle={setTitle} setImage={setImage} />
+      </header>
+    </div>
+  );
+}
+
+export default App;
