a0073e688df4bc06ee38f9e05f237d8ad108f1c9
tamir-dadon <tamir.dadon63@gmail.com>
2022-05-11 14:37:00 +0300

    tamir

--- 05-React/02-state/Exercise1/tamir2/tamir/src/App.tsx
+++ 05-React/02-state/Exercise1/tamir2/tamir/src/App.tsx
@@ -1,8 +1,11 @@
 import { useState } from 'react';  // state לרשום את השורה הזאת כל פעם שמתמשים  
-import './View/styles/Global.scss' //"scss" מושך את העיצוב מהדף הגלובלי של ה  
+import './View/styles/Global.scss' //"scss" מושך את העיצוב מהדף הגלובלי של ה 
+import Footer from './View/Components/Footer';
+import Heder from './View/Components/Heder';
+
 
 // איך האובייקט יראה לפי המשתנים שיש בתוכו
-interface newone {
+ export interface newone {
   text: string;
   img: string;
   id: string;
@@ -11,8 +14,9 @@ interface newone {
 // הפונקצייה הכללית שמרונדרת על המסך
 function App() {
 
-  
   const [arr, setarr] = useState<Array<newone>>([])
+  const [text, settext] = useState('')
+  const [img, setimg] = useState('')
 
   // פונקצייה שנותנת תעודת זהות מיוחדת כל אחד מהאובייקטים
   function uid() {
@@ -36,41 +40,61 @@ function App() {
     setarr(arr.filter(img => img.id !== id))
   }
 
-  function handleupdate(id: string) {
-    const text:any = prompt('set new name');
+ function handleupdate(id: string) {
     const index = arr.findIndex(one => one.id === id)
     arr[index].text = text
+    arr[index].img = img
     setarr([...arr])
   }
+
+  function handleclick(id:string){
+console.log(id);
+
+  }
+
 
   return (
 
     <div className="App">
+
+
+      <Heder />
+
       <form onSubmit={handleaddone} className='App__form'>
-        <input type="text" name='text' id='text' placeholder='tour text here' />
-        <input type="text" name='img' placeholder='your img url here' />
+        <input type="text" name='text' id='text' placeholder='your text here' className='App__text' />
+        <input type="text" name='img' placeholder='your img url here' className='App__img' />
         <input type="submit" value="submit" />
       </form>
 
-      <div className='App__grid'>
-        
+      <div className='App'>
         {arr.map((one, i) => {
           return (
-            <div key={i} className='App__card'>
-              <img src={one.img} />
-              <h3 className='App__text'>{one.text}</h3>
-              <button onClick={() => handledelete(one.id)}>delete</button>
-              <button onClick={() => handleupdate(one.id)}>update name</button>
+            <div className='App'>
+              <div className='App__card' onClick={()=>handleclick(one.id)}>
+                <div key={i} className='App__item'>
+                  <img src={one.img} />
+                  <h3 className='App__text'>
+                    {one.text}
+                  </h3>
+                </div>
+                <div className='App__opt'>
+                  <button onClick={() => handledelete(one.id)}>delete</button>
+                  <button onClick={() => handleupdate(one.id)}>update name</button>
+                </div>
+              </div>
+            
             </div>
           )
         })}
       </div>
 
+      <Footer settext={settext} setimg={setimg} />
+
     </div>
 
-
-
   )
+
+
 }
 export default App;
 
---
+++ 05-React/02-state/Exercise1/tamir2/tamir/src/View/Components/Heder.tsx
@@ -0,0 +1,12 @@
+
+const Heder = () => {
+
+
+  return (
+    <div className='Heder'>
+      <h1>Real Estate</h1>
+    </div>
+  )
+}
+
+export default Heder;
