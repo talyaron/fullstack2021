73d746998e0f996354c74013a5e74801e0077b4a
Tal Yaron <tal.yaron@gmail.com>
2022-05-12 18:36:10 +0300

    LEON

--- 05-React/03-LiftingState-Up/Exercise/leon/leony-app/src/view/components/Box.tsx
+++ 05-React/03-LiftingState-Up/Exercise/leon/leony-app/src/view/components/Box.tsx
@@ -1,16 +1,22 @@
+//components
+import EditBox from './EditBox';
+
 interface Boxprops {
     text: string;
     title: string;
     img: any;
+    id:string;
+    updateTitle:Function
 }
 
 const Box = (props: Boxprops) => {
-    const { text, title, img } = props;
+    const { id, text, title, img, updateTitle} = props;
     return (
         <div className="box">
             <h1>{title}</h1>
             {img}
             <p>{text}</p>
+            <EditBox id={id} updateTitle={updateTitle} />
         </div>
     );
 }
