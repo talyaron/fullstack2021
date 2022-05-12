012dc3902da616b7724921420c5ef4f5cc9dc9c4
Yossi A <asnafy.junior@gmail.com>
2022-05-12 10:59:20 +0300

    LiftingState-Up-Yossi

---
+++ 05-React/03-LiftingState-Up/Exercise/REACT-3-LiftingState-Up-Yossi/src/Footer.tsx
@@ -0,0 +1,18 @@
+
+
+function Footer({handleUpdateFooter, handleDelete, id, title, url}:any) {
+
+
+    return (
+        <div>
+            <form onSubmit={(ev) => handleUpdateFooter(ev, id)}>
+                <input type="text" name="newTitle" placeholder="enter new title" defaultValue={title}/>
+                <input type="text" name="newUrl" placeholder="enter new url" defaultValue={url}/>
+                <input type="submit" className="submit" value={'APDATE IMAGE'}/>
+            </form>
+            <div className="delete"><button onClick={() => handleDelete(id)}>DELETE</button></div>
+        </div>
+    );
+}
+
+export default Footer;
