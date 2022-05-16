81ed46ee376d9203b869caec8a207d59eb20f0f0
ShaniRom <93940556+ShaniRom@users.noreply.github.com>
2022-05-16 21:19:02 +0300

    switch

--- 05-React/05-conditional-rendering/exercise-dark-mode/shani/src/App.tsx
+++ 05-React/05-conditional-rendering/exercise-dark-mode/shani/src/App.tsx
@@ -1,7 +1,7 @@
 import React,{useState} from 'react';
 
 import './App.css';
-
+import {Switch} from "@mui/material";
 function App() {
   const [mode,setMode]=useState(true)
   return (
@@ -9,6 +9,11 @@ function App() {
     <header className='App-header'>
     {mode?<p>DARK MODE</p>:<p>LIGHT MODE</p>}
     <button onClick={()=>setMode(!mode)}>SWITCH</button>
+    <Switch color='primary' onChange={()=>setMode(!mode)}/>
+    <Switch color='secondary' onChange={()=>setMode(!mode)}/>
+    <Switch color='error' onChange={()=>setMode(!mode)}/>
+    <input type="checkbox"
+                    onChange={()=>setMode(!mode)} />
     </header>
     </div>
   );
