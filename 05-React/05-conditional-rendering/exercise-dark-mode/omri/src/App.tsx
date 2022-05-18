4ae4bccd8b9f0d859bed76d824779522930ae513
OmriAharonov <omri832@gmail.com>
2022-05-18 23:17:17 +0300

    s

--- 05-React/05-conditional-rendering/exercise-dark-mode/omri/src/App.tsx
+++ 05-React/05-conditional-rendering/exercise-dark-mode/omri/src/App.tsx
@@ -1,13 +1,17 @@
 import './view/styles/global.scss';
 import {useState} from 'react';
 import axios from 'axios';
+import Header from './view/components/Header';
 //https://opentdb.com/api.php?amount=10 - trivia API
+//https://www.youtube.com/watch?v=AfDY1ue6YRI
 function App() {
 
   
   return (
     <div className="App">
-      
+      <div className="App__header">
+        <Header />
+      </div>
     </div>
   );
 }
