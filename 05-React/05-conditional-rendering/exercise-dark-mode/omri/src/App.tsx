9aeea89205736e6b23a9e2ad089d47d8c001bf50
OmriAharonov <omri832@gmail.com>
2022-05-18 22:37:33 +0300

    API

--- 05-React/05-conditional-rendering/exercise-dark-mode/omri/src/App.tsx
+++ 05-React/05-conditional-rendering/exercise-dark-mode/omri/src/App.tsx
@@ -1,13 +1,13 @@
 import './view/styles/global.scss';
 import {useState} from 'react';
 import axios from 'axios';
-
+//https://opentdb.com/api.php?amount=10 - trivia API
 function App() {
 
   
   return (
     <div className="App">
-  
+      
     </div>
   );
 }
