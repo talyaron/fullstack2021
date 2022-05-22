80f51323a4c1f1e16468c8e2fca41f4f08a5f331
tamir-dadon <tamir.dadon63@gmail.com>
2022-05-22 22:56:10 +0300

    tamir

--- 05-React/05-conditional-rendering/project/tamir/tamir/src/App.tsx
+++ 05-React/05-conditional-rendering/project/tamir/tamir/src/App.tsx
@@ -1,22 +1,53 @@
-
 import { useState } from "react";
 import Box from "./View/Components/Box";
+import Lobi from "./View/Components/Lobi";
 import "./View/styles/Global.scss";
 
-function App() {
+interface manoi {
+  name: string;
+  fhone: number;
+  id: string;
+}
 
-  // useEffect(() => {}, []);
+function App() {
+  const [arr, setarr] = useState<Array<manoi>>([]);
+  function uid() {
+    return Date.now().toString(36) + Math.random().toString(36).substring(2);
+  }
+
+  function handleaddone(ev:any) {
+    ev.preventDefault();
+    const name = ev.target.name.value;
+    const fhone = ev.target.fhone.value;
+    const obj = { name, fhone, id: uid() };
+    setarr([...arr, obj])
+    console.log(arr,name,fhone);
+    console.log('hey');
+    
+  }
+
   return (
     <div className="App">
       <div className="App__header">
-        <button className="App__log">
-          היכנס
-        </button>
+        <button className="App__log">היכנס</button>
         <div className="App__img">
           <img src="https://www.freepnglogos.com/uploads/netflix-logo-0.png" />
         </div>
+
+        <div className="App__card">
+          {arr.map((one, i) => {
+            return (
+              <div className="App__manoi" key={i}>
+                <p>{one.name}</p>
+                <p>{one.fhone}</p>
+              </div>
+            );
+          })}
+        </div>
       </div>
-         <Box />
+      <Box handleaddone={handleaddone} id={uid()} />
+
+      {/* <Lobi /> */}
     </div>
   );
 }
--- 05-React/05-conditional-rendering/project/tamir/tamir/src/View/Components/Lobi.tsx
+++ 05-React/05-conditional-rendering/project/tamir/tamir/src/View/Components/Lobi.tsx
@@ -1,9 +1,36 @@
-const Lobi = () => {
-  return (
-    <div>
+import { useState } from "react";
 
-    </div>
-  )
+interface Lobi {
+  name: string;
+  year: number;
+  price: number;
+  id: string;
 }
 
-export default Lobi
+const Lobi = () => {
+
+  function handlesubmit(ev:any) {
+    ev.preventDefault();
+    const name = ev.target.elements.name.value;
+    const year =  ev.target.elements.year.value;
+    const price = ev.target.elements.price.value;
+  }
+  function uid() {
+    return Date.now().toString(36) + Math.random().toString(36).substring(2);
+  }
+
+
+  const Movies = [{ name: "", year: 1997, price: 10, id: uid() }];
+  return (
+    <div>
+      <form onSubmit={(id) => handlesubmit(id)}>
+        <input type="text" name="name" placeholder="name of movie" />
+        <input type="month" name="year" />
+        <input type="number" name="price" placeholder="price" />
+        <input type="submit" value="submit" />
+      </form>
+    </div>
+  );
+};
+
+export default Lobi;
