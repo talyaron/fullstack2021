128f2d669ef32b56c7c34feae7c138a24a73cb40
tamir-dadon <tamir.dadon63@gmail.com>
2022-05-25 15:33:07 +0300

    tamir

--- 05-React/05-conditional-rendering/project/tamir/tamir/src/App.tsx
+++ 05-React/05-conditional-rendering/project/tamir/tamir/src/App.tsx
@@ -1,45 +1,56 @@
-import { useState } from "react";
+import { useState,useEffect } from "react";
+import axios from 'axios'
 import Box from "./View/Components/Box";
 import "./View/styles/Global.scss";
 
 interface App {
-  img: string;
   name: String;
   id: String;
 }
 
 function App() {
   const [arr, setarr] = useState<Array<App>>([]);
-  const [img, setimg] = useState("");
   const [name, setname] = useState("");
+
   function uid() {
     return Date.now().toString(36) + Math.random().toString(36).substring(2);
   }
 
-  function handladd(ev: any) {
-    const obj = { img, name, id: uid() };
+  function handladd() {
+    const obj = { name, id: uid() };
     setarr([...arr, obj]);
     console.log(arr);
+    
   }
+
+  function handledelete(id: any) {
+    setarr(arr.filter((one) => one.id !== id));
+  }
+
   return (
     <div className="App">
       <div className="App__header">
-        <button className="App__log">היכנס</button>
+        <button className="App__log">היכנס</button>    
         <div className="App__img">
           <img src="https://www.freepnglogos.com/uploads/netflix-logo-0.png" />
         </div>
       </div>
-      <Box handleaddone={handladd} setname={setname} setimg={setimg} />
+      <Box handleaddone={handladd} setname={setname} />
       <div className="user">
         {arr.map((one, i) => {
           return (
             <div key={i} className="user__card">
               <div className="user__img">
-                <img src={one.img} alt="" />
+                {" "}
+                <img src="https://www.freepnglogos.com/uploads/netflix-logo-circle-png-5.png" />{" "}
               </div>
-              <div className="user__name">
-                <h3>{one.name}</h3>
-              </div>
+              <h3 className="user__h3">{one.name}</h3>
+              <button
+                className="user__delete"
+                onClick={() => handledelete(one.id)}
+              >
+                Delete
+              </button>
             </div>
           );
         })}
--- 05-React/05-conditional-rendering/project/tamir/tamir/src/View/Components/Box.tsx
+++ 05-React/05-conditional-rendering/project/tamir/tamir/src/View/Components/Box.tsx
@@ -1,30 +1,25 @@
 interface boxprops {
   handleaddone: Function;
   setname: Function;
-
-  setimg: Function;
 }
 
 const Box = (props: boxprops) => {
-  const { setname, setimg, handleaddone } = props;
+  const { setname, handleaddone } = props;
 
   function handlesubmit(ev: any) {
     ev.preventDefault();
-    const img = ev.target.elements.img.value;
     const name = ev.target.elements.name.value;
-
+    
     setname(name);
-    setimg(img);
-    handleaddone(name, img);
+    handleaddone(name);
   }
 
   return (
     <div className="Box">
       <h1 className="Box__h1"> .סרטים, תוכניות טלוויזיה ועוד בלי הגבלה</h1>
-      <h3 className="Box__h3">לצפות בכל מקום. לבטל בכל שלב.</h3>
-
+      <h3 className="Box__h3">.לצפות בכל מקום. לבטל בכל שלב</h3>
+      <h3 className="Box__h3">.מלא את שמך להוספת מנוי</h3>
       <form onSubmit={handlesubmit}>
-        <input type="text" name="img" placeholder="your img here" />
         <input type="text" name="name" placeholder="your name here" />
         <input type="submit" value="join" />
       </form>
