4d6795e7058aa1eae81f0a974da804c138c36654
tamir-dadon <tamir.dadon63@gmail.com>
2022-05-25 22:35:42 +0300

    tamir

--- 05-React/05-conditional-rendering/project/tamir/tamir/src/View/Components/User.tsx
+++ 05-React/05-conditional-rendering/project/tamir/tamir/src/View/Components/User.tsx
@@ -2,10 +2,10 @@ const User = () => {
   return (
     <div className="movies">
 <img src="https://th.bing.com/th/id/OIP.i8mv_V-e7MmvoppX-pbKbwHaDt?pid=ImgDet&rs=1" alt="" />
-<img src="https://th.bing.com/th/id/OIP.i8mv_V-e7MmvoppX-pbKbwHaDt?pid=ImgDet&rs=1" alt="" />
-<img src="https://th.bing.com/th/id/OIP.i8mv_V-e7MmvoppX-pbKbwHaDt?pid=ImgDet&rs=1" alt="" />
-<img src="https://th.bing.com/th/id/OIP.i8mv_V-e7MmvoppX-pbKbwHaDt?pid=ImgDet&rs=1" alt="" />
-<img src="https://th.bing.com/th/id/OIP.i8mv_V-e7MmvoppX-pbKbwHaDt?pid=ImgDet&rs=1" alt="" />
+<img src="https://th.bing.com/th/id/OIP.Hu7ck2WmfofqeSqLXDLnGwHaLF?pid=ImgDet&rs=1" alt="" />
+<img src="https://i.ytimg.com/vi/22rpOr8d2QQ/hqdefault.jpg" alt="" />
+<img src="https://th.bing.com/th/id/OIP.a1lt8l8_jKmuh4NfxBC7kQHaFj?pid=ImgDet&rs=1" alt="" />
+<img src="https://th.bing.com/th/id/R.4c26d4287938586501ba8f17ba64194a?rik=mNn8Z2tQeOpk6g&pid=ImgRaw&r=0" alt="" />
 <img src="https://th.bing.com/th/id/OIP.i8mv_V-e7MmvoppX-pbKbwHaDt?pid=ImgDet&rs=1" alt="" />
 <img src="https://th.bing.com/th/id/OIP.i8mv_V-e7MmvoppX-pbKbwHaDt?pid=ImgDet&rs=1" alt="" />
 <img src="https://th.bing.com/th/id/OIP.i8mv_V-e7MmvoppX-pbKbwHaDt?pid=ImgDet&rs=1" alt="" />
