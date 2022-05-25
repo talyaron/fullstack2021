84b314696066b9bb111ea2a1dc86c14541242306
tamir-dadon <tamir.dadon63@gmail.com>
2022-05-25 22:15:43 +0300

    tamir

--- 05-React/05-conditional-rendering/project/tamir/tamir/src/App.tsx
+++ 05-React/05-conditional-rendering/project/tamir/tamir/src/App.tsx
@@ -1,7 +1,9 @@
-import { useState,useEffect } from "react";
-import axios from 'axios'
+import { useState, useEffect } from "react";
+import axios from "axios";
 import Box from "./View/Components/Box";
 import "./View/styles/Global.scss";
+import User from "./View/Components/User";
+import { on } from "events";
 
 interface App {
   name: String;
@@ -11,6 +13,7 @@ interface App {
 function App() {
   const [arr, setarr] = useState<Array<App>>([]);
   const [name, setname] = useState("");
+  const [login, setlogin] = useState(true);
 
   function uid() {
     return Date.now().toString(36) + Math.random().toString(36).substring(2);
@@ -20,46 +23,67 @@ function App() {
     const obj = { name, id: uid() };
     setarr([...arr, obj]);
     console.log(arr);
-    
   }
   // useEffect(()=>{
   //   handladd()
-   
+
   // },[])
 
   function handledelete(id: any) {
     setarr(arr.filter((one) => one.id !== id));
   }
+  if (login) {
+    return (
+      <div className="App">
+        <div className="App__header">
+          <button className="App__log">
+            היכנס
+          </button>
+          <div className="App__img">
+            <img src="https://www.freepnglogos.com/uploads/netflix-logo-0.png" />
+          </div>
+        </div>
 
-  return (
-    <div className="App">
-      <div className="App__header">
-        <button className="App__log">היכנס</button>    
-        <div className="App__img">
-          <img src="https://www.freepnglogos.com/uploads/netflix-logo-0.png" />
+        <Box handleaddone={handladd} setname={setname} />
+        <div className="user">
+          {arr.map((one, i) => {
+            return (
+              <div
+                key={i}
+                className="user__card"
+                onClick={() => setlogin(!login)}
+              >
+                <div className="user__img">
+                  <img src="https://www.freepnglogos.com/uploads/netflix-logo-circle-png-5.png" />
+                </div>
+                <h3 className="user__h3">{one.name}</h3>
+                <button
+                  className="user__delete"
+                  onClick={() => handledelete(one.id)}
+                >
+                  Delete
+                </button>
+              </div>
+            );
+          })}
         </div>
       </div>
-      <Box handleaddone={handladd} setname={setname} />
-      <div className="user">
-        {arr.map((one, i) => {
-          return (
-            <div key={i} className="user__card">
-              <div className="user__img">
-                <img src="https://www.freepnglogos.com/uploads/netflix-logo-circle-png-5.png" />{" "}
-              </div>
-              <h3 className="user__h3">{one.name}</h3>
-              <button
-                className="user__delete"
-                onClick={() => handledelete(one.id)}
-              >
-                Delete
-              </button>
-            </div>
-          );
-        })}
+    );
+  } else {
+    return (
+      <div className="App2">
+        <div className="App__header">
+          <button className="App__log" onClick={() => setlogin(!login)}>
+            יציאה
+          </button>
+          <div className="App__img">
+            <img src="https://www.freepnglogos.com/uploads/netflix-logo-0.png" />
+          </div>
+        </div>
+        <User />
       </div>
-    </div>
-  );
+    );
+  }
 }
 
 export default App;
---
+++ 05-React/05-conditional-rendering/project/tamir/tamir/src/View/Components/User.tsx
@@ -0,0 +1,17 @@
+const User = () => {
+  return (
+    <div className="movies">
+<img src="https://th.bing.com/th/id/OIP.i8mv_V-e7MmvoppX-pbKbwHaDt?pid=ImgDet&rs=1" alt="" />
+<img src="https://th.bing.com/th/id/OIP.i8mv_V-e7MmvoppX-pbKbwHaDt?pid=ImgDet&rs=1" alt="" />
+<img src="https://th.bing.com/th/id/OIP.i8mv_V-e7MmvoppX-pbKbwHaDt?pid=ImgDet&rs=1" alt="" />
+<img src="https://th.bing.com/th/id/OIP.i8mv_V-e7MmvoppX-pbKbwHaDt?pid=ImgDet&rs=1" alt="" />
+<img src="https://th.bing.com/th/id/OIP.i8mv_V-e7MmvoppX-pbKbwHaDt?pid=ImgDet&rs=1" alt="" />
+<img src="https://th.bing.com/th/id/OIP.i8mv_V-e7MmvoppX-pbKbwHaDt?pid=ImgDet&rs=1" alt="" />
+<img src="https://th.bing.com/th/id/OIP.i8mv_V-e7MmvoppX-pbKbwHaDt?pid=ImgDet&rs=1" alt="" />
+<img src="https://th.bing.com/th/id/OIP.i8mv_V-e7MmvoppX-pbKbwHaDt?pid=ImgDet&rs=1" alt="" />
+<img src="https://th.bing.com/th/id/OIP.i8mv_V-e7MmvoppX-pbKbwHaDt?pid=ImgDet&rs=1" alt="" />
+    </div>
+  )
+}
+
+export default User
