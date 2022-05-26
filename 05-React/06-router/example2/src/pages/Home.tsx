2cb1e3d208c83f85de9c3493e68bcd391b07c44b
Tal Yaron <tal.yaron@gmail.com>
2022-05-26 20:27:32 +0300

    HomeTask

--- 05-React/06-router/example2/src/pages/Home.tsx
+++ 05-React/06-router/example2/src/pages/Home.tsx
@@ -4,17 +4,18 @@ import { Link, useNavigate } from "react-router-dom";
 const Home = () => {
   const navigate = useNavigate();
 
-  useEffect(() => {
-    setTimeout(() => {
-      navigate("/about");
-    }, 3000);
-    // eslint-disable-next-line react-hooks/exhaustive-deps
-  }, []);
+//   useEffect(() => {
+//     setTimeout(() => {
+//       navigate("/about");
+//     }, 3000);
+//     // eslint-disable-next-line react-hooks/exhaustive-deps
+//   }, []);
 
   return (
-    <div className="green">
+    <div className="subpage green">
       <h2>Home</h2>
       <Link to="/about">About</Link>
+      <Link to='/'>Login</Link>
     </div>
   );
 };
