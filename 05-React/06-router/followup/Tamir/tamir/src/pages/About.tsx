a4327684faacd7a947487a7f3bceb318489c56c5
tamir-dadon <tamir.dadon63@gmail.com>
2022-05-26 19:24:30 +0300

    tamir

---
+++ 05-React/06-router/followup/Tamir/tamir/src/pages/About.tsx
@@ -0,0 +1,12 @@
+import { Link } from "react-router-dom";
+
+const About = () => {
+  return (
+    <div>
+      <button>About</button>
+      <Link to="/">home</Link>
+    </div>
+  );
+};
+
+export default About;
