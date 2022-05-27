fa8141aafba68b732de650eb5a7e8eda71b2eea3
Michael Dubovyk <93338459+mishalon@users.noreply.github.com>
2022-05-27 20:34:40 +0300

    87

--- 05-React/06-router/followup/misha/follow/src/view/pages/Home.tsx
+++ 05-React/06-router/followup/misha/follow/src/view/pages/Home.tsx
@@ -1,6 +1,16 @@
 import React from 'react'
+import { useEffect } from "react";
+import { Link, useNavigate } from "react-router-dom";
 
 const Home = () => {
+  const navigate = useNavigate();
+  
+  useEffect(() => {
+    setTimeout(() => {
+      navigate("/about");
+    }, 3000);
+    // eslint-disable-next-line react-hooks/exhaustive-deps
+  }, []);
   return (
     <div>Home</div>
   )
