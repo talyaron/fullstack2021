fe49a7077dfce06d33cb05e40e29defb44427d1b
OmriAharonov <omri832@gmail.com>
2022-05-30 00:44:02 +0300

    trivia

--- 05-React/05-conditional-rendering/exercise-dark-mode/omri/src/view/components/Card.tsx
+++ 05-React/05-conditional-rendering/exercise-dark-mode/omri/src/view/components/Card.tsx
@@ -2,7 +2,8 @@
 
 interface CardProps {
   questions: Array<questions>,
-  answers:Array<Answer>
+  answers: Array<Answer>,
+  setScore:Function
 }
 interface Answer {
   answer: string,
@@ -11,22 +12,46 @@ interface Answer {
 
 interface questions {
   question: string
-  answers:Array<Answer>
+  answers: Array<Answer>
 }
+
+function shuffle(array: Array<Answer>) {
+  let currentIndex = array.length, temporaryValue, randomIndex;
+  while (0 !== currentIndex) {
+    randomIndex = Math.floor(Math.random() * currentIndex);
+    currentIndex -= 1;
+    temporaryValue = array[currentIndex];
+    array[currentIndex] = array[randomIndex];
+    array[randomIndex] = temporaryValue;
+  }
+  return array;
+}
+
+function handleCorrectAnswer(answer:any) {
+
+}
+
 
 
 const Card = (props: CardProps) => {
   const { questions } = props;
   return (
     <>
-     {questions.map((question:any,i) => {
-       return(
-         <div   key ={i}>
-           <h5>{question.question}</h5>
-         </div>
-         
-       )
-     })}
+      {questions.map((question: any, i) => {
+        shuffle(question.answers);
+        return (
+          <div key={i}>
+            <h5>{question.question}</h5>
+            {question.answers.map((answer: any, j: any) => {
+              return (
+                <div key={j}>
+                  <p onClick={() => {handleCorrectAnswer(answer)}}>{j + 1}. <span>{answer.answer}</span></p>
+                </div>
+              )
+            })}
+          </div>
+        )
+      })}
     </>
   )
 }
