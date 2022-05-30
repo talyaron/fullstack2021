356e528464152786178ca7c2bdae3130951d2639
ohad gur <ohadgur22@gmail.com>
2022-05-30 20:11:22 +0300

    aa

---
+++ 05-React/07-react-node/followups/ohad/server.ts
@@ -0,0 +1,27 @@
+import express from 'express';
+import mongoose from 'mongoose';
+const app = express()
+const port = process.env.PORT || 4000;
+require('dotenv').config()
+
+const mongodb_uri = process.env.MONGODB_URI
+
+mongoose.connect(
+    mongodb_uri
+  ).then(res=>{
+    console.log("Connected to DB");
+  }).catch(err=>{
+    console.error(err.message)
+  });
+
+  app.use(express.json());
+  app.use(express.static('client/build'));
+  
+
+app.get('/', (req, res) => {
+  res.send('Hello World!')
+})
+
+app.listen(port, () => {
+  console.log(`Example app listening on port ${port}`)
+})
