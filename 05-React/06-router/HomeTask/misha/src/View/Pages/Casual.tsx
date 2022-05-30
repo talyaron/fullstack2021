6311acb6539ea14aea23a243a09f72ff58ddfd00
Michael Dubovyk <93338459+mishalon@users.noreply.github.com>
2022-05-30 09:47:21 +0300

    1

--- 05-React/06-router/HomeTask/misha/src/View/Pages/Casual.tsx
+++ 05-React/06-router/HomeTask/misha/src/View/Pages/Casual.tsx
@@ -1,8 +1,18 @@
 import React from 'react'
+import casualItems from '../../casual.json'
+import Card from '../Components/Card'
+
+const casualDresses = casualItems;
 
 const Casual = () => {
   return (
-    <h1>Casual</h1>
+    <section className='gridWrapper'>
+    {
+      casualDresses.map((dress: any, i: React.Key | null | undefined) => (
+        <Card dress={dress} key={i}/>
+      ))
+    }
+    </section>
   )
 }
 
