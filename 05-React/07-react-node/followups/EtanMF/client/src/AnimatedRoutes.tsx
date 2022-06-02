b427710807e51d4dd1440dbc4fac2e93c3cc48d6
Etanhey <etan@heyman.net>
2022-06-02 13:34:29 -0500

    all good?

--- 05-React/07-react-node/followups/EtanMF/client/src/AnimatedRoutes.tsx
+++ 05-React/07-react-node/followups/EtanMF/client/src/AnimatedRoutes.tsx
@@ -6,22 +6,18 @@ import Login from "./views/Pages/Login";
 import User from "./views/Pages/User";
 import Article from "./views/Pages/Article";
 
-
-
-
-
 function AnimatedRoutes() {
   return (
     <AnimatePresence>
       <Routes>
         <Route path="/" element={<Login />} />
         <Route path="Home" element={<HomePage />}>
-          <Route path=":userId" element={<User/>} >
-              <Route path=":articleId" element={<Article/>}/>
+          <Route path=":userId" element={<User />}>
+            <Route path=":articleId" element={<Article />} />
           </Route>
         </Route>
         <Route path="allArticles" element={<AllArticles />}>
-              <Route path=":articleId" element={<Article/>}/>
+          <Route path=":articleId" element={<Article />} />
         </Route>
       </Routes>
     </AnimatePresence>
