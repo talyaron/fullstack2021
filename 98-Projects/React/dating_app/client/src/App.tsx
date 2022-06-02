2f6675c53befcb8ba48212c12682a457d30a2420
ShaniRom <93940556+ShaniRom@users.noreply.github.com>
2022-06-02 20:50:56 +0300

    start

---
+++ 98-Projects/React/dating_app/client/src/App.tsx
@@ -0,0 +1,26 @@
+import React, { useEffect } from "react";
+
+import "./View/styles/global.scss";
+import axios from "axios";
+import { BrowserRouter, Routes, Route } from "react-router-dom";
+import { useParams } from "react-router-dom";
+
+
+function App() {
+
+  
+  return (
+    // <BrowserRouter>
+    //   <Routes>
+        
+       
+          
+    
+        
+    //   </Routes>
+    // </BrowserRouter>
+    <div>hi</div>
+  );
+}
+
+export default App;
