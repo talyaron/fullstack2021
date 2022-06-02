8db0c4072274a4c6c56cfd12c60275c34acbdfb1
Michael-Frankel <76725406+Michael-Frankel@users.noreply.github.com>
2022-06-02 12:15:50 +0300

    mf

--- 05-React/07-react-node/followups/miki/client/src/App.tsx
+++ 05-React/07-react-node/followups/miki/client/src/App.tsx
@@ -9,15 +9,38 @@ import {
 import Main from './Main';
 import IceCream from './Views/Components/IceCream';
 import Home from './Views/Pages/Home';
-import Login from './Views/Pages/Login'
+import Login from './Views/Pages/Login';
+import Vanilla from './Views/Components/Vanilla';
+import Chokate from './Views/Components/Chokate';
+
+export interface IceCreamFlavourArray {
+  taste: string;
+  subTaste: string;
+}
 
 function App() {
 
-  const [icecream, setIcecream] = useState("")
+  const [vanilla, setVanilla] = useState("")
+  const [chokate, setChokate] = useState("")
+  const [chokateArray, setChokatArray] = useState<Array<IceCreamFlavourArray>>([])
+  // const [login, setLogin] = useState()
 
 
+  async function handleLogin(e: any) {
+
+    e.preventDefault()
+
+    const name = e.target.name.value;
+    const password = e.target.password.value;
+    const payload:any = { name, password }
+
+    const { data } = await axios.post('/icecream/users', payload)
+    console.log(data);
 
 
+    e.target.reset()
+
+  }
 
 
 
@@ -27,12 +50,20 @@ function App() {
 
         <BrowserRouter>
           <Routes>
-          <Route path="/" element={<Login />}></Route>
+            <Route path="/" element={<Login handleLogin={handleLogin} />}></Route>
 
-          
-            <Route path="/home" element={<Main icecream={icecream} setIcecream={setIcecream} />}>
-              <Route index element={<Home />}></Route>
-              <Route path=":iceCreamId" element={<IceCream icecream={icecream} setIcecream={setIcecream} />}></Route>
+
+            <Route path="/home" element={<Main chokateArray={chokateArray} setChokatArray={setChokatArray}
+              vanilla={vanilla} setVanilla={setVanilla} chokate={chokate} setChokate={setChokate} />}>
+
+              <Route path='chokate' element={<Home />}>
+                <Route path=":chokate" element={<Chokate />}></Route>
+              </Route>
+
+              <Route path="vanilla" element={<IceCream />}>
+                <Route path=":vanilla" element={<Vanilla />}></Route>
+              </Route>
+
             </Route>
           </Routes>
         </BrowserRouter>
