20d877bb66ed81044ecd3868e77325aad0236ca1
shayfoyer <93338189+shayfoyer@users.noreply.github.com>
2022-06-06 14:41:26 +0300

    shay

--- 98-Projects/React/dating_app/routes/usersRoute.ts
+++ 98-Projects/React/dating_app/routes/usersRoute.ts
@@ -4,10 +4,12 @@ const router=express.Router()
 
 
 
-import {getUsers,login,getLoginUser} from '../controller/usersController'
+import {getUsers,login,getLoginUser, getUsersProfile, updateUser} from '../controller/usersController'
 
 router
 .get('/users',getUsers)
 .post('/login',login)
 .get('/getLoginUser',getLoginUser)
+.get('/:ProfilesId', getUsersProfile)
+.patch('/updateUsers', updateUser)
 export default router
