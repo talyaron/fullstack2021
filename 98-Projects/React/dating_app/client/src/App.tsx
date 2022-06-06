273219848b3a984638969cbd739c5640e74c47de
shayfoyer <93338189+shayfoyer@users.noreply.github.com>
2022-06-06 14:43:15 +0300

    shay

--- 98-Projects/React/dating_app/client/src/App.tsx
+++ 98-Projects/React/dating_app/client/src/App.tsx
@@ -8,7 +8,7 @@ import LogoBar from './View/Pages/LogoBar';
 import Login from './View/Components/Login';
 import NavBar from './View/Pages/NavBar';
 import Home from './View/Pages/Home';
-
+import Profile from "./View/Components/Profile";
 
 function App() {
    
@@ -29,8 +29,8 @@ function App() {
         <Route index  element={<Login/>}></Route>  
         <Route path="/navBar/:id" element={<NavBar />}>
           <Route index element={<Home />}></Route>
-        {/* <Route path='/:profile'  element={Profile}></Route>   */}
-          </Route>      */}
+          <Route path='/api/:profilesId'  element={<Profile/>}></Route>  
+          </Route>      
        
       </Route>
     </Routes>
