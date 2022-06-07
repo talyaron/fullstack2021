63ddf33cd9cdcf2ca87f997f611376c7587b6581
Yossi A <asnafy.junior@gmail.com>
2022-06-07 16:14:25 +0300

    new

--- 05-React/07-react-node/projects/OR-LEON-YOSSI/client/src/App.tsx
+++ 05-React/07-react-node/projects/OR-LEON-YOSSI/client/src/App.tsx
@@ -1,73 +1,80 @@
 import axios from 'axios';
 import { useEffect, useState } from 'react';
 import './App.css';
+import ListAllUsers from './Pages/ListAllUsers';
+import LoginForm from './View/Components/LoginForm';
 import UserForm from './View/Components/UserForm';
 
 
 function App() {
-
   const [userList, setUserList] = useState([])
-  const [load, isLoad]: any = useState(false)
+  const [load, isLoad] = useState(false);
 
   useEffect(() => {
 
     if (!load) {
       (async () => {
-        const { data }: any = await axios.get('/api/getUsers')
-        const allUsers = data.allUsers
-        console.log(allUsers)
+        const { data } = await axios.get('/api/getUsers')
+        const allUsers = data.allUsers;
         setUserList(allUsers)
-
-      })
-        ();
+        console.log(allUsers)
+      })();
     }
-
   }, [load])
 
 
   async function handleRegister(ev: any) {
-    ev.preventDefault()
-    handleSubmit(ev)
+    ev.preventDefault();
 
+    handleSubmit(ev)
     isLoad(true)
     const registerResponse = await axios.post('/api/addUser', handleSubmit(ev))
+
     if (registerResponse.data === 'Already exists') {
-      window.alert('Already exists')
+      window.alert('AlreadyExists')
     }
     isLoad(false)
   }
 
-    function handleSubmit(ev: any) {
-      ev.preventDefault()
 
-      const name = ev.target.name.value
-      const age = ev.target.age.value
-      const occupation = ev.target.occupation.value
-      const username = ev.target.username.value
-      const password = ev.target.password.value
-      const image = ev.target.image.value
-      console.log(name)
+  function handleSubmit(ev: any) {
+    ev.preventDefault();
+    const name = ev.target.name.value
+    const age = ev.target.age.value
+    const occupation = ev.target.occupation.value
+    const username = ev.target.username.value
+    const password = ev.target.password.value
+    const image = ev.target.image.value
 
-      const userForm = { name, age, occupation, username, password, image }
+    const userForm = { name, age, occupation, username, password, image }
+    console.log(userForm)
+    return userForm
+  }
 
-      return userForm
+
+  function handleLogin(ev: any) {
+    ev.preventDefault();
+
+    const username = ev.target.username.value
+    const password = ev.target.password.value
+
+    const loginUser: any = { username, password }
+    const LoginResponse: any = axios.post('/api/login', loginUser)
+    console.log(LoginResponse)
+
+    if (LoginResponse.data === "Password doesn't match") {
+      window.alert("Password doesn't match")
     }
-
-
-    return (
-      <div className="App">
-
-        <UserForm handleRegister={handleRegister} />
-        {userList.map((user: any, i) =>
-          <div key={i}>
-            <h1>{user.name}</h1>
-            <h1>{user.age}</h1>
-            <h1>{user.username}</h1>
-          </div>
-        )}
-      </div>
-    );
 
   }
 
-  export default App;
+  return (
+    <div className="App">
+      <UserForm handleRegister={handleRegister} />
+      <LoginForm handleLogin={handleLogin} />
+      <ListAllUsers userList={userList} />
+    </div>
+  );
+}
+
+export default App;
--- 05-React/07-react-node/projects/OR-LEON-YOSSI/server.ts
+++ 05-React/07-react-node/projects/OR-LEON-YOSSI/server.ts
@@ -1,15 +1,13 @@
-
 import express from 'express';
 import mongoose from 'mongoose';
 import userRouter from './routes/UserRouters'
-const app = express();
+
+
+const app = express()
 const port = process.env.PORT || 4002;
 
 require('dotenv').config()
 
-app.use(express.static('public/build'))
-app.use(express.json());
-app.use('/api', userRouter)
 
 const mongodb_uri = process.env.MONGODB_URI
 
@@ -20,9 +18,13 @@ mongoose.connect(
   }).catch(err=>{
     console.error(err.message)
   });
- 
+
+  app.use(express.json())
+  app.use(express.static('client/build'))
+  app.use('/api', userRouter)
+
 
 
 app.listen(port, () => {
-    return console.log(`Express is listening at http://localhost:${port}`);
-});
+  console.log(`Example app listening on port ${port}`)
+})
