198898fd41659e30af18c97aec25736508dc105c
tamir-dadon <tamir.dadon63@gmail.com>
2022-06-14 16:56:48 +0300

    tamir

--- 05-React/07-react-node/followups/Tamir-new/Server.ts
+++ 05-React/07-react-node/followups/Tamir-new/Server.ts
@@ -2,7 +2,7 @@ import express from "express";
 import mongoose from "mongoose";
 
 const app = express();
-const port = process.env.PORT || 3000;
+const port = process.env.PORT || 4000;
 
 app.use(express.static("public/build"));
 app.use(express.json());
--- 05-React/07-react-node/followups/Tamir-new/client/src/App.tsx
+++ 05-React/07-react-node/followups/Tamir-new/client/src/App.tsx
@@ -1,17 +1,36 @@
 import "./App.css";
-import { useEffect } from "react";
+import { useEffect, useState } from "react";
 import axios from "axios";
 
 function App() {
+  const [name, setname] = useState("");
+
+  async function handlesubmit(ev: any) {
+    ev.preventDefault();
+    const name = ev.target.elements.name.value;
+    console.log(name);
+  }
+  useEffect(() => {
+    axios
+      .get("/api/users/add-user")
+      .then(({ data }) => console.log(data))
+      .catch((err) => console.error(err));
+  }, []);
+
   useEffect(() => {
     axios
       .get("/api/users/get-user")
       .then(({ data }) => console.log(data))
       .catch((err) => console.error(err));
   }, []);
+
   return (
     <div className="App">
       <h1>hey</h1>
+      <form onSubmit={handlesubmit}>
+        <input type="text" name="name" placeholder="name" />
+        <input type="submit" value="submit" />
+      </form>
     </div>
   );
 }
