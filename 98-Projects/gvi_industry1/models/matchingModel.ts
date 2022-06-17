da177fea588de4f3126f927ef320b62f4feb6410
Alex Rose <52649498+alexrosenbaum@users.noreply.github.com>
2022-06-17 12:20:36 +0300

    alex

--- 98-Projects/gvi_industry1/models/matchingModel.ts
+++ 98-Projects/gvi_industry1/models/matchingModel.ts
@@ -3,25 +3,51 @@ interface MatchingFiltersSchema {
     specialities: Array<speciality>;
     countries: Array<country>;
     TypeOfEscorts: Array<TypeOfEscort>;
-  }
-  
-  interface sector {
+}
+
+interface sector {
     type: string;
     _id: string;
-  }
-  
-  interface speciality {
+}
+
+interface speciality {
     type: string;
     _id: string;
-  }
-  
-  interface country {
+}
+
+interface country {
     name: string;
     _id: string;
-  }
-  
-  interface TypeOfEscort {
+}
+
+interface TypeOfEscort {
     type: string;
     _id: string;
+}
+
+
+
+interface SelectedMentorsSchema {
+    selctedMentors: Array<SelectedMentor>
+}
+
+interface SelectedMentor {
+    userName: Name,
+    _id: String,
+    image: File,
+    sector:string,
+    company:Company
+    lastEntry: Date;
+    linkedInProfile: string;
+
+}
+
+interface Name {
+    first: string;
+    last: string;
+}
+
+interface Company {
+    _id: string;
+    name: string;
   }
-  
