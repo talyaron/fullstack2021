1884a5d5420682ac0fa88fd556b121143c94350b
tamir-dadon <tamir.dadon63@gmail.com>
2022-06-19 08:24:57 +0300

    tamir

--- 05-React/07-react-node/followups/Tamir-new/client/src/App.tsx
+++ 05-React/07-react-node/followups/Tamir-new/client/src/App.tsx
@@ -1,15 +1,18 @@
 import "./App.css";
 import { useEffect, useState } from "react";
 import { BrowserRouter, Routes, Route } from "react-router-dom";
+// import { Outlet } from "react-router-dom";
 import Home from "./Pages/Home";
+import Login from "./Pages/Login";
 import axios from "axios";
 
 interface User {
-  name: string;
+  email: string;
+  password:string
   _id: string;
 }
-interface App{
-  handlesubmit:Function
+interface App {
+  handlesubmit: Function;
 }
 
 function App() {
@@ -27,35 +30,43 @@ function App() {
 
   async function handlesubmit(ev: any) {
     ev.preventDefault();
-    const name = ev.target.elements.name.value;
-    if (name) {
-      axios.post("/api/users/add-user", { name });
-      const user = { name, _id: "" };
+    const email = ev.target.elements.email.value;
+    const password = ev.target.elements.password.value;
+
+    if (email && password) {
+      axios.post("/api/users/add-user", { email , password });
+      const user = { email,password, _id: "" };
       setusers([...users, user]);
     }
+  }
+  async function handleLogin(ev:any) {
+    
   }
 
   return (
     <div className="App">
       <BrowserRouter>
         <Routes>
-          <Route path="/" element={<App />}>
-            <Route index element={<Home handlesubmit={handlesubmit} />} />
-          </Route>
+          <Route path="/" element={<Login />}></Route>
         </Routes>
       </BrowserRouter>
-      {/* <h1>hey</h1>
-      {users.map((user) => (
-        <p key={user._id}>{user.name}</p>
-      ))}
-      
-      </form> */}
-      {/* <form onSubmit={handlesubmit}>
-        <input type="text" name="name" placeholder="name" />
+
+      <form onSubmit={handlesubmit}>
+        <input type="email" name="email" placeholder="email" />
+        <input type="password" name="password" placeholder="password" />
         <input type="submit" value="submit" />
-      </form>  */}
+      </form>
+
+      <form onSubmit={handleLogin}></form>
     </div>
   );
 }
 
 export default App;
+
+{
+  /* <h1>hey</h1>
+      {users.map((user) => (
+        <p key={user._id}>{user.name}</p>
+      ))} */
+}
--- 05-React/07-react-node/followups/Tamir-new/client/src/Pages/Home.tsx
+++ 05-React/07-react-node/followups/Tamir-new/client/src/Pages/Home.tsx
@@ -1,15 +1,12 @@
-interface Home {
-  handlesubmit: Function;
-}
+// interface Home {
+//   handlesubmit: Function;
+// }
 
-const Home = (props: Home) => {
-  const { handlesubmit } = props;
+const Home = () => {
+  // const { handlesubmit } = props;
   return (
     <div>
-      <form onSubmit={handlesubmit}>
-        <input type="text" name="name" placeholder="name" />
-        <input type="submit" value="submit" />
-      </form>
+        eg4thhe
     </div>
   );
 };
