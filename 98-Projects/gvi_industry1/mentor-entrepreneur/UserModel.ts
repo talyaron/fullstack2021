cc9253c3fd32bd0c3d0b5a9167952aa870023f2e
ohad gur <ohadgur22@gmail.com>
2022-06-19 19:46:25 +0300

    a

---
+++ 98-Projects/gvi_industry1/mentor-entrepreneur/UserModel.ts
@@ -0,0 +1,36 @@
+import mongoose from "mongoose";
+
+interface Name {
+    first: string;
+    last: string;
+  }
+
+  export enum UserType {
+    ADMIN = "admin",
+    MENTEE = "mentee",
+    MENTOR = "mentor",
+  }
+
+
+const UserSchema = new mongoose.Schema({
+
+    _id: String,
+    name: Object,
+    description: String,
+    image: File,
+    email: String,
+    country: String,
+    lastEntry: Date,
+    companies: Array,
+    linkedInProfile: String,
+    phone: Number,
+    initiatives: Array,
+    password: String,
+    type: UserType,
+    fieldsOfKnowledge: Array,
+    sectors:Array,
+    preferedUsers: Array,
+});
+
+const users = mongoose.model('users', UserSchema);
+export default users;
