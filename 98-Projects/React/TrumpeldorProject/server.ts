f2395b62d837e7261f34baac27437f75b4dfaacd
artur121255 <arturyaski96@gmail.com>
2022-06-19 16:42:09 +0300

    a

---
+++ 98-Projects/React/TrumpeldorProject/server.ts
@@ -0,0 +1,31 @@
+import express from 'express';
+import mongoose from 'mongoose';
+const app = express();
+const port = process.env.PORT || 4004;
+
+app.use(express.static('/bblog/build'))
+app.use(express.json());
+require('dotenv').config()
+
+const mongodb_uri = process.env.MONGODB_URI;
+
+import blogRoute from './Routes/blogRoute';
+
+app.use("/api/blogs",blogRoute);
+
+mongoose.connect(
+    mongodb_uri
+  ).then(res=>{
+    console.log("Connected to DB");
+  }).catch(err=>{
+    console.error(err.message)
+  });
+
+
+
+app.listen(port, () => {
+return console.log(`Express is listening at http://localhost:${port}`);
+});
+
+
+
