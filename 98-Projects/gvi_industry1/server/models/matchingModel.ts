32869af45362610169989140ab2e9b282bb46957
Alex R <pencilandjelly@gmail.com>
2022-06-20 19:48:32 +0300

    matching model

--- 98-Projects/gvi_industry1/server/models/matchingModel.ts
+++ 98-Projects/gvi_industry1/server/models/matchingModel.ts
@@ -1,50 +1,12 @@
 import mongoose from 'mongoose'
-import {Sector} from '../../mentor-entrepreneur/InitiativeModel'
+import {Sector, FieldsOfKnowledge} from '../../mentor-entrepreneur/InitiativeModel'
 
 const MatchingFiltersSchema = new mongoose.Schema({
-    sectors: Sector
-
-
+    sectors: Sector,
+    specialities:FieldsOfKnowledge,
+    countries: [],
+    typeOfEscorts: []
 })
-
-
-
-
-
-
-interface MatchingFiltersSchema {
-    specialities: Array<speciality>;
-    countries: Array<country>;
-    TypeOfEscorts: Array<TypeOfEscort>;
-}
-
-
-
-
-interface SelectedMentorsSchema {
-    selctedMentors: Array<SelectedMentor>
-}
-
-interface SelectedMentor {
-    userName: Name,
-    _id: String,
-    image: File,
-    sector:string,
-    company:Company
-    lastEntry: Date;
-    linkedInProfile: string;
-
-}
-
-interface Name {
-    first: string;
-    last: string;
-}
-
-interface Company {
-    _id: string;
-    name: string;
-  }
 
 
   const MatchingFilters = mongoose.model('filter', MatchingFiltersSchema);
