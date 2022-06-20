8c827b8166859a6954b7a39560d95e253d80e5a9
Shani <shanaynayrom@gmail.com>
2022-06-20 19:02:11 +0300

    socket

--- 06-Socket.io/shani-chat/server.ts
+++ 06-Socket.io/shani-chat/server.ts
@@ -2,13 +2,22 @@ const express = require("express");
 const app = express();
 const http = require("http");
 const server = http.createServer(app);
+
+//connects the server
 const { Server } = require("socket.io");
-const io = new Server(server);
+//const io = new Server(server);
 const port = process.env.PORT || 4000;
 const cors = require("cors");
 
+const io = new Server(server, {
+  cors: {
+    origin: "http://localhost:3000",
+    methods: ["GET", "POST"],
+  },
+});
 
-app.use(cors());
+
+//app.use(cors());
 
 app.use(express.static('client/build'))
 app.get('/', (req, res) => {
@@ -19,4 +28,5 @@ server.listen(3000, () => {
   console.log('listening on *:3000');
 });
  
+
 
