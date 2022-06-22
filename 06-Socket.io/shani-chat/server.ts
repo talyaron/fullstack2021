ce219798da3e728e4d5251b4869928be53427f2b
Shani <shanaynayrom@gmail.com>
2022-06-22 21:48:00 +0300

    socket.io

--- 06-Socket.io/shani-chat/server.ts
+++ 06-Socket.io/shani-chat/server.ts
@@ -7,7 +7,7 @@ const server = http.createServer(app);
 const { Server } = require("socket.io");
 //const io = new Server(server);
 const port = process.env.PORT || 4000;
-const cors = require("cors");
+//const cors = require("cors");
 
 const io = new Server(server, {
   cors: {
@@ -16,8 +16,29 @@ const io = new Server(server, {
   },
 });
 
-
+;
 //app.use(cors());
+//why needed if i didnd use it
+
+io.on('connection',(socket)=>{
+  console.log('a user is connected')
+  console.log(socket.id)
+
+  socket.on('msg',({text})=>{
+    console.log(text)
+    socket.broadcast.emit('msg',{text});
+    //if its io.emit then its sends to everyone and the person who sent it
+    //makes it that it sends to everyone except the person who wrote it
+    //sends it to the clients
+  })
+  
+
+
+socket.on('disconnect',()=>{
+console.log('user disconnected')
+});
+});
+
 
 app.use(express.static('client/build'))
 app.get('/', (req, res) => {
@@ -27,6 +48,6 @@ app.get('/', (req, res) => {
 server.listen(3000, () => {
   console.log('listening on *:3000');
 });
- 
+ // the star means it not only listens to the port but to the socket
 
 
