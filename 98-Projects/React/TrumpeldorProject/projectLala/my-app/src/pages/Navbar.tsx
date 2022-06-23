3d38f04402dba372c86e97f02dd14334e8e46959
artur yaski <arturyaski96@gmail.com>
2022-06-23 19:56:30 +0300

    Project

--- 98-Projects/React/TrumpeldorProject/projectLala/my-app/src/pages/Navbar.tsx
+++ 98-Projects/React/TrumpeldorProject/projectLala/my-app/src/pages/Navbar.tsx
@@ -1,21 +1,58 @@
 import React from "react";
 import { BrowserRouter, Route, Link } from "react-router-dom";
+import Button from "@mui/material/Button";
 
 function Navbar() {
   return (
-    <nav className="navbar">
-      <ul className="navbar-navList">
-        <li className="navbar-navList-link" style={{display:'block'}}>
-        <Link to="/Orders">להזמדנות</Link>
-        </li>
-        <li>
-          <div>2</div>
-        </li>
-        <li>
-        <div>3</div>
-        </li>
-      </ul>
-    </nav>
+    <div className="Navbar">
+      <Button
+        variant="outlined"
+        className="Navbar-Order"
+        style={{
+          borderRadius: 35,
+          backgroundColor: "yellow",
+          padding: "18px 36px",
+          fontSize: "18px",
+        }}
+      >
+        להזמנה
+      </Button>
+
+      <Button
+        variant="outlined"
+        className="Navbar-StayConnect"
+        style={{
+          borderRadius: 35,
+          padding: "18px 36px",
+          fontSize: "18px",
+        }}
+      >
+        צור קשר
+      </Button>
+      <Button
+        variant="outlined"
+        className="Navbar-Manu"
+        style={{
+          borderRadius: 35,
+          padding: "18px 36px",
+          fontSize: "18px",
+        }}
+      >
+        תפריט
+      </Button>
+      <Button
+        variant="outlined"
+        className="Navbar-Main"
+        style={{
+          borderRadius: 35,
+          padding: "18px 36px",
+          fontSize: "18px",
+        }}
+      >
+        לדף הבית
+      </Button>
+      <div className="Navbar-Silmbol"></div>
+    </div>
   );
 }
 
