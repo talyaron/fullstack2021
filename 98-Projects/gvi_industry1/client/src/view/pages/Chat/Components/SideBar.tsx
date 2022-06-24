58a338915f58d2a680e4e805543bf4ec764e3286
Etan Joseph Heyman <etan@heyman.net>
2022-06-24 18:16:47 -0500

    Chat figma clone
    Done.

--- 98-Projects/gvi_industry1/client/src/view/pages/Chat/Components/SideBar.tsx
+++ 98-Projects/gvi_industry1/client/src/view/pages/Chat/Components/SideBar.tsx
@@ -3,6 +3,10 @@ import {InputBase} from '@mui/material';
 import {faBars} from '@fortawesome/free-solid-svg-icons';
 import {FontAwesomeIcon} from '@fortawesome/react-fontawesome';
 import {UserInterface} from '../Chat';
+import {ReactComponent as SearchUsersIcon} from '../SearchUsers.svg';
+import {ReactComponent as SVGHamburgerIcon} from '../HamburgerIcon.svg';
+import {ReactComponent as SideBarDivider} from '../SideBarDivider.svg';
+
 const HamburgerIcon = <FontAwesomeIcon style={{color: 'blue'}} icon={faBars} />;
 interface SideBarProps {
     userList?: Array<UserInterface>;
@@ -12,12 +16,10 @@ interface SideBarProps {
 function SideBar(props: SideBarProps) {
     const {userList, getUserList} = props;
     useEffect(() => {
-        return () =>{
+        return () => {
             getUserList();
-
-        }
-
-    },[])
+        };
+    }, []);
     const {
         // userList
     } = props;
@@ -25,24 +27,28 @@ function SideBar(props: SideBarProps) {
     return (
         <div className='chat__sideBar'>
             <div className='chat__sideBar__searchBar'>
-                {HamburgerIcon}
+                <SVGHamburgerIcon style={{padding: '21px 26px'}} />
                 <div className={`search` + id}>
-                    <FontAwesomeIcon style={{color: 'red'}} icon={'search'} pull='left' />
-                    <InputBase />
+                    <SearchUsersIcon />
+                    {/* <FontAwesomeIcon style={{color: 'red'}} icon={'search'} pull='left' /> */}
+                    <InputBase placeholder='Search' />
                 </div>
             </div>
             <ul className='chat__sideBar__recipientsList'>
-                {userList
-                    ? userList.map((user, i) => {
-                          return <li key={i}>
-                            <p>
-                            {user.userName.last}
-                            </p>
-                            </li>;
-                      })
-                    : <h1>userList</h1>}
+                {userList ? (
+                    userList.map((user, i) => {
+                        return (
+                            <li key={i}>
+                                <p>{user.userName.last}</p>
+                            </li>
+                        );
+                    })
+                ) : (
+                    <h1>userList</h1>
+                )}
             </ul>
-            <ul className='chat__sideBar__buttons'>
+            <SideBarDivider style={{}}/>
+            <ul className='chat__sideBar__Buttons'>
                 <li className='btn'>100Tasks</li>
                 <li className='btn'>Docs</li>
                 <li className='btn'>Records</li>
