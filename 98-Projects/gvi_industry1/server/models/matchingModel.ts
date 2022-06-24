9352049d610ee1a8257e0c8e0053873bdb1a7f2e
Alex R <pencilandjelly@gmail.com>
2022-06-24 17:00:07 +0300

    matching changes

--- 98-Projects/gvi_industry1/server/models/matchingModel.ts
+++ 98-Projects/gvi_industry1/server/models/matchingModel.ts
@@ -1,11 +1,16 @@
 import mongoose from 'mongoose'
 import {Sector, FieldsOfKnowledge} from './userModel'
 
+export enum MentoringType {
+  UX = "ux",
+  BUSINESS_DEVELOPMENT = "development"
+}
+
 const MatchingFiltersSchema = new mongoose.Schema({
     sectors: Sector,
-    specialities:FieldsOfKnowledge,
-    countries: [],
-    typeOfMentoring: []
+    specialties:FieldsOfKnowledge,
+    countries: [String],
+    typesOfMentoring: MentoringType
 })
 
 
--- 98-Projects/gvi_industry1/server/server.ts
+++ 98-Projects/gvi_industry1/server/server.ts
@@ -1,20 +1,45 @@
 const express = require("express");
+import mongoose from 'mongoose';
+
 const app = express();
 const http = require("http");
 const server = http.createServer(app);
 const { Server } = require("socket.io");
+
+
 export const io = new Server(server);
 const port = process.env.PORT || 4000;
 var cors = require("cors");
+require('dotenv').config()
 
+
+const mongodb_uri = process.env.MONGODB_URI
 
 app.use(cors());
+app.use(express.static('public/build'))
+app.use(express.json());
 
-app.use(express.static('client/build'))
 
 io.on("connection", (socket: any) => {
   console.log('user connected', socket.id)
 });
+
+
+mongoose.connect(
+  mongodb_uri
+)
+  .then(() => {
+      console.log("connected to Mongoose");
+  })
+  .catch((err) => {
+      console.log("Failed to connect to Mongoose:")
+      console.log(err.message);
+  });
+
+
+app.get('/', (req, res) => {
+  res.send('hello world')
+})
 
 server.listen(port, () => {
   console.log(`listening on *:${port}`);
