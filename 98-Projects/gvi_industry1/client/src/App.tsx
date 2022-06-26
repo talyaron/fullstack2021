2446bacbd1e933feae8b85a89096964e8c02db20
Ido gino <davegino220@gmail.com>
2022-06-26 17:23:57 +0300

    card_profile

--- 98-Projects/gvi_industry1/client/src/App.tsx
+++ 98-Projects/gvi_industry1/client/src/App.tsx
@@ -1,11 +1,13 @@
 import React from 'react';
 import './App.css';
 import Card from './view/pages/mentee/Card';
+import Profile from './view/pages/mentee/Profile';
 
 function App() {
   return (
     <>
     <Card/>
+    {/* <Profile /> */}
     
     </>
   );
