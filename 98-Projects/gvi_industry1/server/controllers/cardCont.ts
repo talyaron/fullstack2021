1e9144d73288ba58ddca28b6169f45e94f7c90a0
artur yaski <arturyaski96@gmail.com>
2022-06-26 22:05:34 +0300

    CardWork

--- 98-Projects/gvi_industry1/server/controllers/cardCont.ts
+++ 98-Projects/gvi_industry1/server/controllers/cardCont.ts
@@ -1,19 +1,36 @@
 import UserModel from "../models/userModel";
-export async function getUser (req,res){
-    try {
-        
-        const result = await  UserModel.find({})
-        res.send({ ok: true,result});
-        console.log(result)
-        // console.log(data);
-        
-
-        }
-    catch (err) {
-        console.error(err);
-        res.send({ error: err.message, ok: false });
-
-    }
-
-
+export async function getUser(req, res) {
+  try {
+    const result = await UserModel.find({});
+    res.send({ ok: true, result });
+    console.log(result);
+    // console.log(data);
+  } catch (err) {
+    console.error(err);
+    res.send({ error: err.message, ok: false });
+  }
+}
+export async function postUser(req, res) {
+  try {
+    const {name,
+        description,
+        image,
+        email,
+        country,
+        lastEntry,
+        companies,
+        linkedInProfile,
+        phone,
+        initiatives,
+        password,
+        type,
+        fieldsOfKnowledge,
+        sectors,
+        profession,
+      
+    } = req.body;
+    
+  } catch (error) {
+    console.error(error);
+  }
 }
