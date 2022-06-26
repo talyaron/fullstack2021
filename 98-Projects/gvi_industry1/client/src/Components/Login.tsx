5fb10cf9387f0c02c0b713887ec6d6844c342487
Shani <shanaynayrom@gmail.com>
2022-06-26 20:10:31 +0300

    login window scss and html

--- 98-Projects/gvi_industry1/client/src/Components/Login.tsx
+++ 98-Projects/gvi_industry1/client/src/Components/Login.tsx
@@ -1,4 +1,4 @@
-import React from 'react'
+import React,{useState} from 'react'
 
 
 
@@ -7,20 +7,35 @@ interface LoginProps{
     setLoginWindow:Function;
 }
 const Login = (props:LoginProps) => {
+    const [showPassword,setShowPassword]=useState(true);
+
     const {loginWindow,setLoginWindow}=props
   return (
     <div className={loginWindow?'login showLogin':'login dontShowLogin'}>
        <button className='closeButton' onClick={()=>{setLoginWindow(false)}} >X</button>
-       <h2>Welcome Back!</h2>
+       <h2 className='login__greeting'>Welcome Back!</h2>
        <div className='login__with'>
-<button>google</button>
-<button>linkedin</button>
+        <button>google</button>
+        <button>linkedin</button>
        </div>
        
-       <p className='divider'>or</p>
+       <p className='login__divider'>or</p>
        
       
-      
+      <div className='login__form'>
+        <form>            
+        <label htmlFor='username'>Username or email</label>
+          <input type='text' id="username" name="username" placeholder='username'/>
+        <label htmlFor='password'> Password</label> 
+            <div className='password'>
+                <input type={showPassword?'password':"text"} id="password" name="password" placeholder='password'/>         
+                <img onClick={()=>setShowPassword(!showPassword)} src={showPassword?"https://cdn2.iconfinder.com/data/icons/ui-essential-10/24/hide-512.png":"https://cdn2.iconfinder.com/data/icons/ui-essential-10/24/eye-512.png"}/>
+                
+            </div>
+        <input type="submit" value="Login" className='login__submit'/>
+        </form>
+
+      </div>
 
     </div>
   )
