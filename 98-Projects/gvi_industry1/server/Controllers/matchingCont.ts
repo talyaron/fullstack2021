2139e68acbb89ddc653347c8f9f06462c49b6fad
Alex R <pencilandjelly@gmail.com>
2022-06-27 19:15:05 +0300

    model changes

--- 98-Projects/gvi_industry1/server/Controllers/matchingCont.ts
+++ 98-Projects/gvi_industry1/server/Controllers/matchingCont.ts
@@ -1,6 +1,8 @@
 import MatchingModel from '../Models/matchingModel'
 
 export const getMatches = async (req,res) => {
-    const matchings = await MatchingModel.find();
+    const matchings = await MatchingModel.find({countries:{$regex: /rus/i}});
     res.send({ok:true,matchings})
 }
+
+{$regex: /canada/gi}
--- 98-Projects/gvi_industry1/server/models/matchingModel.ts
+++ 98-Projects/gvi_industry1/server/models/matchingModel.ts
@@ -4,9 +4,9 @@ import {Sector, FieldsOfKnowledge} from '../Models/userModel'
 
 
 const MatchingFiltersSchema = new mongoose.Schema({
-    sectors: [String],
-    specialties:[String],
-    countries: [String],
+    sectors: String,
+    specialties:String,
+    countries: String,
     typesOfMentoring: {
       UX : String,
       BUSINESS_DEVELOPMENT :String
