3e85c85e0ec9ad71752c5acae7a0b79499ecb141
ohad <ohadgur22@gmail.com>
2022-07-04 19:54:26 +0300

    fix

--- 98-Projects/gvi_industry1/client/src/App.tsx
+++ 98-Projects/gvi_industry1/client/src/App.tsx
@@ -1,22 +1,22 @@
 import { socket } from "./index";
 import Profile from "./view/pages/mentee/Profile";
 // import Card from "./view/pages/mentee/Card"//
-
+import SearchMentor from "./view/Components/SearchMentor";
 import React from "react";
-import "./view/Styles/global.scss";
-import HomePage from "./view/components/HomePage";
+import "./view/styles/global.scss";
+import HomePage from "./view/Components/HomePage";
 import Matching from "./view/pages/matching/Matching";
 import Chat from "./view/pages/Chat/Chat";
 import { useState, useEffect } from "react";
 import axios from "axios";
 import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
-import Button from "./view/components/Button";
-import SelectedMentor from "./view/pages/mentee/SelectedMentor";
+import Button from "./view/Components/Button";
 
 function App() {
-
   const [mentorsList, setMentorsList] = useState([]);
   const [currentUser, setCurrentUser] = useState({});
+
+
 
   useEffect(() => {
     (async () => {
@@ -30,10 +30,10 @@ function App() {
   return (
     <Router>
       <Routes>
-        <Route path="profile/_id" element={<Profile user={currentUser}/>}
-        {/* <Route path='/' element={<HomePage />}/>
+        <Route path='/' element={<HomePage />}/>
         <Route path="/chat" element={<Chat />} />
-        <Route path="/matching" element={<Matching mentorsList={mentorsList} setMentorsList={setMentorsList} currentUser={currentUser} />} /> */}
+        <Route path="/matching" element={<Matching mentorsList={mentorsList} setMentorsList={setMentorsList} currentUser={currentUser} />} />
+        
       </Routes>
     </Router>
   );
