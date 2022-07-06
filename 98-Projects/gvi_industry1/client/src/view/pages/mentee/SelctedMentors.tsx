48b9181d07996eb30b5cfe5b264250eb2cb4e8f0
artur yaski <arturyaski96@gmail.com>
2022-07-06 18:40:31 +0300

    selected mentor work

---
+++ 98-Projects/gvi_industry1/client/src/view/pages/mentee/SelctedMentors.tsx
@@ -0,0 +1,20 @@
+import React from "react";
+import "../mentee/style/selectedPage.scss";
+import Card from "./Card";
+function SelctedMentors() {
+  return (
+    <div className="selectedPage">
+      <h5>Selcted-Mentors</h5>
+      <div className="selectedPage__wrapper">
+        <Card />
+        <Card />
+        <Card />
+        <Card />
+        <Card />
+        <Card />
+      </div>
+    </div>
+  );
+}
+
+export default SelctedMentors;
