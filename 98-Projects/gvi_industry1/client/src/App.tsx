94d1b9ef1e83c04133c91d375e128f1b1f324f69
Omri <omri832@gmail.com>
2022-07-06 10:02:27 +0300

    fix file name

--- 98-Projects/gvi_industry1/client/src/App.tsx
+++ 98-Projects/gvi_industry1/client/src/App.tsx
@@ -1,16 +1,16 @@
 import { socket } from "./index";
 import Profile from "./view/pages/mentee/Profile";
 // import Card from "./view/pages/mentee/Card"//
-import SearchMentor from "./view/Components/SearchMentor";
+import SearchMentor from "./view/components/SearchMentor";
 import React from "react";
 import "./view/styles/global.scss";
-import HomePage from "./view/Components/HomePage";
+import HomePage from "./view/components/HomePage";
 import Matching from "./view/pages/matching/Matching";
 import Chat from "./view/pages/Chat/Chat";
 import { useState, useEffect } from "react";
 import axios from "axios";
 import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
-import Button from "./view/Components/Button";
+import Button from "./view/components/Button";
 
 function App() {
   const [mentorsList, setMentorsList] = useState([]);
