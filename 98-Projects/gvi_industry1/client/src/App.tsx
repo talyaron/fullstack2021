eca9702ff8c457c39fa17e8e2a7d1d142b6d6d48
Ido gino <davegino220@gmail.com>
2022-07-06 19:47:36 +0300

    profileForm

--- 98-Projects/gvi_industry1/client/src/App.tsx
+++ 98-Projects/gvi_industry1/client/src/App.tsx
@@ -1,9 +1,8 @@
 import { socket } from "./index";
 import Profile from "./view/pages/mentee/Profile";
 // import Card from "./view/pages/mentee/Card"//
-import SearchMentor from "./view/components/SearchMentor";
 import React from "react";
-import "./view/styles/global.scss";
+import "./view/Styles/global.scss";
 import HomePage from "./view/components/HomePage";
 import Matching from "./view/pages/matching/Matching";
 import Chat from "./view/pages/Chat/Chat";
@@ -11,14 +10,10 @@ import { useState, useEffect } from "react";
 import axios from "axios";
 import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
 import Button from "./view/components/Button";
-<<<<<<< Updated upstream
-=======
 import SelectedMentor from "./view/pages/mentee/SelectedMentor";
-import { Card } from "@mui/material";
-import MatchingCards from "./view/pages/matching/components/MatchingCards";
->>>>>>> Stashed changes
 
 function App() {
+
   const [mentorsList, setMentorsList] = useState([]);
   const [currentUser, setCurrentUser] = useState({});
 
@@ -33,21 +28,13 @@ function App() {
 
   return (
     <Router>
-<<<<<<< Updated upstream
       <Routes>
         <Route path="/" element={<Profile id="1234"/>} />
         {/* <Route path='/' element={<HomePage />}/>
         <Route path="/chat" element={<Chat />} />
-        <Route path="/matching" element={<Matching mentorsList={mentorsList} setMentorsList={setMentorsList} currentUser={currentUser} />} />
-        
+        <Route path="/matching" element={<Matching mentorsList={mentorsList} setMentorsList={setMentorsList} currentUser={currentUser} />} /> */}
       </Routes>
     </Router>
-=======
-        <Route path="/" element={<Card />}>
-          <Route path="/Matching" element={<Matching />} />
-        </Route>
-      </Router>
->>>>>>> Stashed changes
   );
 }
 
--- 98-Projects/gvi_industry1/client/src/view/pages/mentee/SelctedMentors.tsx
+++ 98-Projects/gvi_industry1/client/src/view/pages/mentee/SelctedMentors.tsx
@@ -1,20 +1,20 @@
 import React from "react";
 import "../mentee/style/selectedPage.scss";
-import Card from "./Card";
-function SelctedMentors() {
-  return (
-    <div className="selectedPage">
-      <h5>Selcted-Mentors</h5>
-      <div className="selectedPage__wrapper">
-        <Card />
-        <Card />
-        <Card />
-        <Card />
-        <Card />
-        <Card />
-      </div>
-    </div>
-  );
-}
+// import Card from "./Card";
+// function SelctedMentors() {
+//   return (
+//     <div className="selectedPage">
+//       <h5>Selcted-Mentors</h5>
+//       <div className="selectedPage__wrapper">
+//         <Card />
+//         <Card />
+//         <Card />
+//         <Card />
+//         <Card />
+//         <Card />
+//       </div>
+//     </div>
+//   );
+// }
 
-export default SelctedMentors;
+// export default SelctedMentors;
