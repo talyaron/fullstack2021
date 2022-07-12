2b95509ec8a8d65b2fdd1d3a4b510a3aaa170416
ShayFoyer <tfoyer@gmail.com>
2022-07-12 10:57:20 +0300

    shay

--- 98-Projects/gvi_industry1/client/src/view/Components/Login.tsx
+++ 98-Projects/gvi_industry1/client/src/view/Components/Login.tsx
@@ -42,7 +42,7 @@ const Login = (props:LoginProps) => {
        <h2 className='login__greeting'>Welcome Back!</h2>
        <div className='login__with'>
         
-        <button><div><img src="/googleLogo.png" alt="google logo" className='login__with--googleLoginButtonImg'/></div><div>Continue with google</div></button>
+        <button><div><img src="./googleLogo.png" alt="google logo" className='login__with--googleLoginButtonImg'/></div><div>Continue with google</div></button>
         
        </div>
        
---
+++ 98-Projects/gvi_industry1/client/src/view/Components/NavBar.tsx
@@ -0,0 +1,20 @@
+import React from 'react';
+
+
+
+function NavBar() {
+  return (
+    <div>
+      <div className='navBar'>
+
+        
+                    
+        <div className='logo'><img src="./gvi-logo.png" alt="logo" /></div>
+     
+        
+      </div>
+    </div>
+  )
+}
+
+export default NavBar
