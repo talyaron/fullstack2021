ece15290048209684cebc5e936ae34a83420ddeb
michael dubovyk <michaeldubovik@gmail.com>
2022-07-12 12:46:23 +0300

    recipients
    selected update

--- 98-Projects/gvi_industry1/client/src/view/pages/Chat/Components/SideBar.tsx
+++ 98-Projects/gvi_industry1/client/src/view/pages/Chat/Components/SideBar.tsx
@@ -1,9 +1,10 @@
-import {useId, useEffect, useState, useRef} from 'react';
-import {InputBase} from '@mui/material';
-import {UserInterface} from '../Chat';
+import { useId, useEffect, useState, useRef } from 'react';
+import { InputBase } from '@mui/material';
+import { UserInterface } from '../Chat';
 import SearchUsersIcon from '../Icons/SearchUsers';
 import SideBarDivider from '../Icons/SideBarDivider';
-import {socket} from '../../../../index';
+import { socket } from '../../../../index';
+
 
 interface SideBarProps {
 
@@ -15,21 +16,51 @@ interface SideBarProps {
 
 function SideBar(props: SideBarProps) {
 
-    const {userList, setRecipient} = props;
+    const { userList, setRecipient } = props;
 
-    const [userListMap,setUserListMap] = useState(userList[0])
+    const [userListMap, setUserListMap] = useState(userList[0])
 
-    const [selectedRec, setSelectedRec] = useState(false)
+    const [selectedRec, setSelectedRec] = useState<any>('')
 
-    const SelectedRef = useRef(null);
-    
+    // const SelectedRefs = useRef<HTMLLIElement>(null!);
+    const SelectedRefs: any = useRef([])
+    SelectedRefs.current = [];
+
     const id = useId();
 
-    useEffect(()=>{
-        return (()=>{
-            console.log('userlistmap sidebar'+ userListMap)
+    useEffect(() => {
+        return (() => {
+            console.log('userlistmap sidebar' + userListMap)
         })
-    },[])
+    }, [])
+
+    const addToRefs = (el: any) => {
+
+        if (el && !SelectedRefs.current.includes(el)) {
+            SelectedRefs.current.push(el)
+        }
+    }
+
+    useEffect(() => {
+
+        SelectedRefs.current.forEach((recipient: any) => {
+
+            if (recipient.classList.contains('selected')) {
+                recipient.classList.remove('selected')
+            }
+
+            if (selectedRec) {
+                if (selectedRec._id.$oid === recipient.id) {
+                    recipient.classList.add('selected')
+                }
+            }
+
+
+
+        });
+
+
+    }, [selectedRec])
 
     return (
         <div className='chat__sideBar'>
@@ -41,16 +72,21 @@ function SideBar(props: SideBarProps) {
             </div>
             <ul className='chat__sideBar__recipientsList'>
                 {userListMap ? (
-                    userListMap.map((user:any, i:any) => {
+                    userListMap.map((user: any, i: any) => {
                         return (
-                            <li className={selectedRec ? "selected": "notSelected"}
+                            <li
+                                ref={addToRefs}
                                 key={i}
+                                id={user._id.$oid}
+                                className='recipient'
                                 onClick={() => {
+
+                                    setSelectedRec(user)
                                     // setRecipient(user);
-                                    setSelectedRec(true)
+                                    // setSelected()
                                 }}
-                                >
-                                <p ref={SelectedRef}>{user.userName.first} {user.userName.last}</p>
+                            >
+                                <p >{user.userName.first} {user.userName.last}</p>
                             </li>
                         );
                     })
