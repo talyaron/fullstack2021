5c3289a8d82c962bbc0de167fd652fb31cbeda03
ShayFoyer <tfoyer@gmail.com>
2022-07-13 20:25:10 +0300

    putting function to get user logged in profile

--- 98-Projects/gvi_industry1/client/src/view/pages/navBar/NavBar.tsx
+++ 98-Projects/gvi_industry1/client/src/view/pages/navBar/NavBar.tsx
@@ -4,13 +4,8 @@ import NavBarMentor from "./components/NavBarMentor";
 import { useState, useEffect } from "react";
 import axios from "axios";
 import { type } from "os";
-// interface navBarProps{
-//     id:String;
-//     image:String;
 
-// }
 function NavBar() {
-  //const{image, id} =props
   const [myMentors, setMyMentors] = useState(false);
   const [myProfile, setMyProfile] = useState(false);
   const [request, setRequest] = useState(false);
@@ -20,10 +15,8 @@ function NavBar() {
   const [myMentee, setMyMentee] = useState(false);
   const [matching, setMatching] = useState(false);
   const [currentUserType, setCurrentUserType] = useState("");
-  // const [mentee, setMentee]= useState(false);
-  // const[mentor, setMentor]=useState(false);
-  //  const [image, setImage]= useState("")
-  //  const id=""
+ 
+ 
   useEffect(() => {
     //get data on the user and show the chosen user by id
 
@@ -31,21 +24,11 @@ function NavBar() {
       try {
         const { data } = await axios.post("/api/users/get-LoggedIn-Profile");
         const { currentUser } = data;
-        console.log(currentUser);
-        console.log(currentUser.image, currentUser._id);
-        // console.log(data);
-        //console.log(profileId +"profile id from navbar")
+    
+     
         setloggedInUser(currentUser);
           setCurrentUserType(currentUser.type);
-        // if (currentUser.type == "mentee") {
-          
-        // } else {
-        //   if (currentUser.type == "mentor") {
-        //     setloggedInUser(currentUser);
-        //     setCurrentUserType(currentUser.type);
-        //   }
-        // }
-        // setloggedInUser(loggedInUserObj);
+
         if (!loggedInUser) {
           throw new Error("no profile");
         }
@@ -55,19 +38,6 @@ function NavBar() {
     })();
   }, []);
 
-  // if(currentUserType==="mentor"){
-  // return (
-  //   <div>
-
-  //       <NavBarMentee  myProfile={myProfile} setMyProfile={setMyProfile} request={request} setRequest={setRequest} myMentors={myMentors} setMyMentors={setMyMentors} mentorMatching={mentorMatching} setMentorMatching={setMentorMatching} loggedInUser={loggedInUser} myTask={myTask} setMyTask={setMyTask}/>
-  //     </div>)}
-
-  // else{
-  //   return(
-  //     <div>
-  //       <NavBarMentor  myProfile={myProfile} setMyProfile={setMyProfile} request={request} setRequest={setRequest} myMentee={myMentee} setMyMentee={setMyMentee} matching={matching} setMatching={setMatching} loggedInUser={loggedInUser}/>
-
-  //     </div>)}
   return(
   
     currentUserType === "mentee" ? (
