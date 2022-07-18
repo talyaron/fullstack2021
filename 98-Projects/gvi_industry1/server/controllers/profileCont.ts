dea77a37dfce9328cb6183eb081e2ad5c71b162c
Tal Yaron <tal.yaron@gmail.com>
2022-07-18 19:40:21 +0300

    cont

---
+++ 98-Projects/gvi_industry1/server/controllers/profileCont.ts
@@ -0,0 +1,15 @@
+import { v2 as cloudinary } from "cloudinary";
+import UserModel from "../models/userModel";
+
+const cloud_name = process.env.CLOUD_NAME;
+const api_key = process.env.API_KEY;
+const api_secret = process.env.API_SECRET;
+
+if(!cloud_name || !api_key || !api_secret) console.error('no keys for cloudinary in profileCont')
+
+cloudinary.config({
+  cloud_name,
+  api_key,
+  api_secret,
+  secure: true,
+});
