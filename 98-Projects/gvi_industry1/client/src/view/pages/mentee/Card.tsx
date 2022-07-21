8b824e632d11c7613314cb20b8f4436f6439e0db
Omri <omri832@gmail.com>
2022-07-21 14:33:24 +0300

    selectedUsers css

--- 98-Projects/gvi_industry1/client/src/view/pages/mentee/Card.tsx
+++ 98-Projects/gvi_industry1/client/src/view/pages/mentee/Card.tsx
@@ -22,29 +22,32 @@ const Card = (props: CardProps) => {
 
   return (
     <>
-    {selectedUsers.map((selectedUser: any, i:any) =>
+      {selectedUsers.map((selectedUser: any, i: any) =>
         <div className="card" key={i}>
+
           <div className="card__photo">
-            {selectedUser.image ? <img src={`${selectedUser.image}`} />: <img src={'https://www.pngitem.com/pimgs/m/504-5040528_empty-profile-picture-png-transparent-png.png'} />}
+            <img src={selectedUser.image ? `${selectedUser.image}` : 'https://www.pngitem.com/pimgs/m/504-5040528_empty-profile-picture-png-transparent-png.png'} />
           </div>
+
           <div className="card__center">
             <p>{selectedUser['name']['first']} {selectedUser['name']['last']}</p>
             <div className="card__flex">
-              <img src={`${selectedUser.country}`}  />
-              <LinkedInIcon className="card__flex__linkdIn" style={{ fontSize: "30px" }} >
-                {/* {selectedUser.linkedInProfile} */}
-              </LinkedInIcon>
+              <img src={`${selectedUser.country}`} />
+              <a href={selectedUser.linkedInProfile ? `${selectedUser.linkedInProfile}` : 'https://www.linkedin.com/'}>
+                <LinkedInIcon className="card__flex__linkdIn" style={{ fontSize: "30px" }} ></LinkedInIcon>
+              </a>
             </div>
             <p className="card__company">{selectedUser.fieldsOfKnowledge}</p>
             <p className="card__profession">{selectedUser.sector}</p>
           </div>
+
           <div className="card__star">
             <StarIcon></StarIcon>
           </div>
 
         </div>
       )
-    }
+      }
     </>
   );
 };
