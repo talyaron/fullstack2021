3a99d34e4011ebe84c3f99031700a7db21b0d9c7
Etan Joseph Heyman <etan@heyman.net>
2022-07-31 12:10:31 -0500

    more homework work

--- 05-React/08-redux/thunk/homeWork/etan/src/App.tsx
+++ 05-React/08-redux/thunk/homeWork/etan/src/App.tsx
@@ -1,6 +1,7 @@
 
-import {useAppDispatch} from './app/hooks';
+import {useAppDispatch,useAppSelector} from './app/hooks';
 import {getPicAsync} from './features/picComp/picCompApi';
+import {selectPicStatus, status} from './features/picComp/picCompSlice'
 import Pic from './Views/Components/Pic';
 import './styles/global.scss';
 
@@ -8,6 +9,7 @@ import './styles/global.scss';
 
 function App() {
     const dispatch = useAppDispatch();
+    const picCompStatus = useAppSelector(selectPicStatus)
 
     const handleGetPicAsync =  () => {
         try {
@@ -19,7 +21,7 @@ function App() {
     return (
         <div className='App'>
             <header className='App-header'>
-                <button className='myButton' onClick={handleGetPicAsync}>Want to see some dogs?</button>
+               <button className='myButton' onClick={handleGetPicAsync}>{picCompStatus === status.LOADING ? 'Loading...' : 'Want to see some dogs?'}</button>
                 <Pic />
             </header>
         </div>
--- 05-React/08-redux/thunk/homeWork/etan/src/Views/Components/Pic.tsx
+++ 05-React/08-redux/thunk/homeWork/etan/src/Views/Components/Pic.tsx
@@ -1,19 +1,18 @@
-import { PacmanLoader } from 'react-spinners';
-import { useAppSelector } from '../../app/hooks';
-import {selectPic, selectPicStatus, status} from '../../features/picComp/picCompSlice'
+import {HashLoader, PacmanLoader} from 'react-spinners';
+import {useAppSelector} from '../../app/hooks';
+import {selectPic, selectPicStatus, status} from '../../features/picComp/picCompSlice';
 
 function Pic() {
-  const image = useAppSelector(selectPic)
-  const imageReady:status = useAppSelector(selectPicStatus)
-  return (
-    <div className="picComp">
-
-{imageReady === status.LOADING? <PacmanLoader/> 
-:
-<img src={image} className="App-logo" alt="" />
+    const image = useAppSelector(selectPic);
+    const imageReady: status = useAppSelector(selectPicStatus);
+    return <div className='picComp'>{
+      imageReady === status.LOADING ?
+       <
+        // PacmanLoader
+        HashLoader
+         loading={imageReady === status.LOADING} speedMultiplier={1} size='30px' color='#d5b8ff' /> 
+       
+       : <img src={image} className='App-logo' alt='' />}</div>;
 }
-    </div>
-  )
-}
 
-export default Pic
+export default Pic;
