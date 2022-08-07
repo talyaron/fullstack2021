decd61168997ac03667399fe12f790a432007722
Ido gino <davegino220@gmail.com>
2022-08-07 13:06:15 +0300

    profile and navbar profile pic done

--- 98-Projects/GVI/client/src/view/pages/Layout.tsx
+++ 98-Projects/GVI/client/src/view/pages/Layout.tsx
@@ -1,4 +1,5 @@
-import React from "react";
+import React ,{useEffect,useState} from "react";
+import axios from 'axios'
 import NavBar from "./navBar/NavBar";
 import { Outlet } from "react-router-dom";
 
@@ -8,10 +9,14 @@ interface navBarLayoutProps {
 }
 const Layout = (props: navBarLayoutProps) => {
   const { currentUserType, currentUser } = props;
+
+  const profilePic = currentUser.image
+
+  
   
   return (
     <>
-      <NavBar currentUser={currentUser} currentUserType={currentUserType} />
+      <NavBar profilePic={profilePic} currentUser={currentUser} currentUserType={currentUserType} />
 
       <Outlet />
     </>
