dbf0a77057a10f9f4519d7ce1cf7db06d709b3b4
Shani <shanaynayrom@gmail.com>
2022-08-09 22:11:51 +0300

    env

--- 05-React/08-redux/thunk/smallProject/shani_project/server/server.ts
+++ 05-React/08-redux/thunk/smallProject/shani_project/server/server.ts
@@ -2,18 +2,24 @@ import express from 'express';
 import mongoose from 'mongoose';
 const app = express();
 const port = process.env.PORT || 4001;
+require('dotenv').config()
 
 app.use(express.static('public/build'))
 app.use(express.json());
+const uri=process.env.MONGODB_URI;
 
-mongoose.connect('mongodb+srv://ShaniRom:GyHbSJcn3KZ1moKe@cluster0.vh1hg.mongodb.net/reduxChat?retryWrites=true&w=majority')
-    .then(res => {
-        console.log("connected to Mongoose");
-    })
-    .catch((err) => {
-        console.log("Failed to connect to Mongoose:")
-        console.log(err.message);
-    });
+mongoose
+  .connect(
+    uri
+  )
+  .then((result) => {
+    console.log("connected to db");
+ 
+    
+  })
+  .catch((err) => {
+    console.log(err.message);
+  });
 
 app.listen(port, () => {
     return console.log(`Express is listening at http://localhost:${port}`);
