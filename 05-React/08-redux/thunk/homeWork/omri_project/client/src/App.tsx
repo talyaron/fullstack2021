084f865b0075f4bf02e5b31fd0bdf04d19b601fb
Omri <omri832@gmail.com>
2022-08-09 11:32:59 +0300

    omri pro start

---
+++ 05-React/08-redux/thunk/homeWork/omri_project/client/src/App.tsx
@@ -0,0 +1,15 @@
+import React from 'react';
+import logo from './logo.svg';
+import { Counter } from './features/counter/Counter';
+import axios from 'axios';
+import './App.css';
+
+function App() {
+  return (
+    <div className="App">
+
+    </div>
+  );
+}
+
+export default App;
