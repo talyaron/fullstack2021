f233cf467716b9df452b693f5700de6f2bdcdccb
ShayFoyer <tfoyer@gmail.com>
2022-08-15 17:40:01 +0300

    shay redux hw

---
+++ 05-React/08-redux/thunk/smallProject/shay_project/client/src/App.tsx
@@ -0,0 +1,17 @@
+import React from 'react';
+import logo from './logo.svg';
+import { Texts } from './features/text/Texts';
+import './App.css';
+
+function App() {
+  return (
+    <div className="App">
+      <header className="App-header">
+    <Texts/>
+      
+      </header>
+    </div>
+  );
+}
+
+export default App;
