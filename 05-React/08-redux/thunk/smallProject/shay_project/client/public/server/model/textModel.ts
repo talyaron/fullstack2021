a42164d163bb452449bc663e684aa5c743ed4948
ShayFoyer <tfoyer@gmail.com>
2022-08-15 19:25:27 +0300

    accidental copy twice

--- 05-React/08-redux/thunk/smallProject/shay_project/client/public/server/model/textModel.ts
+++
@@ -1,9 +0,0 @@
-import mongoose from "mongoose";
-
-export const TextSchema= new mongoose.Schema({
-    value:String,
-    img:String
-
-})
-const Text = mongoose.model('texts', TextSchema);
-export default Text;
