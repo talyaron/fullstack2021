d805ef793997247f2ee970a23738d96940b7acf4
Omri <omri832@gmail.com>
2022-08-17 21:39:48 +0300

    SQL

---
+++ 07-SQL/01-basic/Omri/public/index.ts
@@ -0,0 +1,10 @@
+async function handleClick() {
+    try {
+
+        const { data } = await axios.post('/api/create-databse', {});
+        //   const {data} = await axios.delete('/api/delete-databse',{});
+        console.log(data)
+    } catch (error) {
+        console.error(error)
+    }
+}
