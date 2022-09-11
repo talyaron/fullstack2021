83267a54615785fe87cafeec5629db48fb78c950
OmriAharonov <omri832@gmail.com>
2022-09-11 19:55:19 +0300

    .

--- 05-React/07-react-node/followups/omri chaim yahya/models/blogModel.ts
+++
@@ -1,5 +0,0 @@
-import mongoose from "mongoose";
-const BlogSchema= new mongoose.Schema({title: 'string', article:'string'})
- const blogModel= mongoose.model('blogs', BlogSchema)
-
- export default blogModel
--- 05-React/07-react-node/followups/omri chaim yahya/routs/blogRoute.ts
+++
@@ -1,7 +0,0 @@
-import  express  from "express";
-import { addBlog } from "../controler/blogControler";
-const router= express.Router();
-
-router.post('/add-Blog', addBlog )
-
-export default router;
